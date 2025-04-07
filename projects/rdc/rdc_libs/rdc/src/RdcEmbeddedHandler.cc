ROCm/rocm-systems

commit f0f44d977fe17f8d333f9a747fec26a46a0bfc7a

Implement CPU discovery support (#77)
* Implement CPU discovery support

SWDEV-482949:

enable the CPU model name info support to the RDC, rdci command
can detect GPU and CPU modules at the same time.
It will query the CPU info through the amdsmi interface like below:

1 GPUs found.
-----------------------------------------------------------------
GPU Index        Device Information
0               AMD Radeon PRO W7800
=================================================================
1 CPUs found.
-----------------------------------------------------------------
CPU Index        Device Information
0               AMD Ryzen Threadripper PRO 7995WX 96-Cores
-----------------------------------------------------------------

Change-Id: Ibc6533c9a61000cd86c45b1bae14c3eb6788c119
Signed-off-by: Perry Yuan <perry.yuan@amd.com>

* CMAKE - Add required version for amdsmi

Change-Id: I341a89351d196ec66cce215a5d1d3953302fcc66
Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

---------

Signed-off-by: Perry Yuan <perry.yuan@amd.com>
Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
Co-authored-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

[ROCm/rdc commit: 3bdca8b8b67890d973b01ddfd1b8bf253d17431a]

projects/rdc/rdc_libs/rdc/src/RdcEmbeddedHandler.cc
@@ -49,7 +49,11 @@ class smi_initializer {
   smi_initializer() {
     // Make sure smi will not be initialized multiple times
     amdsmi_shut_down();
-    amdsmi_status_t ret = amdsmi_init(AMDSMI_INIT_AMD_GPUS);
+    amdsmi_status_t ret;
+    uint64_t init_flag_;
+    //initialize CPU and GPU instances
+    init_flag_ = AMDSMI_INIT_AMD_GPUS | AMDSMI_INIT_AMD_CPUS;
+    ret = amdsmi_init(init_flag_);
     if (ret != AMDSMI_STATUS_SUCCESS) {
       throw amd::rdc::RdcException(RDC_ST_FAIL_LOAD_MODULE, "SMI initialize fail");
     }
@@ -193,6 +197,28 @@ rdc_status_t RdcEmbeddedHandler::rdc_device_get_all(uint32_t gpu_index_list[RDC_
   return RDC_ST_OK;
 }
 
+// Discovery API
+rdc_status_t RdcEmbeddedHandler::rdc_device_get_all_cpu(uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES],
+                                                    uint32_t* count) {
+  if (!count) {
+    return RDC_ST_BAD_PARAMETER;
+  }
+
+  rdc_field_value device_count;
+  rdc_status_t status = metric_fetcher_->fetch_smi_cpu_field(0, RDC_FI_DEV_CPU_COUNT, &device_count);
+  if (status != RDC_ST_OK) {
+    std::cout << "rdc_device_get_all_cpu failed to get cpu count";
+    return status;
+  }
+
+  // Assign the index to the index list
+  *count = device_count.value.l_int;
+  for (uint32_t i = 0; i < *count; i++) {
+    cpu_index_list[i] = i;
+  }
+
+  return RDC_ST_OK;
+}
 rdc_status_t RdcEmbeddedHandler::rdc_device_get_attributes(uint32_t gpu_index,
                                                            rdc_device_attributes_t* p_rdc_attr) {
   if (!p_rdc_attr) {
@@ -201,6 +227,19 @@ rdc_status_t RdcEmbeddedHandler::rdc_device_get_attributes(uint32_t gpu_index,
   rdc_field_value device_name;
   rdc_status_t status = metric_fetcher_->fetch_smi_field(gpu_index, RDC_FI_DEV_NAME, &device_name);
   strncpy_with_null(p_rdc_attr->device_name, device_name.value.str, RDC_MAX_STR_LENGTH);
+  return status;
+}
+
+rdc_status_t RdcEmbeddedHandler::rdc_device_get_cpu_attributes(uint32_t cpu_index,
+                                                           rdc_device_attributes_t* p_rdc_attr) {
+  if (!p_rdc_attr) {
+    return RDC_ST_BAD_PARAMETER;
+  }
+  rdc_field_value device_name;
+
+  rdc_status_t status = metric_fetcher_->fetch_smi_cpu_field(cpu_index, RDC_FI_DEV_CPU_MODEL, &device_name);
+  strncpy_with_null(p_rdc_attr->device_name, device_name.value.str, RDC_MAX_STR_LENGTH);
+
   return status;
 }
 

projects/rdc/rdc_libs/rdc/src/RdcMetricFetcherImpl.cc
@@ -1295,5 +1295,66 @@ rdc_status_t RdcMetricFetcherImpl::acquire_smi_handle(RdcFieldKey fk) {
   return ret;
 }
 
+rdc_status_t RdcMetricFetcherImpl::fetch_smi_cpu_field(uint32_t cpu_index, rdc_field_t field_id,
+                                                       rdc_field_value* value) {
+  amdsmi_status_t ret;
+  amdsmi_processor_handle processor_handle = {};
+
+  ret = amdsmi_init(AMDSMI_INIT_AMD_CPUS);
+  if (ret != AMDSMI_STATUS_SUCCESS) {
+    RDC_LOG(RDC_ERROR, "Fail to init amdsmi for CPU");
+    return Smi2RdcError(ret);
+  }
+
+  if (!value) {
+    return RDC_ST_BAD_PARAMETER;
+  }
+
+  if (!is_field_valid(field_id)) {
+    RDC_LOG(RDC_ERROR, "Fail to fetch CPU field " << field_id << " which is not supported");
+    return RDC_ST_NOT_SUPPORTED;
+  }
+
+  ret = get_processor_handle_from_id(cpu_index, &processor_handle);
+  if (ret != AMDSMI_STATUS_SUCCESS) {
+    RDC_LOG(RDC_ERROR, "Failed to get processor handle for GPU " << cpu_index << " error: " << ret);
+    return Smi2RdcError(ret);
+  }
+
+  value->ts = now();
+  value->field_id = field_id;
+  value->status = AMDSMI_STATUS_NOT_SUPPORTED;
+
+  switch (field_id) {
+    case RDC_FI_DEV_CPU_COUNT: {
+      uint32_t processor_count = 0;
+      value->status = get_processor_count(processor_count);
+      value->type = INTEGER;
+      if (value->status == AMDSMI_STATUS_SUCCESS) {
+        value->value.l_int = static_cast<int64_t>(processor_count);
+      }
+
+      value->type = INTEGER;
+      value->status = Smi2RdcError(ret);
+      break;
+    }
+    case RDC_FI_DEV_CPU_MODEL: {
+      amdsmi_cpu_info_t cpu_info;
+      value->status = amdsmi_get_cpu_model_name(processor_handle, &cpu_info);
+      value->type = STRING;
+      if (value->status == AMDSMI_STATUS_SUCCESS) {
+        memcpy(value->value.str, cpu_info.model_name, sizeof(cpu_info.model_name));
+      }
+      break;
+    }
+    default: {
+      RDC_LOG(RDC_ERROR, "field_id is not supported: " << field_id);
+      break;
+    }
+  }
+
+  return RDC_ST_OK;
+}
+
 }  // namespace rdc
 }  // namespace amd

projects/rdc/CMakeLists.txt
@@ -85,6 +85,11 @@ option(BUILD_EXAMPLES "Build examples" OFF)
 # Enable shared libraries for gtest
 option(BUILD_SHARED_LIBS "Build shared library (.so) or not." ON)
 
+option(ENABLE_ESMI_LIB "Enable AMDSMI ESMI Library" ON)
+if(ENABLE_ESMI_LIB)
+    add_definitions("-DENABLE_ESMI_LIB=1")
+endif()
+
 # Enable address sanitizer
 set(ADDRESS_SANITIZER_DEFAULT OFF)
 if(DEFINED ENV{ADDRESS_SANITIZER})
@@ -165,7 +170,7 @@ if(BUILD_STANDALONE AND GRPC_ROOT STREQUAL GRPC_ROOT_DEFAULT)
     Continuing without gRPC install")
 endif()
 
-find_package(amd_smi
+find_package(amd_smi 25.4.0
     NAMES amd_smi
     HINTS ${ROCM_DIR}/lib/cmake
     CONFIGURE REQUIRED)

projects/rdc/common/rdc_field.data
@@ -217,3 +217,16 @@ FLD_DESC_ENT(RDC_HEALTH_RETIRED_PAGE_LIMIT,       "Retired page limit",         
 FLD_DESC_ENT(RDC_HEALTH_EEPROM_CONFIG_VALID,      "Verify checksum of EEPROM",                  "EEPROM_CONFIG_VALID",      true)
 FLD_DESC_ENT(RDC_HEALTH_POWER_THROTTLE_TIME,      "Power throttle status counter",              "POWER_THROTTLE_TIME",      true)
 FLD_DESC_ENT(RDC_HEALTH_THERMAL_THROTTLE_TIME,    "Total time(ms) in thermal throttle status",  "THERMAL_THROTTLE_TIME",    true)
+// CPU-related fields description
+FLD_DESC_ENT(RDC_FI_DEV_CPU_UTIL_TOTAL,          "CPU total percentage of time in use",          "CPU_UTIL_TOTAL",          false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_UTIL_USER,           "The percentage of time in use by the user",    "CPU_UTIL_USER",           false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_UTIL_NICE,           "The percentage of time in use by low priority (high nice score) programs", "CPU_UTIL_NICE", false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_UTIL_SYS,            "The percentage of time in use by the system",   "CPU_UTIL_SYS",            false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_UTIL_IRQ,            "The percentage of time in use by interrupts",   "CPU_UTIL_IRQ",            false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_TEMP_CURRENT,        "Instantaneous temperature (Celsius)",           "CPU_TEMP_CURRENT",        false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_CLOCK_CURRENT,       "Instantaneous clock speed (KHz)",               "CPU_CLOCK_CURRENT",       false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_POWER_UTIL_CURRENT,  "Instantaneous power usage (watts)",             "CPU_POWER_UTIL_CURRENT",  false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_POWER_LIMIT,         "Instantaneous power limit (watts)",             "CPU_POWER_LIMIT",         false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_VENDOR,              "The name of the vendor",                        "CPU_VENDOR",              false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_MODEL,               "The name of the model",                         "CPU_MODEL",               false)
+FLD_DESC_ENT(RDC_FI_DEV_CPU_COUNT,               "The number of CPU cores",                       "CPU_COUNT",               false)

projects/rdc/include/rdc/rdc.h
@@ -410,6 +410,21 @@ typedef enum {
   RDC_HEALTH_EEPROM_CONFIG_VALID,    //!< Reads the EEPROM and verifies the checksums
   RDC_HEALTH_POWER_THROTTLE_TIME,    //!< Power throttle status counter
   RDC_HEALTH_THERMAL_THROTTLE_TIME,  //!< Total time in thermal throttle status (microseconds)
+  /**
+   * @brief RDC CPU related fields
+   */
+  RDC_FI_DEV_CPU_UTIL_TOTAL = 10001,  //!< CPU total percentage of time in use
+  RDC_FI_DEV_CPU_UTIL_USER,           //!< The percentage of time in use by the user
+  RDC_FI_DEV_CPU_UTIL_NICE,           //!< The percentage of time in use by low priority (high nice score) programs
+  RDC_FI_DEV_CPU_UTIL_SYS,            //!< The percentage of time in use by the system
+  RDC_FI_DEV_CPU_UTIL_IRQ,            //!< The percentage of time in use by interrupts
+  RDC_FI_DEV_CPU_TEMP_CURRENT,        //!< Instantaneous temperature (Celsius)
+  RDC_FI_DEV_CPU_CLOCK_CURRENT,       //!< Instantaneous clock speed (KHz)
+  RDC_FI_DEV_CPU_POWER_UTIL_CURRENT,  //!< Instantaneous power usage (watts)
+  RDC_FI_DEV_CPU_POWER_LIMIT,         //!< Instantaneous power limit (watts)
+  RDC_FI_DEV_CPU_VENDOR,              //!< The name of the vendor
+  RDC_FI_DEV_CPU_MODEL,               //!< The name of the model
+  RDC_FI_DEV_CPU_COUNT,
 } rdc_field_t;
 
 // even and odd numbers are used for correctable and uncorrectable errors
@@ -1042,6 +1057,25 @@ rdc_status_t rdc_device_get_all(rdc_handle_t p_rdc_handle,
                                 uint32_t gpu_index_list[RDC_MAX_NUM_DEVICES], uint32_t* count);
 
 /**
+ *  @brief Get indexes corresponding to all the devices on the system.
+ *
+ *  @details Indexes represents RDC CPU Id corresponding to each CPU on the
+ * system and is immutable during the lifespan of the engine. The list
+ * should be queried again if the engine is restarted.
+ *
+ *  @param[in] p_rdc_handle The RDC handler.
+ *
+ *  @param[out] cpu_index_list Array reference to fill CPU indexes present on
+ *  the system.
+ *
+ *  @param[out] count Number of CPUs returned in cpu_index_list.
+ *
+ *  @retval ::RDC_ST_OK is returned upon successful call.
+ */
+rdc_status_t rdc_device_get_all_cpu(rdc_handle_t p_rdc_handle,
+                                uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES], uint32_t* count);
+
+/**
  *  @brief Gets device attributes corresponding to the gpu_index.
  *
  *  @details Fetch the attributes, such as device name, of a GPU.
@@ -1057,6 +1091,23 @@ rdc_status_t rdc_device_get_all(rdc_handle_t p_rdc_handle,
  */
 rdc_status_t rdc_device_get_attributes(rdc_handle_t p_rdc_handle, uint32_t gpu_index,
                                        rdc_device_attributes_t* p_rdc_attr);
+
+/**
+ *  @brief Gets device attributes corresponding to the cpu_index.
+ *
+ *  @details Fetch the attributes, such as device name, of a CPU.
+ *
+ *  @param[in] p_rdc_handle The RDC handler.
+ *
+ *  @param[in] cpu_index CPU index corresponding to which the attributes
+ *  should be fetched
+ *
+ *  @param[out] p_rdc_attr CPU attribute corresponding to the cpu_index.
+ *
+ *  @retval ::RDC_ST_OK is returned upon successful call.
+ */
+rdc_status_t rdc_device_get_cpu_attributes(rdc_handle_t p_rdc_handle, uint32_t cpu_index,
+                                      rdc_device_attributes_t* p_rdc_attr);
 
 /**
  *  @brief Get version information of components used by rdc.

projects/rdc/include/rdc_lib/RdcHandler.h
@@ -43,7 +43,11 @@ class RdcHandler {
   // Discovery API
   virtual rdc_status_t rdc_device_get_all(uint32_t gpu_index_list[RDC_MAX_NUM_DEVICES],
                                           uint32_t* count) = 0;
+  virtual rdc_status_t rdc_device_get_all_cpu(uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES],
+                                          uint32_t* count) = 0;
   virtual rdc_status_t rdc_device_get_attributes(uint32_t gpu_index,
+                                                 rdc_device_attributes_t* p_rdc_attr) = 0;
+  virtual rdc_status_t rdc_device_get_cpu_attributes(uint32_t cpu_index,
                                                  rdc_device_attributes_t* p_rdc_attr) = 0;
   virtual rdc_status_t rdc_device_get_component_version(rdc_component_t component,
                                                         rdc_component_version_t* p_rdc_compv) = 0;

projects/rdc/include/rdc_lib/RdcMetricFetcher.h
@@ -39,7 +39,8 @@ class RdcMetricFetcher {
 
   virtual rdc_status_t fetch_smi_field(uint32_t gpu_index, rdc_field_t field_id,
                                        rdc_field_value* value) = 0;
-
+  virtual rdc_status_t fetch_smi_cpu_field(uint32_t cpu_index, rdc_field_t field_id,
+                               rdc_field_value* value) = 0;
   virtual rdc_status_t bulk_fetch_smi_fields(
       rdc_gpu_field_t* fields, uint32_t fields_count,
       std::vector<rdc_gpu_field_value_t>& results) = 0;  // NOLINT

projects/rdc/include/rdc_lib/impl/RdcEmbeddedHandler.h
@@ -53,7 +53,12 @@ class RdcEmbeddedHandler final : public RdcHandler {
   // Discovery API
   rdc_status_t rdc_device_get_all(uint32_t gpu_index_list[RDC_MAX_NUM_DEVICES],
                                   uint32_t* count) override;
+  // Discovery API For CPU
+  rdc_status_t rdc_device_get_all_cpu(uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES],
+                                  uint32_t* count) override;
   rdc_status_t rdc_device_get_attributes(uint32_t gpu_index,
+                                         rdc_device_attributes_t* p_rdc_attr) override;
+  rdc_status_t rdc_device_get_cpu_attributes(uint32_t cpu_index,
                                          rdc_device_attributes_t* p_rdc_attr) override;
   rdc_status_t rdc_device_get_component_version(rdc_component_t component,
                                                 rdc_component_version_t* p_rdc_compv) override;

commit 3e8f56c4301ecac9b380d605a152085705a9bfb1

Fix breaking changes introduced with CPU support
Changes introduced in f0f44d977fe17f8d333f9a747fec26a46a0bfc7a
broke RDC if it was compiled without ESMI support, or if esmi driver is
not loaded when RDC is being used.

Change-Id: Id54e1e9002d2e3cf09240081149eed84178700af
Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>


[ROCm/rdc commit: 0aeceefcb3086b181efbf867eb6f00e326983041]

projects/rdc/rdc_libs/rdc/src/RdcEmbeddedHandler.cc
@@ -50,11 +50,18 @@ class smi_initializer {
     // Make sure smi will not be initialized multiple times
     amdsmi_shut_down();
     amdsmi_status_t ret;
-    uint64_t init_flag_;
-    //initialize CPU and GPU instances
-    init_flag_ = AMDSMI_INIT_AMD_GPUS | AMDSMI_INIT_AMD_CPUS;
+    uint64_t init_flag_ = AMDSMI_INIT_AMD_GPUS;
+#ifdef ENABLE_ESMI_LIB
+    init_flag_ = init_flag_ | AMDSMI_INIT_AMD_CPUS;
+#endif
     ret = amdsmi_init(init_flag_);
+    if (init_flag_ & AMDSMI_INIT_AMD_CPUS) {
+      RDC_LOG(RDC_ERROR,
+              "Failed to initalize amdsmi with CPUs and GPUs enabled.. trying GPUs only.");
+      ret = amdsmi_init(AMDSMI_INIT_AMD_GPUS);
+    }
     if (ret != AMDSMI_STATUS_SUCCESS) {
+      RDC_LOG(RDC_ERROR, "SMI FAILED with" << ret);
       throw amd::rdc::RdcException(RDC_ST_FAIL_LOAD_MODULE, "SMI initialize fail");
     }
   }
@@ -198,14 +205,15 @@ rdc_status_t RdcEmbeddedHandler::rdc_device_get_all(uint32_t gpu_index_list[RDC_
 }
 
 // Discovery API
-rdc_status_t RdcEmbeddedHandler::rdc_device_get_all_cpu(uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES],
-                                                    uint32_t* count) {
+rdc_status_t RdcEmbeddedHandler::rdc_device_get_all_cpu(
+    uint32_t cpu_index_list[RDC_MAX_NUM_DEVICES], uint32_t* count) {
   if (!count) {
     return RDC_ST_BAD_PARAMETER;
   }
 
   rdc_field_value device_count;
-  rdc_status_t status = metric_fetcher_->fetch_smi_cpu_field(0, RDC_FI_DEV_CPU_COUNT, &device_count);
+  rdc_status_t status =
+      metric_fetcher_->fetch_smi_cpu_field(0, RDC_FI_DEV_CPU_COUNT, &device_count);
   if (status != RDC_ST_OK) {
     std::cout << "rdc_device_get_all_cpu failed to get cpu count";
     return status;
@@ -216,9 +224,9 @@ rdc_status_t RdcEmbeddedHandler::rdc_device_get_all_cpu(uint32_t cpu_index_list[
   for (uint32_t i = 0; i < *count; i++) {
     cpu_index_list[i] = i;
   }
-
   return RDC_ST_OK;
 }
+
 rdc_status_t RdcEmbeddedHandler::rdc_device_get_attributes(uint32_t gpu_index,
                                                            rdc_device_attributes_t* p_rdc_attr) {
   if (!p_rdc_attr) {
@@ -230,14 +238,15 @@ rdc_status_t RdcEmbeddedHandler::rdc_device_get_attributes(uint32_t gpu_index,
   return status;
 }
 
-rdc_status_t RdcEmbeddedHandler::rdc_device_get_cpu_attributes(uint32_t cpu_index,
-                                                           rdc_device_attributes_t* p_rdc_attr) {
+rdc_status_t RdcEmbeddedHandler::rdc_device_get_cpu_attributes(
+    uint32_t cpu_index, rdc_device_attributes_t* p_rdc_attr) {
   if (!p_rdc_attr) {
     return RDC_ST_BAD_PARAMETER;
   }
   rdc_field_value device_name;
 
-  rdc_status_t status = metric_fetcher_->fetch_smi_cpu_field(cpu_index, RDC_FI_DEV_CPU_MODEL, &device_name);
+  rdc_status_t status =
+      metric_fetcher_->fetch_smi_cpu_field(cpu_index, RDC_FI_DEV_CPU_MODEL, &device_name);
   strncpy_with_null(p_rdc_attr->device_name, device_name.value.str, RDC_MAX_STR_LENGTH);
 
   return status;

projects/rdc/rdc_libs/rdc/src/RdcMetricFetcherImpl.cc
@@ -1338,6 +1338,8 @@ rdc_status_t RdcMetricFetcherImpl::fetch_smi_cpu_field(uint32_t cpu_index, rdc_f
       value->status = Smi2RdcError(ret);
       break;
     }
+#ifdef ENABLE_ESMI_LIB
+// amdsmi_cpu_info_t is only defined if ENABLE_ESMI_LIB is set
     case RDC_FI_DEV_CPU_MODEL: {
       amdsmi_cpu_info_t cpu_info;
       value->status = amdsmi_get_cpu_model_name(processor_handle, &cpu_info);
@@ -1347,6 +1349,7 @@ rdc_status_t RdcMetricFetcherImpl::fetch_smi_cpu_field(uint32_t cpu_index, rdc_f
       }
       break;
     }
+#endif
     default: {
       RDC_LOG(RDC_ERROR, "field_id is not supported: " << field_id);
       break;