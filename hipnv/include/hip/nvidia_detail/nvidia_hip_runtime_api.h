e3ddcc3b315ee1816d478ed5a84b5a04566a2cdb
agunashe <ajay.gunashekar@amd.com>
2025-07-25 13:40:28 -0700

    SWDEV-546177 - hipModuleGetLoadingMode nvidia

--- hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
+++ hipnv/include/hip/nvidia_detail/nvidia_hip_runtime_api.h
@@ -3570,6 +3570,10 @@ inline static hipError_t hipModuleGetFunctionCount(unsigned int* count, hipModul
     return hipCUResultTohipError(cuModuleGetFunctionCount(count, mod));
 }
 
+inline static hipError_t hipModuleGetLoadingMode(hipModuleLoadingMode_t* mode) {
+    return hipCUResultTohipError(cuModuleGetLoadingMode(mode));
+}
+
 inline static hipError_t hipModuleGetTexRef(hipTexRef* pTexRef, hipModule_t hmod, const char* name){
     return hipCUResultTohipError(cuModuleGetTexRef(pTexRef, hmod, name));
 }
