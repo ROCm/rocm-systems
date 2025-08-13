86037949f7cf3d1585721b8b8536b6d2e783acef
Marko Arandjelovic <Marko.Arandjelovic@amd.com>
2025-08-13 10:12:01 +0000

    SWDEV-548482 - Added destroy calls

--- projects/hip-tests/catch/unit/memory/hipGetProcAddressMemoryApis.cc
+++ projects/hip-tests/catch/unit/memory/hipGetProcAddressMemoryApis.cc
@@ -1121,6 +1121,7 @@ TEST_CASE("Unit_hipGetProcAddress_MemoryApisSetAndGetAttributes") {
     REQUIRE(allAttributesDataWithPtr.hostPointer == allAttributesData.hostPointer);
     REQUIRE(allAttributesDataWithPtr.isManaged == allAttributesData.isManaged);
     REQUIRE(allAttributesDataWithPtr.allocationFlags == allAttributesData.allocationFlags);
+    HIP_CHECK(hipFree(devPtr2));
   }
 
   // Validating hipDrvPointerGetAttributes API
@@ -1155,6 +1156,8 @@ TEST_CASE("Unit_hipGetProcAddress_MemoryApisSetAndGetAttributes") {
     REQUIRE(devicePointerWithPtr == devicePointer);
     REQUIRE(rangeSizeWithPtr == rangeSize);
     REQUIRE(startAddressWithPtr == startAddress);
+
+    HIP_CHECK(hipFree(devPtr3));
   }
 }
 
@@ -5780,7 +5783,7 @@ TEST_CASE("Unit_hipGetProcAddress_MemoryApisStreamOrderedMemory") {
     REQUIRE(size == 1024);
     REQUIRE(dyn_hipMallocFromPoolAsync_ptr(&dPtr, 1, mem_pool, stream) == hipErrorOutOfMemory);
 
-    HIP_CHECK(hipFreeAsync(dPtr, stream));
+    HIP_CHECK(dyn_hipFreeAsync_ptr(dPtr, stream));
     HIP_CHECK(hipMemPoolDestroy(mem_pool));
     HIP_CHECK(hipStreamDestroy(stream));
   }
