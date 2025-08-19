388beb561ee5ebcd93d4b2652b22ec1906506614
ggottipa <ggottipa@amd.com>
2025-08-19 06:44:53 -0500

    Enabling --kokkos-trace in rocprofiler-compute (for SWDEV-520867, after verifying SWDEV-516561). It was disabled in 9a92c7e1b1b4c9b03c215d8a4566851888a9e8ef

--- projects/rocprofiler-compute/src/argparser.py
+++ projects/rocprofiler-compute/src/argparser.py
@@ -162,8 +162,8 @@ Examples:
         required=False,
         default=False,
         action="store_true",
-        help=argparse.SUPPRESS,
-        # help="\t\t\tKokkos trace, traces Kokkos API calls.",
+        # help=argparse.SUPPRESS,
+        help="\t\t\tKokkos trace, traces Kokkos API calls.",
     )
     profile_group.add_argument(
         "-k",
