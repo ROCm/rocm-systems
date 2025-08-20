3165bffe73279f2cfbeb0f68ac4a60b8ccafa735
Danylo Lytovchenko <danylo.lytovchenko@amd.com>
2025-08-20 11:00:40 +0200

    [DO NOT MERGE] testing approval requirements from codeowners

--- projects/rocm-core/rocm_getpath.cpp
+++ projects/rocm-core/rocm_getpath.cpp
@@ -11,8 +11,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -23,30 +23,33 @@
 //
 ////////////////////////////////////////////////////////////////////////////////
 
-#include <string.h>
-#include <stdlib.h>
-#include <limits.h> /* PATH_MAX */
-#include <stdio.h>
-#include <link.h>
-#include <dlfcn.h>
 #include "rocm_getpath.h"
+#include <dlfcn.h>
+#include <limits.h> /* PATH_MAX */
+#include <link.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* Macro for NULL CHECK */
-#define NULL_CHECK(ptr) if(!ptr) return PathIncorrecPararmeters;
-
+#define NULL_CHECK(ptr)                                                        \
+  if (!ptr)                                                                    \
+    return PathIncorrecPararmeters;
 
 /* Target Library Install Dir */
 #define TARGET_LIB_INSTALL_DIR TARGET_LIBRARY_INSTALL_DIR
 
 /* Target Library Name Buf Size */
-#define LIBRARY_FILENAME_BUFSZ (PATH_MAX+1)
+#define LIBRARY_FILENAME_BUFSZ (PATH_MAX + 1)
 
 /* Internal Function to get Base Path - Ref from Icarus Logic*/
 static int getROCmBase(char *buf);
 
 /* Public Function to get the ROCm Install Base Path
-//  Argument1 (out) :  InstallPath (char** pointer which will return InstallPath found)
-//  Argument2 (out) :  InstallPathLen (Pointer to integer (size of InstallPath) returned)
+//  Argument1 (out) :  InstallPath (char** pointer which will return InstallPath
+found)
+//  Argument2 (out) :  InstallPathLen (Pointer to integer (size of InstallPath)
+returned)
 //  Usage :
 //      char *installPath=NULL;
 //      int installPathLen = 0;
@@ -58,66 +61,66 @@ static int getROCmBase(char *buf);
 //    ...
 
 */
-PathErrors_t getROCmInstallPath( char** InstallPath, unsigned int *InstallPathLen ) {
+PathErrors_t getROCmInstallPath(char **InstallPath,
+                                unsigned int *InstallPathLen) {
 
-        NULL_CHECK(InstallPath);
-        NULL_CHECK(InstallPathLen);
-        int ret = PathErrorMAX;
-        char *bufPtr = (char *)NULL;
-        unsigned int bufSz = 0;
+  NULL_CHECK(InstallPath);
+  NULL_CHECK(InstallPathLen);
+  int ret = PathErrorMAX;
+  char *bufPtr = (char *)NULL;
+  unsigned int bufSz = 0;
 
-        bufPtr = (char *)malloc( LIBRARY_FILENAME_BUFSZ * sizeof(char) );
-        memset( bufPtr, 0, LIBRARY_FILENAME_BUFSZ );
-        *InstallPathLen = 0;
-        *InstallPath = NULL;
+  bufPtr = (char *)malloc(LIBRARY_FILENAME_BUFSZ * sizeof(char));
+  memset(bufPtr, 0, LIBRARY_FILENAME_BUFSZ);
+  *InstallPathLen = 0;
+  *InstallPath = NULL;
 
-        ret = getROCmBase(bufPtr);
-        if (0 > ret){
-          free(bufPtr);
-          return (PathErrors_t)ret;
-        }
-        else if (0 == ret){
-          free(bufPtr);
-          return PathFailedToGetBase;
-        }
-        else{
-          bufSz = ret;//additional char for null termination
-        }
+  ret = getROCmBase(bufPtr);
+  if (0 > ret) {
+    free(bufPtr);
+    return (PathErrors_t)ret;
+  } else if (0 == ret) {
+    free(bufPtr);
+    return PathFailedToGetBase;
+  } else {
+    bufSz = ret; // additional char for null termination
+  }
 
-        *InstallPath = bufPtr;
-        *InstallPathLen = bufSz;
-        return  PathSuccess;
+  *InstallPath = bufPtr;
+  *InstallPathLen = bufSz;
+  return PathSuccess;
 }
 
-/* General purpose function that fills the directory to find rocm related stuff */
+/* General purpose function that fills the directory to find rocm related stuff
+ */
 /* returns the offset into the buffer for the terminating NUL or -1 for error */
 /* The buffer should be at least PATH_MAX */
-static int getROCmBase(char *buf)
-{
-  int len=0;
-  char *envStr=NULL;
+static int getROCmBase(char *buf) {
+  int len = 0;
+  char *envStr = NULL;
   char libFileName[LIBRARY_FILENAME_BUFSZ];
-  char *end=NULL;
+  char *end = NULL;
 
   // Check Environment Variable is set for ROCM
   // install base path, then use it directly.
   if ((envStr = getenv("ROCM_PATH"))) {
-    /* User space override, essentially just copied through as long as it is not too long */
+    /* User space override, essentially just copied through as long as it is not
+     * too long */
     len = strlen(envStr);
     if (len > 0) {
       if (envStr[len] == '/') {
-         /* Already has at least one terminating */
-         len--;
+        /* Already has at least one terminating */
+        len--;
       }
-      if (len > PATH_MAX-1 ) {
-         return PathValuesTooLong;
+      if (len > PATH_MAX - 1) {
+        return PathValuesTooLong;
       }
       strncpy(buf, envStr, len);
-      buf[len]='/';
-      buf[len+1]='\0';
+      buf[len] = '/';
+      buf[len + 1] = '\0';
 
       /* Length of string including trailing '/' */
-      return len+1;
+      return len + 1;
     }
   }
 
@@ -126,23 +129,22 @@ static int getROCmBase(char *buf)
   // and get rocm base install path using the lib Path.
 #if BUILD_SHARED_LIBS
   sprintf(libFileName, "lib%s.so", TARGET_LIBRARY_NAME);
-  void *handle=dlopen(libFileName,RTLD_NOW);
-  if (!handle){
+  void *handle = dlopen(libFileName, RTLD_NOW);
+  if (!handle) {
     /* We can't find the library */
     return PathLinuxRuntimeErrors;
   }
   /* Variable to hold the return value from dlinfo */
-  struct link_map *map = (struct link_map*)NULL;
+  struct link_map *map = (struct link_map *)NULL;
   /* Query the runtime linker */
-  dlinfo(handle,RTLD_DI_LINKMAP,&map);
-  if (map ->l_name && realpath(map ->l_name,buf)) {
+  dlinfo(handle, RTLD_DI_LINKMAP, &map);
+  if (map->l_name && realpath(map->l_name, buf)) {
     /* Get Library Directory Path */
     char *end = strrchr(buf, '/');
     if (end && end > buf) {
       *end = '\0';
     }
-  }
-  else{
+  } else {
     /* If l_name is NULL or realpath() failed
      * Close handle before return error */
     dlclose(handle);
@@ -152,8 +154,8 @@ static int getROCmBase(char *buf)
   dlclose(handle);
   /* find the start of substring TARGET_LIB_INSTALL_DIR
    * To strip down Path up to Parent Directory of TARGET_LIB_INSTALL_DIR. */
-  end=strstr(buf, TARGET_LIB_INSTALL_DIR);
-  if( NULL == end ){
+  end = strstr(buf, TARGET_LIB_INSTALL_DIR);
+  if (NULL == end) {
     /* We can't find the library install directory*/
     return PathLinuxRuntimeErrors;
   }
@@ -165,4 +167,3 @@ static int getROCmBase(char *buf)
   len = strlen(buf);
   return len;
 }
-
--- projects/rocm-core/rocm_version.cpp
+++ projects/rocm-core/rocm_version.cpp
@@ -1,77 +1,77 @@
 #include "rocm_version.h"
-#include <string.h>
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+#define NULL_CHECK(ptr)                                                        \
+  if (!ptr)                                                                    \
+    return VerIncorrecPararmeters;
 
-#define NULL_CHECK(ptr)	if(!ptr) return VerIncorrecPararmeters;
+#define CHECK_AND_REPORT_API_RESULT(val)                                       \
+  do {                                                                         \
+    if (VerSuccess != val) {                                                   \
+      const char *ErrStrings[VerErrorMAX] = {                                  \
+          "VerSuccess", "VerIncorrecPararmeters", "VerValuesNotDefined"};      \
+      fprintf(stderr, " API returned : %s \n", ErrStrings[val]);               \
+      fflush(stderr);                                                          \
+      return val;                                                              \
+    }                                                                          \
+  } while (0);
 
+VerErrors getROCmVersion(unsigned int *Major, unsigned int *Minor,
+                         unsigned int *Patch) {
 
-#define CHECK_AND_REPORT_API_RESULT(val)	do {					\
-							if(VerSuccess != val) {	\
-								const char *ErrStrings[VerErrorMAX]= { "VerSuccess", "VerIncorrecPararmeters", "VerValuesNotDefined" }; 	\
-								fprintf(stderr, " API returned : %s \n", ErrStrings[val]);	\
-								fflush(stderr);		\
-								return val;		\
-							}				\
-						}while(0);
+  NULL_CHECK(Major)
+  NULL_CHECK(Minor)
+  NULL_CHECK(Patch)
 
+  *Major = ROCM_VERSION_MAJOR;
+  *Minor = ROCM_VERSION_MINOR;
+  *Patch = ROCM_VERSION_PATCH;
 
-
-VerErrors getROCmVersion(unsigned int* Major, unsigned int* Minor, unsigned int* Patch) {
-
-	NULL_CHECK(Major)
-	NULL_CHECK(Minor)
-	NULL_CHECK(Patch)
-
-	*Major=ROCM_VERSION_MAJOR;
-	*Minor=ROCM_VERSION_MINOR;
-	*Patch=ROCM_VERSION_PATCH;
-
-	return	VerSuccess;
+  return VerSuccess;
 }
 
+static VerErrors getBuildInfoLen(int *InfoStrlen) {
 
-
-static VerErrors getBuildInfoLen( int* InfoStrlen ) {
-
-	NULL_CHECK(InfoStrlen);
+  NULL_CHECK(InfoStrlen);
 #if defined(ROCM_BUILD_INFO)
-	*InfoStrlen = 1 + strlen(ROCM_BUILD_INFO);//additional char for null termination
+  *InfoStrlen =
+      1 + strlen(ROCM_BUILD_INFO); // additional char for null termination
 #else
-	return VerValuesNotDefined;
-#endif //end defination checker
-	return	VerSuccess;
+  return VerValuesNotDefined;
+#endif // end defination checker
+  return VerSuccess;
 }
 
-static VerErrors getBuildInfo( char* InfoString, int len ) {
+static VerErrors getBuildInfo(char *InfoString, int len) {
 
-	NULL_CHECK(InfoString);
+  NULL_CHECK(InfoString);
 #if defined(ROCM_BUILD_INFO)
 
-	strcpy(InfoString,ROCM_BUILD_INFO);
-	InfoString[len]='\0';
+  strcpy(InfoString, ROCM_BUILD_INFO);
+  InfoString[len] = '\0';
 #else
-	return VerValuesNotDefined;
-#endif //end defination checker
-	return	VerSuccess;
+  return VerValuesNotDefined;
+#endif // end defination checker
+  return VerSuccess;
 }
 
 VerErrors printBuildInfo() {
 
-	int lenstr=0;
-        VerErrors apiret=VerSuccess;
+  int lenstr = 0;
+  VerErrors apiret = VerSuccess;
 
-	apiret=getBuildInfoLen(&lenstr);
-	CHECK_AND_REPORT_API_RESULT(apiret);
+  apiret = getBuildInfoLen(&lenstr);
+  CHECK_AND_REPORT_API_RESULT(apiret);
 
-	char* cstr=(char*) malloc(lenstr*sizeof(char));
-        apiret=getBuildInfo(cstr,lenstr);
-	CHECK_AND_REPORT_API_RESULT(apiret);
+  char *cstr = (char *)malloc(lenstr * sizeof(char));
+  apiret = getBuildInfo(cstr, lenstr);
+  CHECK_AND_REPORT_API_RESULT(apiret);
 
-	printf("\n Build Info of lib = [%s] \n",cstr);
+  printf("\n Build Info of lib = [%s] \n", cstr);
 
-        free(cstr);
+  free(cstr);
 
-	return VerSuccess;
+  return VerSuccess;
 }
