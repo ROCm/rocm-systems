65ee1cf9ab96181ae9915eb82f0badd53f7c91cd
SrinivasRao Gollamandala <Srinivasarao.Gollamandala@amd.com>
2025-08-21 09:48:44 +0000

    SWDEV-546345-Added tests for hipMemcpyBatchAsync API

---
+++ projects/hip-tests/catch/unit/memory/hipMemcpyBatchAsync.cc
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <hip_test_common.hh>
+#include <hip_test_defgroups.hh>
+/**
+ * @addtogroup hipMemcpyBatchAsync hipMemcpyBatchAsync
+ * @{
+ * @ingroup MemoryTest
+ * `hipError_t hipMemcpyBatchAsync(void** dsts, void** srcs, size_t* sizes, size_t count,
+                               hipMemcpyAttributes* attrs, size_t* attrsIdxs, size_t numAttrs,
+                               size_t* failIdx, hipStream_t stream __dparm(0))` -
+ * Perform Batch of 1D copies.
+ */
+/**
+ * Test Description
+ * ------------------------
+ * - Test case to verify the 1D batch memory copy.
+ * 1. Create Array of device pointers(Src, Dst).
+ * 2. Set the MemcpyBatch params.
+ * 3. Perform batch memcpy operation.
+ * 4. Validate data on host.
+ * Test source
+ * ------------------------
+ * - catch/unit/memory/hipMemcpyBatchAsync.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 7.1
+ */
+#if HT_NVIDIA
+TEST_CASE("Unit_hipMemcpyBatchAsync_BasicFunctional") {
+  const size_t count = 2;
+  size_t numAttrs = 1;
+  size_t size = 4096 * sizeof(char);
+  hipStream_t stream = NULL;
+  HIP_CHECK(hipStreamCreate(&stream));
+
+  // Allocate buffers for pointer-ptr copy
+  void *srcPtr[count], *dstPtr[count];
+  char** hostPtr = new char*[count];
+  size_t sizes[2];
+  size_t attrsIdxs[1];
+  for (int i = 0; i < count; i++) {
+    HIP_CHECK(hipMalloc(&srcPtr[i], size));
+    HIP_CHECK(hipMalloc(&dstPtr[i], size));
+    HIP_CHECK(hipMemset(srcPtr[i], 'b', size));  // Fill with value
+    sizes[i] = size;
+    hostPtr[i] = new char[size];
+  }
+
+  hipMemcpyAttributes attr[1];
+  attr[0].dstLocHint.type = hipMemLocationTypeDevice;
+  attr[0].dstLocHint.id = 0;
+  attr[0].srcLocHint.type = hipMemLocationTypeDevice;
+  attr[0].srcLocHint.id = 0;
+  attr[0].flags = hipMemcpyFlagDefault;
+  attr[0].srcAccessOrder = hipMemcpySrcAccessOrderStream;
+
+  attrsIdxs[0] = 0;
+  size_t failIdx;
+
+  HIP_CHECK(hipMemcpyBatchAsync(dstPtr, srcPtr, sizes, count, attr, attrsIdxs, numAttrs, &failIdx,
+                                stream));
+  for (int i = 0; i < count; i++) {
+    HIP_CHECK(hipMemcpy(hostPtr[i], dstPtr[i], size, hipMemcpyDeviceToHost));
+    for (int j = 0; j < size; j++) {
+      REQUIRE(hostPtr[i][j] == 'b');
+    }
+  }
+}
+#endif
+/**
+ * Test Description
+ * ------------------------
+ * - Test case to verify the negative cases of hipMemcpyBatchAsync.
+ * 1. Dst Array as nullptr.
+ * 2. Src Array as nullptr.
+ * 3. Operations Count as 0.
+ * 4. Num of attributes as 0.
+ * 5. Sizes Array as nullptr.
+ * 6. Attr Array as nullptr.
+ * 7. AttrsIdxs Array as nullptr.
+ * Test source
+ * ------------------------
+ * - catch/unit/memory/hipMemcpyBatchAsync.cc
+ * Test requirements
+ * ------------------------
+ *  - HIP_VERSION >= 7.1
+ */
+TEST_CASE("Unit_hipMemcpyBatchAsync_NegativeTsts") {
+  const size_t count = 2;
+  size_t numAttrs = 1;
+  size_t sizes[2];
+  size_t attrsIdxs[1];
+  size_t size = 4096 * sizeof(char);
+  hipStream_t stream = NULL;
+  HIP_CHECK(hipStreamCreate(&stream));
+  void *srcPtr[count], *dstPtr[count];
+  for (int i = 0; i < count; i++) {
+    HIP_CHECK(hipMalloc(&srcPtr[i], size));
+    HIP_CHECK(hipMalloc(&dstPtr[i], size));
+    sizes[i] = size;
+  }
+  hipMemcpyAttributes attr[1];
+  attr[0].dstLocHint.type = hipMemLocationTypeDevice;
+  attr[0].dstLocHint.id = 0;
+  attr[0].srcLocHint.type = hipMemLocationTypeDevice;
+  attr[0].srcLocHint.id = 0;
+  attr[0].flags = hipMemcpyFlagDefault;
+  attr[0].srcAccessOrder = hipMemcpySrcAccessOrderStream;
+
+  attrsIdxs[0] = 0;
+  size_t failIdx;
+  SECTION("Dst Array as nullptr") {
+    HIP_CHECK_ERROR(hipMemcpyBatchAsync(nullptr, srcPtr, sizes, count, attr, attrsIdxs, numAttrs,
+                                        &failIdx, stream),
+                    hipErrorInvalidValue);
+  }
+  SECTION("Src Array as nullptr") {
+    HIP_CHECK_ERROR(hipMemcpyBatchAsync(dstPtr, nullptr, sizes, count, attr, attrsIdxs, numAttrs,
+                                        &failIdx, stream),
+                    hipErrorInvalidValue);
+  }
+  SECTION("Count as zero") {
+    HIP_CHECK_ERROR(
+        hipMemcpyBatchAsync(dstPtr, srcPtr, sizes, 0, attr, attrsIdxs, numAttrs, &failIdx, stream),
+        hipErrorInvalidValue);
+  }
+  SECTION("sizes Array as nullptr") {
+    HIP_CHECK_ERROR(hipMemcpyBatchAsync(dstPtr, srcPtr, nullptr, count, attr, attrsIdxs, numAttrs,
+                                        &failIdx, stream),
+                    hipErrorInvalidValue);
+  }
+#if HT_NVIDIA
+  SECTION("Number of Attributes as zero") {
+    HIP_CHECK_ERROR(
+        hipMemcpyBatchAsync(dstPtr, srcPtr, sizes, count, attr, attrsIdxs, 0, &failIdx, stream),
+        hipErrorInvalidValue);
+  }
+  SECTION("Attr Array as nullptr") {
+    HIP_CHECK_ERROR(hipMemcpyBatchAsync(dstPtr, srcPtr, sizes, count, nullptr, attrsIdxs, numAttrs,
+                                        &failIdx, stream),
+                    hipErrorInvalidValue);
+  }
+
+  SECTION("attrsIdxs Array as nullptr") {
+    HIP_CHECK_ERROR(hipMemcpyBatchAsync(dstPtr, srcPtr, sizes, count, attr, nullptr, numAttrs,
+                                        &failIdx, stream),
+                    hipErrorInvalidValue);
+  }
+#endif
+}
+/**
+ * End doxygen group MemoryTest.
+ * @}
+ */
