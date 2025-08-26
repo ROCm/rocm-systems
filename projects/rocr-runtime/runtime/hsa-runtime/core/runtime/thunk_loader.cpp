6aaa319116250be9c69c0506d90080c031be1997
Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>
2025-08-26 16:06:56 -0400

    rocr: Initial implementation of AMD Infinity Storage (AIS)
    Implement first two API: hsa_amd_ais_file_write and hsa_amd_ais_file_read
    
    v2: Change API from hsa_amd_ to hsa_amd_ais_
        Change API to take in handle instead of fd for compatibility accross
         different platforms
    
    Original Author: Chris Freehill <Chris.Freehill@amd.com>
    Signed-off-by: Harish Kasiviswanathan <Harish.Kasiviswanathan@amd.com>

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/thunk_loader.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/thunk_loader.cpp
@@ -379,6 +379,9 @@ namespace core {
       DRM_PFN(amdgpu_device_initialize) = (DRM_DEF(amdgpu_device_initialize)*)dlsym(thunk_handle, "amdgpu_device_initialize");
       if (DRM_PFN(amdgpu_device_initialize) == NULL) goto ERROR;
 
+      HSAKMT_PFN(hsaKmtAisReadWriteFile) = (HSAKMT_DEF(hsaKmtAisReadWriteFile)*)dlsym(thunk_handle, "hsaKmtAisReadWriteFile");
+      if (HSAKMT_PFN(hsaKmtAisReadWriteFile) == NULL) goto ERROR;
+
       DRM_PFN(amdgpu_device_deinitialize) = (DRM_DEF(amdgpu_device_deinitialize)*)dlsym(thunk_handle, "amdgpu_device_deinitialize");
       if (DRM_PFN(amdgpu_device_deinitialize) == NULL) goto ERROR;
 
@@ -500,6 +503,7 @@ ERROR:
       HSAKMT_PFN(hsaKmtPcSamplingStop) = (HSAKMT_DEF(hsaKmtPcSamplingStop)*)(&hsaKmtPcSamplingStop);
       HSAKMT_PFN(hsaKmtPcSamplingSupport) = (HSAKMT_DEF(hsaKmtPcSamplingSupport)*)(&hsaKmtPcSamplingSupport);
       HSAKMT_PFN(hsaKmtModelEnabled) = (HSAKMT_DEF(hsaKmtModelEnabled)*)(&hsaKmtModelEnabled);
+      HSAKMT_PFN(hsaKmtAisReadWriteFile) = (HSAKMT_DEF(hsaKmtAisReadWriteFile)*)(&hsaKmtAisReadWriteFile);
 
       DRM_PFN(amdgpu_device_initialize) = (DRM_DEF(amdgpu_device_initialize)*)(&amdgpu_device_initialize);
       DRM_PFN(amdgpu_device_deinitialize) = (DRM_DEF(amdgpu_device_deinitialize)*)(&amdgpu_device_deinitialize);
