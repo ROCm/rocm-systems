4db3620d3329f729dd15658dea9955984cf9712e
Mark Meserve <Mark.Meserve@amd.com>
2025-09-09 21:39:31 +0000

    attach: add cmake variable for attachment support

--- projects/rocprofiler-register/cmake/rocprofiler_register_options.cmake
+++ projects/rocprofiler-register/cmake/rocprofiler_register_options.cmake
@@ -39,6 +39,7 @@ rocprofiler_register_add_option(ROCPROFILER_REGISTER_BUILD_FMT "Build FMT" ON)
 rocprofiler_register_add_option(
     ROCPROFILER_REGISTER_DEP_ROCMCORE "DEB and RPM package depend on rocm-core package"
     ${ROCM_DEP_ROCMCORE})
+rocprofiler_register_add_option(ROCPROFILER_REGISTER_ALWAYS_SUPPORT_ATTACH "Enable attachment unconditionally, instead of using an environment variable" OFF)
 
 # In the future, we will do this even with clang-tidy enabled
 if(ROCPROFILER_REGISTER_BUILD_CI
