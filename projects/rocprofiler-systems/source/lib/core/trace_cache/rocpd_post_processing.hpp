fa74e17260cf62492b5a189260088e761436a2aa
Kian Cossettini <Kian.Cossettini@amd.com>
2025-09-18 07:56:46 -0400

    Remove relic function

--- projects/rocprofiler-systems/source/lib/core/trace_cache/rocpd_post_processing.hpp
+++ projects/rocprofiler-systems/source/lib/core/trace_cache/rocpd_post_processing.hpp
@@ -48,7 +48,6 @@ private:
     postprocessing_callback get_memory_copy_callback() const;
 #if(ROCPROFILER_VERSION >= 600)
     postprocessing_callback get_memory_allocate_callback() const;
-    postprocessing_callback get_ompt_callback() const;
 #endif
     postprocessing_callback get_region_callback() const;
     postprocessing_callback get_in_time_sample_callback() const;
