fb4385e142c4ae09a9e4f6e918e8c2961965a99a
Kian Cossettini <Kian.Cossettini@amd.com>
2025-09-29 14:02:39 -0400

    Part 2: Properly remove backend choices

--- projects/rocprofiler-systems/source/lib/core/argparse.cpp
+++ projects/rocprofiler-systems/source/lib/core/argparse.cpp
@@ -576,18 +576,20 @@ add_core_arguments(parser_t& _parser, parser_data& _data)
                                   "rcclp",      "amd-smi", "rocm", "mutex-locks",
                                   "spin-locks", "rw-locks" };
 
-#if !defined(ROCPROFSYS_USE_MPI) && !defined(ROCPROFSYS_USE_MPI_HEADERS)
+#if(!defined(ROCPROFSYS_USE_MPI) || ROCPROFSYS_USE_MPI == 0) &&                          \
+    (!defined(ROCPROFSYS_USE_MPI_HEADERS) || ROCPROFSYS_USE_MPI_HEADERS == 0)
     _backend_choices.erase("mpip");
 #endif
 
-#if !defined(ROCPROFSYS_USE_OMPT)
+#if !defined(ROCPROFSYS_USE_OMPT) || ROCPROFSYS_USE_OMPT == 0
     _backend_choices.erase("ompt");
 #endif
 
-#if !defined(ROCPROFSYS_USE_ROCM)
+#if !defined(ROCPROFSYS_USE_ROCM) || ROCPROFSYS_USE_ROCM == 0
     _backend_choices.erase("amd-smi");
     _backend_choices.erase("rocm");
     _backend_choices.erase("rcclp");
+    _backend_choices.erase("ompt");
 #endif
 
     if(gpu::device_count() == 0)
