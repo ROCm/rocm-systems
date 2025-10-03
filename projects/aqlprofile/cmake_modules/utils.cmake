1497dfbe3dfbc7a1e896deb5c0f166899db44ec6
Jonathan Luu <jonatluu@amd.com>
2025-10-03 12:49:32 -0400

    fix lintian error

--- projects/aqlprofile/cmake_modules/utils.cmake
+++ projects/aqlprofile/cmake_modules/utils.cmake
@@ -136,24 +136,6 @@ function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTA
             @ONLY
         )
 
-        if( BUILD_ENABLE_LINTIAN_OVERRIDES )
-            if(ENABLE_ASAN_PACKAGING)
-                string( FIND ${DEB_OVERRIDES_INSTALL_FILENM} "asan" OUT_VAR2)
-                if(OUT_VAR2 EQUAL -1)
-                    set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-asan" )
-                endif()
-            endif()
-        set( DEB_OVERRIDES_INSTALL_FILENM
-            "${DEB_OVERRIDES_INSTALL_FILENM}" CACHE STRING "Debian Package Lintian Override File Name" FORCE)
-            # Configure the changelog file
-            configure_file(
-            "${CMAKE_SOURCE_DIR}/DEBIAN/overrides.in"
-            "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}"
-        FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-            @ONLY
-            )
-        endif()
-
         # Install Change Log
         find_program ( DEB_GZIP_EXEC gzip )
         if(EXISTS "${CMAKE_BINARY_DIR}/DEBIAN/changelog.Debian" )
@@ -170,14 +152,6 @@ function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTA
             install ( FILES "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_CHANGELOG_INSTALL_FILENM}"
                     DESTINATION ${CMAKE_INSTALL_DOCDIR}
                     COMPONENT ${COMPONENT_NAME_T})
-        endif()
-
-        # Install lintian overrides
-        if( BUILD_ENABLE_LINTIAN_OVERRIDES STREQUAL "ON" AND BUILD_DEBIAN_PKGING_FLAG STREQUAL "ON")
-            set( OVERRIDE_FILE "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}" )
-            install ( FILES ${OVERRIDE_FILE}
-                DESTINATION ${DEB_OVERRIDES_INSTALL_PATH}
-                COMPONENT ${COMPONENT_NAME_T})
         endif()
       
     else()
