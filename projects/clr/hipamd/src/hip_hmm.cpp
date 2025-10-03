7009bcea2eebbd883171cc2c4929fda1175d41b5
Sourabh Betigeri <sourabhbetigeri@gmail.com>
2025-10-03 00:00:45 +0000

    SWDEV-551259 - Negative cases check

--- projects/clr/hipamd/src/hip_hmm.cpp
+++ projects/clr/hipamd/src/hip_hmm.cpp
@@ -113,7 +113,8 @@ hipError_t hipMemPrefetchAsync_v2(const void* dev_ptr, size_t count, hipMemLocat
                                   unsigned int flags, hipStream_t stream) {
   HIP_INIT_API(hipMemPrefetchAsync_v2, dev_ptr, count, location, flags, stream);
   CHECK_STREAM_CAPTURE_SUPPORTED();
-  if (flags != 0) {
+  if ((flags != 0) || (location.type == hipMemLocationTypeInvalid) ||
+      (location.type == hipMemLocationTypeNone) || (location.id < 0)) {
     HIP_RETURN(hipErrorInvalidValue);
   }
   HIP_RETURN(ihipMemPrefetchAsync(dev_ptr, count, location, stream));
@@ -140,6 +141,10 @@ hipError_t hipMemAdvise_v2(const void* dev_ptr, size_t count, hipMemoryAdvise ad
                            hipMemLocation location) {
   HIP_INIT_API(hipMemAdvise_v2, dev_ptr, count, advice, location);
   CHECK_STREAM_CAPTURE_SUPPORTED();
+  if ((location.type == hipMemLocationTypeInvalid) || (location.type == hipMemLocationTypeNone) ||
+      (location.id < 0)) {
+    HIP_RETURN(hipErrorInvalidValue);
+  }
   HIP_RETURN(ihipMemAdvise(dev_ptr, count, advice, location));
 }
 
