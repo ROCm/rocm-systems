37a1f4a3f615bec4d27e95f1783253e8ad0f27fa
ajanicijamd <Aleksandar.Janicijevic@amd.com>
2025-10-07 11:09:34 -0400

    Update projects/rocprofiler-systems/tests/generate_papi_nic_events.sh
    Co-authored-by: David Galiffi <David.Galiffi@amd.com>

--- projects/rocprofiler-systems/tests/generate_papi_nic_events.sh
+++ projects/rocprofiler-systems/tests/generate_papi_nic_events.sh
@@ -5,7 +5,8 @@
 
 # This script gets a list of default NICs from ip command
 # and generates a list of PAPI events, 4 for each NIC.
-# For example, if the NIC is enp7s0, the PAPI events are
+# and generates a list of PAPI events; 4 for each NIC.
+# For example, if the NIC is enp7s0, the PAPI events are:
 # net:::enp7s0:tx:byte net:::enp7s0:rx:byte net:::enp7s0:tx:packet net:::enp7s0:rx:packet
 
 nic_list=$(ip r | awk '/default/{print $5}')
