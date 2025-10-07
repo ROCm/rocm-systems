418f726087bba628d7061d9fbde5052dd07a6dac
geomin12 <geomin12@amd.com>
2025-10-07 15:08:04 -0700

    Removing subtrees

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -20,11 +20,9 @@ project_map = {
     "core": {
         "cmake_options": "-DTHEROCK_ENABLE_CORE=ON -DTHEROCK_ENABLE_HIP_RUNTIME=ON -DTHEROCK_ENABLE_ALL=OFF",
         "project_to_test": "hip-tests",
-        "subtree_checkout": "projects/clr\nprojects/hip\nprojects/hip-tests\nprojects/rocminfo\nprojects/rocr-runtime",
     },
     "profiler": {
         "cmake_options": "-DTHEROCK_ENABLE_PROFILER=ON -DTHEROCK_ENABLE_ALL=OFF",
         "project_to_test": "rocprofiler-tests",
-        "subtree_checkout": "projects/aqlprofile\nprojects/rocprofiler\nprojects/rocprofiler-compute\nprojects/rocprofiler-register\nprojects/rocprofiler-sdk\nprojects/rocprofiler-systems\nprojects/roctracer",
     },
 }
