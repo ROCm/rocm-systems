fadb45d5122ef138b1d3400594b5716ddc76896a
SrinivasRao Gollamandala <Srinivasarao.Gollamandala@amd.com>
2025-10-09 15:19:14 +0000

    SWDEV-548633-Added tests for hipModuleGetLoadingMode

---
+++ projects/hip-tests/catch/unit/module/hipModuleGetLoadingMode.cc
@@ -0,0 +1,110 @@
+/*
+Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANNTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER INN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR INN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+#include <hip_test_common.hh>
+#include <hip_test_defgroups.hh>
+constexpr int LEN = 64;
+constexpr auto SIZE = (LEN << 2);
+constexpr auto codeObjFile1 = "vcpy_kernel.code";
+constexpr auto kernel_name1 = "hello_world";
+constexpr auto codeObjFile2 = "copyKernel.code";
+constexpr auto kernel_name2 = "copy_ker";
+/**
+ * @addtogroup hipModuleGetLoadingMode hipModuleGetLoadingMode
+ * @{
+ * @ingroup ModuleTest
+ * `hhipError_t hipModuleGetLoadingMode(hipModuleLoadingMode_t* mode)` -
+ * Function gets the current module load mode
+ */
+
+/**
+ * Test Description
+ * ------------------------
+ * - Test case verifies the positive case of hipModuleGetLoadingMode API.
+ * Test source
+ * ------------------------
+ * - catch/unit/module/hipModuleGetLoadingMode.cc
+ * Test requirements
+ * ------------------------
+ * - HIP_VERSION >= 7.1
+ */
+TEST_CASE("Unit_hipModuleGetLoadingMode_Functional") {
+  hipModuleLoadingMode_t mode;
+  HIP_CHECK(hipModuleGetLoadingMode(&mode));
+  REQUIRE(mode == HIP_MODULE_LAZY_LOADING);  // if env var HIP_MODULE_LOADING = lazy
+  setenv("HIP_MODULE_LOADING", "EAGER", 1);
+  HIP_CHECK(hipModuleGetLoadingMode(&mode));
+  REQUIRE(mode == HIP_MODULE_EAGER_LOADING);  // if env var HIP_MODULE_LOADING = EAGER
+  unsetenv("HIP_MODULE_LOADING");
+}
+
+TEST_CASE("Unit_hipModuleGetLoadingMode_Functional2") {
+  float *Ad, *Bd;
+  std::vector<float> A(LEN, 1.0f), B(LEN, 0.0f);
+
+  HIP_CHECK(hipMalloc(&Ad, SIZE));
+  HIP_CHECK(hipMalloc(&Bd, SIZE));
+  HIP_CHECK(hipMemcpy(Ad, A.data(), SIZE, hipMemcpyHostToDevice));
+  HIP_CHECK(hipMemcpy(Bd, B.data(), SIZE, hipMemcpyHostToDevice));
+
+  hipModule_t Module;
+  hipFunction_t Function;
+  hipModuleLoadingMode_t mode;
+  HIP_CHECK(hipModuleGetLoadingMode(&mode));
+  if (mode == HIP_MODULE_EAGER_LOADING) {
+    HIP_CHECK(hipModuleLoad(&Module, codeObjFile1));
+	HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name1));
+  }
+  else if (mode == HIP_MODULE_LAZY_LOADING) {
+    HIP_CHECK(hipModuleLoad(&Module, codeObjFile2));
+	HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name2));
+  }
+    hipStream_t stream;
+    HIP_CHECK(hipStreamCreate(&stream));
+
+    struct {
+      void* _Ad;
+      void* _Bd;
+    } args;
+    args._Ad = reinterpret_cast<void*>(Ad);
+    args._Bd = reinterpret_cast<void*>(Bd);
+    size_t size = sizeof(args);
+
+    void* config[] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, &args, HIP_LAUNCH_PARAM_BUFFER_SIZE, &size,
+                    HIP_LAUNCH_PARAM_END};
+    HIP_CHECK(hipModuleLaunchKernel(Function, 1, 1, 1, LEN, 1, 1, 0, stream, NULL,
+                                  reinterpret_cast<void**>(&config)));
+
+    HIP_CHECK(hipStreamDestroy(stream));
+
+    HIP_CHECK(hipMemcpy(B.data(), Bd, SIZE, hipMemcpyDeviceToHost));
+
+    for (uint32_t i = 0; i < LEN; i++) {
+      REQUIRE(A[i] != B[i]);
+    }
+  //}
+  HIP_CHECK(hipFree(Bd));
+  HIP_CHECK(hipFree(Ad));
+  //delete[] B;
+  //delete[] A;
+  HIP_CHECK(hipModuleUnload(Module));
+}
+/**
+ * End doxygen group ModuleTest.
+ * @}
+ */
