983028032a1a2d5cfd63bd6a9121a9dc0d9382c4
Yang Wang <ywang103@amd.com>
2025-10-15 15:56:55 -0500

    add --kernel-trace for rocprofiler-sdk mode

--- projects/rocprofiler-compute/src/utils/utils.py
+++ projects/rocprofiler-compute/src/utils/utils.py
@@ -974,6 +974,7 @@ def pc_sampling_prof(
             "ROCPROF_PC_SAMPLING_UNIT": unit,
             "ROCPROF_PC_SAMPLING_INTERVAL": str(interval),
             "ROCPROF_PC_SAMPLING_METHOD": method,
+            "ROCPROF_KERNEL_TRACE": "1",
         }
         new_env = os.environ.copy()
         for key, value in options.items():
@@ -985,12 +986,12 @@ def pc_sampling_prof(
         )
     else:
         options = [
+            "--kernel-trace",
             "--pc-sampling-beta-enabled",
             "--pc-sampling-method",
             method,
             "--pc-sampling-unit",
             unit,
-            "--kernel-trace",
             "--output-format",
             "csv",
             "json",
