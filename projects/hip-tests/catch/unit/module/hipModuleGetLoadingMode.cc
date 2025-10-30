d4f49757a9d120e6a6fb6df68329006dc05f57fd
SrinivasRao Gollamandala <Srinivasarao.Gollamandala@amd.com>
2025-10-30 12:17:58 +0000

    SWDEV-548633-Updated the code as per review comments

--- projects/hip-tests/catch/unit/module/hipModuleGetLoadingMode.cc
+++ projects/hip-tests/catch/unit/module/hipModuleGetLoadingMode.cc
@@ -41,19 +41,32 @@ constexpr auto kernel_name2 = "copy_ker";
  * - catch/unit/module/hipModuleGetLoadingMode.cc
  * Test requirements
  * ------------------------
- * - HIP_VERSION >= 7.1
+ * - HIP_VERSION >= 7.2
  */
-TEST_CASE("Unit_hipModuleGetLoadingMode_Functional") {
+TEST_CASE("Unit_hipModuleGetLoadingMode_UnitTest_1") {
   hipModuleLoadingMode_t mode;
-  HIP_CHECK(hipModuleGetLoadingMode(&mode));
-  REQUIRE(mode == HIP_MODULE_LAZY_LOADING);  // if env var HIP_MODULE_LOADING = lazy
-  setenv("HIP_MODULE_LOADING", "EAGER", 1);
-  HIP_CHECK(hipModuleGetLoadingMode(&mode));
-  REQUIRE(mode == HIP_MODULE_EAGER_LOADING);  // if env var HIP_MODULE_LOADING = EAGER
+  if (setenv("HIP_MODULE_LOADING", "EAGER", 1) != 0) {
+    HIP_CHECK(hipModuleGetLoadingMode(&mode));
+    REQUIRE(mode == HIP_MODULE_LAZY_LOADING); // Default mode is lazy mode
+  } else {
+    HIP_CHECK(hipModuleGetLoadingMode(&mode));
+    REQUIRE(mode ==
+            HIP_MODULE_EAGER_LOADING); // if env var HIP_MODULE_LOADING = EAGER
+  }
   unsetenv("HIP_MODULE_LOADING");
 }
-
-TEST_CASE("Unit_hipModuleGetLoadingMode_Functional2") {
+/**
+ * Test Description
+ * ------------------------
+ * - Test case verifies the positive case with kernel Launch.
+ * Test source
+ * ------------------------
+ * - catch/unit/module/hipModuleGetLoadingMode.cc
+ * Test requirements
+ * ------------------------
+ * - HIP_VERSION >= 7.2
+ */
+TEST_CASE("Unit_hipModuleGetLoadingMode_UnitTest_2") {
   float *Ad, *Bd;
   std::vector<float> A(LEN, 1.0f), B(LEN, 0.0f);
 
@@ -63,59 +76,64 @@ TEST_CASE("Unit_hipModuleGetLoadingMode_Functional2") {
   HIP_CHECK(hipMemcpy(Bd, B.data(), SIZE, hipMemcpyHostToDevice));
 
   hipModule_t Module;
-  hipFunction_t Function;
+  hipFunction_t Function = nullptr;
   hipModuleLoadingMode_t mode;
   HIP_CHECK(hipModuleGetLoadingMode(&mode));
   if (mode == HIP_MODULE_EAGER_LOADING) {
     HIP_CHECK(hipModuleLoad(&Module, codeObjFile1));
-	HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name1));
-  }
-  else if (mode == HIP_MODULE_LAZY_LOADING) {
+    HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name1));
+  } else if (mode == HIP_MODULE_LAZY_LOADING) {
     HIP_CHECK(hipModuleLoad(&Module, codeObjFile2));
-	HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name2));
+    HIP_CHECK(hipModuleGetFunction(&Function, Module, kernel_name2));
   }
-    hipStream_t stream;
-    HIP_CHECK(hipStreamCreate(&stream));
+  hipStream_t stream;
+  HIP_CHECK(hipStreamCreate(&stream));
 
-    struct {
-      void* _Ad;
-      void* _Bd;
-    } args;
-    args._Ad = reinterpret_cast<void*>(Ad);
-    args._Bd = reinterpret_cast<void*>(Bd);
-    size_t size = sizeof(args);
+  struct {
+    void *_Ad;
+    void *_Bd;
+  } args;
+  args._Ad = reinterpret_cast<void *>(Ad);
+  args._Bd = reinterpret_cast<void *>(Bd);
+  size_t size = sizeof(args);
 
-    void* config[] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, &args, HIP_LAUNCH_PARAM_BUFFER_SIZE, &size,
-                    HIP_LAUNCH_PARAM_END};
-    HIP_CHECK(hipModuleLaunchKernel(Function, 1, 1, 1, LEN, 1, 1, 0, stream, NULL,
-                                  reinterpret_cast<void**>(&config)));
+  void *config[] = {HIP_LAUNCH_PARAM_BUFFER_POINTER, &args,
+                    HIP_LAUNCH_PARAM_BUFFER_SIZE, &size, HIP_LAUNCH_PARAM_END};
+  HIP_CHECK(hipModuleLaunchKernel(Function, 1, 1, 1, LEN, 1, 1, 0, stream, NULL,
+                                  reinterpret_cast<void **>(&config)));
 
-    HIP_CHECK(hipStreamDestroy(stream));
+  HIP_CHECK(hipStreamDestroy(stream));
 
-    HIP_CHECK(hipMemcpy(B.data(), Bd, SIZE, hipMemcpyDeviceToHost));
+  HIP_CHECK(hipMemcpy(B.data(), Bd, SIZE, hipMemcpyDeviceToHost));
 
-    for (uint32_t i = 0; i < LEN; i++) {
-      REQUIRE(A[i] != B[i]);
-    }
-  //}
+  for (uint32_t i = 0; i < LEN; i++) {
+    REQUIRE(A[i] != B[i]);
+  }
   HIP_CHECK(hipFree(Bd));
   HIP_CHECK(hipFree(Ad));
-  //delete[] B;
-  //delete[] A;
   HIP_CHECK(hipModuleUnload(Module));
 }
 
-void setMode() {
-  setenv("HIP_MODULE_LOADING", "EAGER", 1);
-}
+void setMode() { setenv("HIP_MODULE_LOADING", "EAGER", 1); }
 
-void ChkMode(){
+void ChkMode() {
   hipModuleLoadingMode_t mode;
   HIP_CHECK(hipModuleGetLoadingMode(&mode));
   REQUIRE(mode == HIP_MODULE_EAGER_LOADING);
   unsetenv("HIP_MODULE_LOADING");
 }
-
+/**
+ * Test Description
+ * ------------------------
+ * - Test case verifies the multithread Scenario.
+ * - Set mode Env in one thread. Verify mode in another thread.
+ * Test source
+ * ------------------------
+ * - catch/unit/module/hipModuleGetLoadingMode.cc
+ * Test requirements
+ * ------------------------
+ * - HIP_VERSION >= 7.2
+ */
 TEST_CASE("Unit_hipModuleGetLoadingMode_MultiThread") {
   // Create Thraed one.
   std::thread t1(setMode);
