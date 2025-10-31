c1e782ee1d740d5e11eca0cb49f3a3c3e74d3e8e
Aleksei Tumakaev <atumakae@amd.com>
2025-10-31 14:40:04 +0000

    Remove temporary crash fix

--- projects/rocprofiler-sdk/source/lib/python/rocpd/source/perfetto.cpp
+++ projects/rocprofiler-sdk/source/lib/python/rocpd/source/perfetto.cpp
@@ -465,7 +465,6 @@ write_perfetto(
                 auto  _name      = itr.name;
                 auto  _operation = itr.name;
 
-                /* Temporary crash fix
                 if(itr.has_extdata())
                 {
                     auto _extdata = itr.get_extdata();
@@ -474,7 +473,6 @@ write_perfetto(
                     if(_extdata.operation && !_extdata.operation->empty())
                         _operation = _extdata.operation.value();
                 }
-                */
 
                 const char* category_name = get_category_string(itr.category);
 
