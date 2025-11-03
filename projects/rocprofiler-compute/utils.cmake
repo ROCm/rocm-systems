1099e6fc2707bc734638d532f4787d60ab0dc53d
Jonathan Luu <jonatluu@amd.com>
2025-11-03 14:51:06 -0500

    fix formatting use existing changelog if provided

--- projects/rocprofiler-compute/utils.cmake
+++ projects/rocprofiler-compute/utils.cmake
@@ -24,34 +24,34 @@ function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTA
 
       # Configure the changelog file
       configure_file(
-        "${CMAKE_SOURCE_DIR}/DEBIAN/changelog.in"
+	      "${CMAKE_SOURCE_DIR}/CHANGELOG.md"
         "${CMAKE_BINARY_DIR}/DEBIAN/changelog.Debian"
         @ONLY
       )
 
       if( BUILD_ENABLE_LINTIAN_OVERRIDES )
-	if(DEFINED BUILD_SHARED_LIBS AND NOT ${BUILD_SHARED_LIBS} STREQUAL "")
-	  string(FIND ${DEB_OVERRIDES_INSTALL_FILENM} "static" OUT_VAR1)
-	  if(OUT_VAR1 EQUAL -1)
-	    set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-static" )
+        if(DEFINED BUILD_SHARED_LIBS AND NOT ${BUILD_SHARED_LIBS} STREQUAL "")
+          string(FIND ${DEB_OVERRIDES_INSTALL_FILENM} "static" OUT_VAR1)
+          if(OUT_VAR1 EQUAL -1)
+            set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-static" )
           endif()
-	else()
+        else()
           if(ENABLE_ASAN_PACKAGING)
-	    string( FIND ${DEB_OVERRIDES_INSTALL_FILENM} "asan" OUT_VAR2)
-	    if(OUT_VAR2 EQUAL -1)
-	      set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-asan" )
-	    endif()
+            string( FIND ${DEB_OVERRIDES_INSTALL_FILENM} "asan" OUT_VAR2)
+            if(OUT_VAR2 EQUAL -1)
+              set( DEB_OVERRIDES_INSTALL_FILENM "${DEB_OVERRIDES_INSTALL_FILENM}-asan" )
+            endif()
           endif()
-	endif()
-	set( DEB_OVERRIDES_INSTALL_FILENM
-		"${DEB_OVERRIDES_INSTALL_FILENM}" CACHE STRING "Debian Package Lintian Override File Name" FORCE)
-        # Configure the changelog file
-        configure_file(
-          "${CMAKE_SOURCE_DIR}/DEBIAN/overrides.in"
-          "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}"
-	   FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
-          @ONLY
-        )
+        endif()
+        set( DEB_OVERRIDES_INSTALL_FILENM
+          "${DEB_OVERRIDES_INSTALL_FILENM}" CACHE STRING "Debian Package Lintian Override File Name" FORCE)
+              # Configure the changelog file
+              configure_file(
+                "${CMAKE_SOURCE_DIR}/DEBIAN/overrides.in"
+                "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_OVERRIDES_INSTALL_FILENM}"
+          FILE_PERMISSIONS OWNER_READ OWNER_WRITE GROUP_READ WORLD_READ
+                @ONLY
+              )
       endif()
 
       # Install Change Log 
