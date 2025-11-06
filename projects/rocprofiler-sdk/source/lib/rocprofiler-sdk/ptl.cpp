901e7cae32f7da7921d8cf05ad3d08171c01876c
Mythreya <mythreya.kuricheti@amd.com>
2025-11-06 22:59:28 +0000

    Misc fixes

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/ptl.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/ptl.cpp
@@ -24,6 +24,7 @@
 #include "lib/rocprofiler-sdk/counters/ioctl.hpp"
 #include "lib/rocprofiler-sdk/details/kfd_ioctl.h"
 #include "lib/rocprofiler-sdk/pc_sampling/ioctl/ioctl_adapter.hpp"
+#include "rocprofiler-sdk/fwd.h"
 
 #include <fmt/core.h>
 #include <sys/ioctl.h>
@@ -66,7 +67,7 @@ get_num_sample_infos(int gpu_id)
         check_ioctl(pc_sampling::ioctl::get_kfd_fd(), AMDKFD_IOC_PC_SAMPLE, &args);
     if(err != 0)
     {
-        ROCP_WARNING << fmt::format("PTL: Querying sample infos for GPU %d failed: errno=%d (%s)",
+        ROCP_WARNING << fmt::format("PTL: Querying sample infos for GPU {:5} failed: errno={} ({})",
                                     gpu_id,
                                     err,
                                     std::strerror(err));
@@ -103,13 +104,13 @@ create_pc_sampling_session(int gpu_id, int ptl_state, int format1, int format2)
     if(err != 0)
     {
         ROCP_WARNING << fmt::format(
-            "PTL: PC Sampling session create for GPU %d failed: errno=%d (%s)",
+            "PTL: PC Sampling session create for GPU {:5} failed: errno={} ({})",
             gpu_id,
             std::strerror(err));
         return -1;
     }
 
-    ROCP_INFO << fmt::format("Created PC Sampling session with trace ID: %d", args.trace_id);
+    ROCP_INFO << fmt::format("Created PC Sampling session with trace ID: {}", args.trace_id);
 
     return args.trace_id;
 }
@@ -128,18 +129,20 @@ destroy_pc_sampling_session(int gpu_id, int trace_id)
         check_ioctl(pc_sampling::ioctl::get_kfd_fd(), AMDKFD_IOC_PC_SAMPLE, &args);
 
     ROCP_WARNING_IF(err != 0) << fmt::format(
-        "PTL: Could not destroy PC Sampling session ID %d: errno=%d (%s)",
+        "PTL: Could not destroy PC Sampling session ID {}: errno={} ({})",
         args.trace_id,
         err,
         std::strerror(err));
 
-    ROCP_INFO << fmt::format("PTL: Destroyed PC Sampling session with trace ID: %d", args.trace_id);
+    ROCP_INFO << fmt::format("PTL: Destroyed PC Sampling session with trace ID: {}", args.trace_id);
 }
 
 int
 enable_ptl(int gpu_id, int format1, int format2)
 {
     const auto trace_id = create_pc_sampling_session(gpu_id, 0, format1, format2);
+    ROCP_INFO << fmt::format(
+        "PTL: Enabling PTL for GPU {:5} (format1={}, format2={})", gpu_id, format1, format2);
 
     // could not create a session
     if(trace_id == -1) return -1;
@@ -160,10 +163,10 @@ enable_ptl(int gpu_id, int format1, int format2)
         check_ioctl(pc_sampling::ioctl::get_kfd_fd(), AMDKFD_IOC_PC_SAMPLE, &args);
 
     ROCP_WARNING_IF(err != 0) << fmt::format(
-        "PTL: Could not enable PTL for GPU %d: errno=%d (%s)\n", gpu_id, err, std::strerror(err));
+        "PTL: Could not enable PTL for GPU {:5}: errno={} ({})\n", gpu_id, err, std::strerror(err));
 
     ROCP_INFO_IF(err == 0) << fmt::format(
-        "PTL: PTL state set for GPU %d with format1=%d, format2=%d\n", gpu_id, format1, format2);
+        "PTL: PTL state set for GPU {:5} with format1={}, format2={}\n", gpu_id, format1, format2);
 
     destroy_pc_sampling_session(gpu_id, trace_id);
 
@@ -175,6 +178,8 @@ disable_ptl(int gpu_id)
 {
     // Maybe KFD issue. Need to set this to 1 and then disable, create with 0 does not seem to work
     const auto trace_id = create_pc_sampling_session(gpu_id, 1, 0, 0);
+
+    ROCP_INFO << fmt::format("PTL: Disabling PTL for GPU {:5}", gpu_id);
 
     // could not create a session
     if(trace_id == -1) return -1;
@@ -190,8 +195,8 @@ disable_ptl(int gpu_id)
         check_ioctl(pc_sampling::ioctl::get_kfd_fd(), AMDKFD_IOC_PC_SAMPLE, &args);
 
     ROCP_WARNING_IF(err != 0) << fmt::format(
-        "PTL: Disable PTL failed on GPU %d. Counter collection may "
-        "be unavailable: errno=%d (%s): %s\n",
+        "PTL: Disable PTL failed on GPU {:5}. Counter collection may "
+        "be inaccurate or degraded: errno={} ({}): {}\n",
         gpu_id,
         err,
         std::strerror(err));
@@ -201,7 +206,7 @@ disable_ptl(int gpu_id)
     return err;
 }
 
-static constexpr uint32_t GPU_PCIE_DID = 0x74a2;
+constexpr uint32_t GPU_PCIE_DID = 0x74a2;
 
 bool
 enable_ptl(const rocprofiler_agent_t* agent, uint32_t format1, uint32_t format2)
@@ -234,7 +239,12 @@ disable_ptl(const rocprofiler_agent_t* agent)
 agent_ptl_state
 get_agent_ptl_state(const rocprofiler_agent_t* rocp_agent)
 {
-    const auto gpu_id           = rocp_agent->gpu_id;
+    const auto& agent  = *rocp_agent;
+    const auto  gpu_id = agent.gpu_id;
+
+    ROCP_INFO << fmt::format(
+        "PTL: Querying PTL state for GPU {:5} (node_id: {})", agent.gpu_id, agent.node_id);
+
     const auto num_sample_infos = get_num_sample_infos(gpu_id);
 
     std::vector<kfd_pc_sample_info> samples(num_sample_infos);
@@ -256,14 +266,14 @@ get_agent_ptl_state(const rocprofiler_agent_t* rocp_agent)
         check_ioctl(pc_sampling::ioctl::get_kfd_fd(), AMDKFD_IOC_PC_SAMPLE, &args);
 
     ROCP_WARNING_IF(ret != 0) << fmt::format(
-        "PTL: Failed to query PTL state for GPU %d: errno=%d (%s)",
+        "PTL: Failed to query PTL state for GPU {:5}: errno={} ({})",
         gpu_id,
         err,
         std::strerror(err));
 
     for(int i = 0; i < num_sample_infos; ++i)
     {
-        ROCP_TRACE << fmt::format("PTL:  Sample [%d]:\n", i);
+        ROCP_TRACE << fmt::format("PTL:  Sample [{}]:\n", i);
         ROCP_TRACE << fmt::format("PTL:      ptl_state:    %u\n", samples[i].ptl_state);
         ROCP_TRACE << fmt::format("PTL:      pref_format1: %u\n", samples[i].pref_format1);
         ROCP_TRACE << fmt::format("PTL:      pref_format2: %u\n", samples[i].pref_format2);
@@ -276,35 +286,24 @@ get_agent_ptl_state(const rocprofiler_agent_t* rocp_agent)
         .agent        = rocp_agent,
         .ptl_enabled  = samples[0].ptl_state == 1,
         .pref_format1 = samples[0].pref_format1,
-        .perf_format2 = samples[0].pref_format2,
+        .pref_format2 = samples[0].pref_format2,
     };
 }
 
-void
-restore_ptl_state(const agent_ptl_state& prev_state)
-{
-    if(prev_state.agent == nullptr)
-    {
-        ROCP_ERROR << "PTL: Agent cannot be null";
-    }
-
-    // enable ptl if it was enabled before we disabled it
-    if(prev_state.ptl_enabled)
-    {
-        enable_ptl(prev_state.agent, prev_state.pref_format1, prev_state.perf_format2);
-    }
-}
-
-std::vector<ptl::agent_ptl_state>*
+const std::vector<ptl::agent_ptl_state>*
 get_ptl_cache()
 {
     static auto*& _v = common::static_object<std::vector<ptl::agent_ptl_state>>::construct([]() {
         std::vector<ptl::agent_ptl_state> prev_states{};
         for(const auto& agent : agent::get_agents())
         {
-            prev_states.emplace_back(ptl::get_agent_ptl_state(agent));
+            if(agent->type == ROCPROFILER_AGENT_TYPE_GPU)
+            {
+                prev_states.emplace_back(ptl::get_agent_ptl_state(agent));
+            }
         }
-    });
+        return prev_states;
+    }());
     return _v;
 }
 }  // namespace
