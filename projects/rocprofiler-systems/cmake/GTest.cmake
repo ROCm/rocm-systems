6b825735a9c21a7c72681bb1f0cbf8b0fe98809a
Aleksandar Djordjevic <adjordje@amd.com>
2025-11-10 13:06:15 +0100

    googletest project set up

---
+++ projects/rocprofiler-systems/cmake/GTest.cmake
@@ -0,0 +1,126 @@
+# MIT License
+#
+# Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+include_guard(GLOBAL)
+
+if(ROCPROFILER_BUILD_GTEST)
+    message(STATUS "Setting up GTEST to build from source!")
+
+    rocprofiler_systems_checkout_git_submodule(
+        RELATIVE_PATH    external/googletest
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        TEST_FILE        CMakeLists.txt
+        REPO_URL         https://github.com/google/googletest.git
+        REPO_BRANCH      "v1.17.0"
+    )
+
+    set(_GTEST_INSTALL_DIR ${PROJECT_BINARY_DIR}/external/googletest/install)
+
+    # Configure GoogleTest
+    execute_process(
+        COMMAND
+            ${CMAKE_COMMAND} -B ${PROJECT_BINARY_DIR}/external/googletest/build -G
+            ${CMAKE_GENERATOR} -S ${PROJECT_SOURCE_DIR}/external/googletest
+            -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+            -DCMAKE_INSTALL_PREFIX=${_GTEST_INSTALL_DIR}
+            -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}
+            -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DBUILD_GMOCK=ON -DINSTALL_GTEST=ON
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/external/googletest
+        RESULT_VARIABLE _GTEST_CONFIGURE_RET
+        ERROR_VARIABLE _GTEST_CONFIGURE_ERR
+        OUTPUT_QUIET
+    )
+
+    if(NOT _GTEST_CONFIGURE_RET EQUAL 0)
+        message(FATAL_ERROR "Failed to configure GoogleTest: ${_GTEST_CONFIGURE_ERR}")
+    endif()
+    message(STATUS "GoogleTest configured successfully!")
+
+    set(_GTEST_LIB_DIR "${_GTEST_INSTALL_DIR}/lib")
+
+    # Build and install GoogleTest
+    add_custom_target(
+        rocprofiler-systems-googletest-library-build
+        ALL
+        COMMAND
+            ${CMAKE_COMMAND} --build ${PROJECT_BINARY_DIR}/external/googletest/build
+            --config ${CMAKE_BUILD_TYPE}
+        COMMAND
+            ${CMAKE_COMMAND} --install ${PROJECT_BINARY_DIR}/external/googletest/build
+            --config ${CMAKE_BUILD_TYPE}
+        WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/external/googletest/build
+        COMMENT "Building and installing GoogleTest"
+        BYPRODUCTS
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgtest.a
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgtest_main.a
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgmock.a
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgmock_main.a
+    )
+
+    # Create interface library
+    add_library(rocprofiler-systems-googletest-library INTERFACE)
+    add_dependencies(
+        rocprofiler-systems-googletest-library
+        rocprofiler-systems-googletest-library-build
+    )
+
+    target_include_directories(
+        rocprofiler-systems-googletest-library
+        SYSTEM
+        INTERFACE ${PROJECT_BINARY_DIR}/external/googletest/install/include
+    )
+
+    target_link_libraries(
+        rocprofiler-systems-googletest-library
+        INTERFACE
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgtest.a
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgtest_main.a
+            ${PROJECT_BINARY_DIR}/external/googletest/install/lib/libgmock.a
+    )
+
+    # GoogleTest requires threading support
+    find_package(Threads REQUIRED)
+    target_link_libraries(
+        rocprofiler-systems-googletest-library
+        INTERFACE Threads::Threads
+    )
+
+    message(STATUS "GoogleTest install directory: ${_GTEST_INSTALL_DIR}")
+else()
+    message(STATUS "Using system GTest library")
+    find_package(GTest REQUIRED)
+    add_library(rocprofiler-systems-googletest-library INTERFACE)
+
+    # Link against system GTest targets
+    target_link_libraries(
+        rocprofiler-systems-googletest-library
+        INTERFACE GTest::gtest GTest::gtest_main
+    )
+
+    # Also link gmock if available
+    if(TARGET GTest::gmock)
+        target_link_libraries(
+            rocprofiler-systems-googletest-library
+            INTERFACE GTest::gmock
+        )
+    endif()
+endif()
