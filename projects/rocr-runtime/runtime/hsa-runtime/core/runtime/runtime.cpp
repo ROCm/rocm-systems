5f7215cdd8e25b55700b68a2da4d6688506f3b83
Alysa.Liu@amd.com <Alysa.Liu@amd.com>
2025-11-10 13:55:50 -0500

    rocr: Fix signal memleak
    Fix signal memleak.

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
@@ -150,7 +150,7 @@ hsa_status_t Runtime::Release() {
 
   if (runtime_singleton_ == nullptr) return HSA_STATUS_ERROR_NOT_INITIALIZED;
 
-  if (runtime_singleton_->ref_count_-- == 1) {
+  if (runtime_singleton_->ref_count_ == 1) {
     auto system_event_handlers = runtime_singleton_->GetSystemEventHandlers();
 
     if (!system_event_handlers.empty()) {
@@ -165,6 +165,8 @@ hsa_status_t Runtime::Release() {
     // Release all registered memory, then unload backends
     runtime_singleton_->Unload();
   }
+
+  runtime_singleton_->ref_count_--;
 
   if (runtime_singleton_->ref_count_ == 0) {
     delete runtime_singleton_;
