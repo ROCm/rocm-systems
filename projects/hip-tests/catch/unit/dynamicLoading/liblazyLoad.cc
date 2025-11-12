72fff0cd04fffe540abab1e4179c7e1ca07a0275
Jatin Chaudhary <jatchaud@amd.com>
2025-11-12 14:27:52 +0000

    More fixes, change compiler to HIP for standalone builds, simplify lazy load

--- projects/hip-tests/catch/unit/dynamicLoading/liblazyLoad.cc
+++ projects/hip-tests/catch/unit/dynamicLoading/liblazyLoad.cc
@@ -32,8 +32,8 @@ THE SOFTWARE.
   }
 
 __global__ static void addition(float* C, float* A, float* B, size_t N) {
-  size_t offset = hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x;
-  size_t stride = hipBlockDim_x * hipGridDim_x;
+  size_t offset = blockIdx.x * blockDim.x + threadIdx.x;
+  size_t stride = blockDim.x * gridDim.x;
 
   for (size_t i = offset; i < N; i += stride) {
     C[i] = A[i] + B[i];
