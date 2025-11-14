ed429a6092a3140ff62b4a54e5f34026e9fde60b
Venkateshwar Reddy Kandula <Venkateshwarreddy.Kandula@amd.com>
2025-11-14 14:57:15 -0600

    Update reproducible-runtime.cpp

--- projects/rocprofiler-sdk/tests/bin/reproducible-runtime/reproducible-runtime.cpp
+++ projects/rocprofiler-sdk/tests/bin/reproducible-runtime/reproducible-runtime.cpp
@@ -175,7 +175,7 @@ run(int tid, int devid)
 
     roctxRangeStop(roctx_range_id);
 
-    constexpr auto scale = 1.3;
+    constexpr auto scale = 1.4;
     if(time > scale * nruntime)
     {
         auto _msg = std::stringstream{};
