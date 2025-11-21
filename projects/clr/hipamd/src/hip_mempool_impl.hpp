6cf9a51456ac51f740c5d461ad5c8193e53e9ef2
Honglei Huang <honghuan@amd.com>
2025-11-21 13:55:51 +0800

    tmp fix for virgl us
    Signed-off-by: Honglei Huang <honghuan@amd.com>

--- projects/clr/hipamd/src/hip_mempool_impl.hpp
+++ projects/clr/hipamd/src/hip_mempool_impl.hpp
@@ -240,7 +240,7 @@ class MemoryPool : public amd::ReferenceCountedObject, amd::VmHeapArray {
     }
     state_.interprocess_ = properties_.handleTypes != hipMemHandleTypeNone;
     // Check if VM heap can be enabled
-    if (DEBUG_HIP_MEM_POOL_VMHEAP && !state_.phys_mem_ && !state_.interprocess_) {
+    if (0) {
       state_.use_vm_heap_ = true;
       busy_heap_.EnableVmHeap();
       free_heap_.EnableVmHeap();
