ea941a657291f245f545d4d3a224b44d2864e5a8
jbonnell-amd <jason.bonnell@amd.com>
2025-11-24 09:56:29 -0500

    Address PR feedback

--- projects/rocprofiler-compute/tools/run-ci.py
+++ projects/rocprofiler-compute/tools/run-ci.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-
+##############################################################################
 # MIT License
 #
-# Copyright (c) 2025 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (c) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -21,6 +21,7 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
+##############################################################################
 
 import argparse
 import glob
