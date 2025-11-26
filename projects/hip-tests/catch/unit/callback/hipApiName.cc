34b86c6d9e2da8847d25153324340f4bb31ecf24
sourabhuday <sourabhbetigeri@gmail.com>
2025-11-26 13:53:46 -0800

    Add category tags to HIP unit tests - Part 6: Interop & Advanced

--- projects/hip-tests/catch/unit/callback/hipApiName.cc
+++ projects/hip-tests/catch/unit/callback/hipApiName.cc
@@ -45,7 +45,7 @@ const uint32_t kApiNumber{1024};
  *  - HIP_VERSION >= 5.2
  *  - Platform specific (AMD)
  */
-TEST_CASE("Unit_hipApiName_Positive_Basic") {
+TEST_CASE("Unit_hipApiName_Positive_Basic", "[callback]") {
   std::vector<std::string> hip_api_names;
 
   for (uint32_t i = 0; i < kApiNumber; ++i) {
