61b2937dcdc770277837ee985e8d23309247d361
Vignesh Edithal <Vignesh.Edithal@amd.com>
2025-11-27 08:36:21 -0500

    fix format

--- projects/rocprofiler-compute/src/utils/amdsmi_interface.py
+++ projects/rocprofiler-compute/src/utils/amdsmi_interface.py
@@ -38,7 +38,7 @@ _amdsmi_module = None
 
 
 # Ignore undefined name amdsmi since it's dynamically imported
-def import_amdsmi_module() -> "amdsmi": # noqa: F821
+def import_amdsmi_module() -> "amdsmi":  # noqa: F821
     """
     Dynamically import the amdsmi module because we only
     want profile time dependency on amdsmi.
