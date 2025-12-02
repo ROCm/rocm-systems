b01f697578779a2fd6a9375ecc57bc7becc28d28
Mythreya <mythreya.kuricheti@amd.com>
2025-12-02 18:29:31 +0000

    code review

--- projects/aqlprofile/src/core/pm4_factory.cpp
+++ projects/aqlprofile/src/core/pm4_factory.cpp
@@ -63,20 +63,19 @@ aqlprofile_agent_handle_t RegisterAgent(const aqlprofile_agent_info_v1_t* agent_
 
   aqlprofile_agent_handle_t agent_id{};
   AgentInfo int_agent_info{};
+  static_assert(sizeof(int_agent_info.name) == 64);
 
-  if (agent_info->size == 40) {
-    int_agent_info.cu_num = agent_info->cu_num;
-    int_agent_info.se_num = agent_info->se_num;
-    int_agent_info.xcc_num = agent_info->xcc_num;
-    int_agent_info.shader_arrays_per_se = agent_info->shader_arrays_per_se;
-    int_agent_info.domain = agent_info->domain;
-    int_agent_info.bdf_id = agent_info->location_id;
-  } else {
+  if (agent_info->size != 40) {
     ERR_LOGGING << "Unsupported agent_info size " << agent_info->size;
     throw std::runtime_error("Unsupported agent_info size");
   }
 
-  static_assert(sizeof(int_agent_info.name) == 64);
+  int_agent_info.cu_num = agent_info->cu_num;
+  int_agent_info.se_num = agent_info->se_num;
+  int_agent_info.xcc_num = agent_info->xcc_num;
+  int_agent_info.shader_arrays_per_se = agent_info->shader_arrays_per_se;
+  int_agent_info.domain = agent_info->domain;
+  int_agent_info.bdf_id = agent_info->location_id;
 
   if (agent_info->agent_gfxip == nullptr) {
     constexpr auto name_null =
