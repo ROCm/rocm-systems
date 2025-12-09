88d88ac2f0ce760f2805b6e4ea433e211f40563e
Fábio Mestre <fabio.mestre@amd.com>
2025-12-09 10:28:56 -0600

    Do not declare ocml attributes when using gcc

--- projects/clr/hipamd/include/hip/amd_detail/amd_hip_bf16.h
+++ projects/clr/hipamd/include/hip/amd_detail/amd_hip_bf16.h
@@ -111,9 +111,8 @@
 #if !defined(__HIPCC_RTC__)
 #include <hip/amd_detail/amd_hip_common.h>
 #include "amd_hip_vector_types.h"  // float2 etc
-#include "device_library_decls.h"  // ocml conversion functions
-#include "math_fwd.h"              // ocml device functions
 #if defined(__clang__) && defined(__HIP__)
+#include "math_fwd.h"              // ocml device functions
 #include <hip/amd_detail/amd_warp_functions.h>       // define warpSize
 #include <hip/amd_detail/amd_warp_sync_functions.h>  // Sync functions
 #endif
