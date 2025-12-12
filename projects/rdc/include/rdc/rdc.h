e18ad9c244179c473ca4a407c1afec0500ea8686
yalmusaf_amdeng <Yazen.ALMusaffar@amd.com>
2025-12-12 11:59:46 -0600

    comment update for wrong units associted with RDC_FI_GPU_MEMORY_CUR_BANDWIDTH
    Signed-off-by: yalmusaf_amdeng <Yazen.ALMusaffar@amd.com>

--- projects/rdc/include/rdc/rdc.h
+++ projects/rdc/include/rdc/rdc.h
@@ -209,7 +209,7 @@ typedef enum {
   RDC_FI_GPU_MEMORY_ACTIVITY,       //!< Memory busy percentage
   RDC_FI_GPU_MEMORY_MAX_BANDWIDTH,  //<! The Memory max bandwidth at current memory clock in
                                     // Mb/Second
-  RDC_FI_GPU_MEMORY_CUR_BANDWIDTH,  //<! The Memory current bandwidth in Mb/Second
+  RDC_FI_GPU_MEMORY_CUR_BANDWIDTH,  //<! The Memory current bandwidth in GB/Second
   RDC_FI_GPU_BUSY_PERCENT,          //<! The GPU busy percentage
 
   /**
