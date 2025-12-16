994f6b491e65c64a233c7a827c674a69d915f113
Aleksandar Djordjevic <aleksandar.djordjevic@amd.com>
2025-12-16 13:38:47 +0100

    [rocprofiler-systems] Add span (#2142)
    * Add span
    * Update unit tests

--- projects/rocprofiler-systems/source/lib/common/span.hpp
+++ projects/rocprofiler-systems/source/lib/common/span.hpp
@@ -5,40 +5,61 @@
 
 namespace rocprofsys
 {
-namespace common
+inline namespace common
 {
 
 template <typename T>
 struct span
 {
-    span(T* data, size_t size)
+    using value_type = T;
+
+    constexpr span(T* data, size_t size) noexcept
     : m_data(data)
     , m_size(size)
     {}
 
-    span(std::vector<T>& vec)
+    constexpr span(std::vector<T>& vec) noexcept
     : m_data(vec.data())
     , m_size(vec.size())
     {}
 
     template <size_t N>
-    span(const std::array<T, N>& arr)
+    constexpr span(std::array<T, N>& arr) noexcept
     : m_data(arr.data())
     , m_size(arr.size())
     {}
 
-    T*       data() const { return m_data; }
-    T*       data() { return m_data; }
-    T*       begin() { return m_data; }
-    T*       end() { return m_data + m_size; }
-    size_t   size() const { return m_size; }
-    bool     empty() const { return m_size == 0; }
-    T&       operator[](size_t index) { return m_data[index]; }
-    const T& operator[](size_t index) const { return m_data[index]; }
+    constexpr inline __attribute__((always_inline)) T* data() const noexcept
+    {
+        return m_data;
+    }
+    constexpr inline __attribute__((always_inline)) T* data() noexcept { return m_data; }
+    constexpr inline __attribute__((always_inline)) T* begin() noexcept { return m_data; }
+    constexpr inline __attribute__((always_inline)) T* end() noexcept
+    {
+        return m_data + m_size;
+    }
+    constexpr inline __attribute__((always_inline)) size_t size() const noexcept
+    {
+        return m_size;
+    }
+    constexpr inline __attribute__((always_inline)) bool empty() const noexcept
+    {
+        return m_size == 0;
+    }
+    constexpr inline __attribute__((always_inline)) T& operator[](size_t index) noexcept
+    {
+        return m_data[index];
+    }
+    constexpr inline __attribute__((always_inline)) const T& operator[](
+        size_t index) const noexcept
+    {
+        return m_data[index];
+    }
 
 private:
-    T*     m_data;
-    size_t m_size;
+    T*     m_data = nullptr;
+    size_t m_size = 0;
 };
 
 }  // namespace common
