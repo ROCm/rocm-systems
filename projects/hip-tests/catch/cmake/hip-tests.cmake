7935ad509817c0bd119993432495bfb6ad1af4ca
agunashe <ajay.gunashekar@amd.com>
2025-12-15 16:33:14 -0800

    clean up STANDALONE_TESTS flag usage

--- projects/hip-tests/catch/cmake/hip-tests.cmake
+++ projects/hip-tests/catch/cmake/hip-tests.cmake
@@ -14,46 +14,10 @@ function(hip_add_exe_to_target)
     "${args}"
     "${list_args}"
   )
-  hip_gen_exe_target(
-    NAME ${_NAME}
-    TEST_TARGET_NAME ${_TEST_TARGET_NAME}
-    TEST_SRC ${_TEST_SRC}
-    LINKER_LIBS ${_LINKER_LIBS}
-    COMMON_SHARED_SRC ${_COMMON_SHARED_SRC}
-    COMPILE_OPTIONS ${_COMPILE_OPTIONS}
-    PROPERTY ${_PROPERTY}
-    STANDALONE_FLAG 0
-  )
-  # If STANDALONE_TESTS==1, also generate per-file targets
-  if(STANDALONE_TESTS EQUAL "1")
-    hip_gen_exe_target(
-      NAME ${_NAME}
-      TEST_TARGET_NAME ${_TEST_TARGET_NAME}
-      TEST_SRC ${_TEST_SRC}
-      LINKER_LIBS ${_LINKER_LIBS}
-      COMMON_SHARED_SRC ${_COMMON_SHARED_SRC}
-      COMPILE_OPTIONS ${_COMPILE_OPTIONS}
-      PROPERTY ${_PROPERTY}
-      STANDALONE_FLAG 1
-    )
-  endif()
 
-endfunction()
-
-function(hip_gen_exe_target)
-  set(options)
-  set(args NAME TEST_TARGET_NAME PLATFORM COMPILE_OPTIONS STANDALONE_FLAG)
-  set(list_args TEST_SRC LINKER_LIBS COMMON_SHARED_SRC PROPERTY)
-  cmake_parse_arguments(
-    PARSE_ARGV 0
-    "" # variable prefix
-    "${options}"
-    "${args}"
-    "${list_args}"
-  )
   foreach(SRC_NAME ${TEST_SRC})
 
-    if(NOT _STANDALONE_FLAG EQUAL "1")
+    if(NOT ${STANDALONE_TESTS} EQUAL "1")
       set(_EXE_NAME ${_NAME})
       set(SRC_NAME ${TEST_SRC})
     else()
@@ -64,7 +28,6 @@ function(hip_gen_exe_target)
         continue()
       endif()
     endif()
-
     # Create shared lib of all tests
     set_source_files_properties(${SRC_NAME} PROPERTIES LANGUAGE HIP)
     set_source_files_properties(${COMMON_SHARED_SRC} PROPERTIES LANGUAGE HIP)
@@ -120,7 +83,7 @@ function(hip_gen_exe_target)
     file(GLOB CTEST_INC_FILES "${CMAKE_CURRENT_BINARY_DIR}/${_EXE_NAME}-*_include.cmake")
     set_property(GLOBAL APPEND PROPERTY G_INSTALL_CTEST_INCLUDE_FILES ${CTEST_INC_FILES})
 
-    if(NOT _STANDALONE_FLAG EQUAL "1")
+    if(NOT ${STANDALONE_TESTS} EQUAL "1")
       break()
     endif()
   endforeach()
