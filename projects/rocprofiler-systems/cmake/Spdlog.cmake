2149199ec3e3f4513ed8593675cbb0cb4ae928d6
Milan Radosavljevic <milan.radosavljevic@amd.com>
2025-12-19 10:38:01 +0100

    Include spdlog in CMake

---
+++ projects/rocprofiler-systems/cmake/Spdlog.cmake
@@ -0,0 +1,27 @@
+include_guard(GLOBAL)
+
+if(ROCPROFSYS_BUILD_SPDLOG)
+    message(STATUS "Building spdlog from source!")
+
+    include(FetchContent)
+    FetchContent_Declare(
+        spdlog
+        GIT_REPOSITORY https://github.com/gabime/spdlog.git
+        GIT_TAG v1.14.1
+        GIT_SHALLOW TRUE
+    )
+
+    set(SPDLOG_BUILD_SHARED OFF CACHE BOOL "" FORCE)
+    set(SPDLOG_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
+    set(SPDLOG_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+    set(SPDLOG_INSTALL OFF CACHE BOOL "" FORCE)
+    set(SPDLOG_FMT_EXTERNAL OFF CACHE BOOL "" FORCE)
+
+    FetchContent_MakeAvailable(spdlog)
+
+    target_link_libraries(rocprofiler-systems-spdlog INTERFACE spdlog::spdlog)
+else()
+    message(STATUS "Using system spdlog library")
+    find_package(spdlog REQUIRED)
+    target_link_libraries(rocprofiler-systems-spdlog INTERFACE spdlog::spdlog)
+endif()
