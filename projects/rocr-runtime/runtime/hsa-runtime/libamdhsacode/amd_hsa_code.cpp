2086bf55798b23bb451641965af3b9263a7ff1ef
Prerona Ghosh <Prerona.Ghosh@amd.com>
2026-01-06 13:38:06 -0500

    SWDEV-569319 Fix dangling reference warning

--- projects/rocr-runtime/runtime/hsa-runtime/libamdhsacode/amd_hsa_code.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/libamdhsacode/amd_hsa_code.cpp
@@ -1742,17 +1742,17 @@ namespace code {
       return false;
     }
 
-      const std::shared_ptr<AmdHsaCode>& AmdHsaCodeManager::FromHandle(hsa_code_object_t c)
+      const std::unique_ptr<AmdHsaCode>& AmdHsaCodeManager::FromHandle(hsa_code_object_t c)
       {
         CodeMap::iterator i = codeMap.find(c.handle);
         if (i == codeMap.end()) {
-          std::shared_ptr<AmdHsaCode> code = std::make_shared<AmdHsaCode>();
+          std::unique_ptr<AmdHsaCode> code = std::make_unique<AmdHsaCode>();
           const void* buffer = reinterpret_cast<const void*>(c.handle);
           if (!code->InitAsBuffer(buffer, 0)) {
-            return 0;
+            return nullptr;
           }
-          codeMap[c.handle] = code;
-          return code;
+          codeMap[c.handle] = std::move(code);
+          return codeMap[c.handle];
         }
         return i->second;
       }
