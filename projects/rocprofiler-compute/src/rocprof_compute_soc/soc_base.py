cf021826a617eb27f4f525799239ee6761f56d51
Abhinab Paul Choudhury <abchoudh@amd.com>
2026-01-27 23:22:44 -0600

    Fixed roofline workload dir

--- projects/rocprofiler-compute/src/rocprof_compute_soc/soc_base.py
+++ projects/rocprofiler-compute/src/rocprof_compute_soc/soc_base.py
@@ -730,6 +730,8 @@ class OmniSoC_Base:
                 workload, workload.path, is_gui=False, debug=False
             )
 
+            self.roofline_obj.set_workload_dir(args.path)
+
             self.roofline_obj.post_processing(filtered_pmc)
 
     @abstractmethod
