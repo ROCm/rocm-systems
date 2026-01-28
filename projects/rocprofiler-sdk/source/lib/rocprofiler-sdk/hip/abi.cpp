bee68b081643594adcf66f54f239611c6f454262
Jonathan R. Madsen <jonathanrmadsen@gmail.com>
2026-01-27 21:32:52 -0600

    Revert "SWDEV-558836, SWDEV-558837 - Add hipMemSetMemPool and hipMemGetMemPoo… (#1349)"
    This reverts commit 9a8942a89cba3ee1e3a6e06f59b4a8000b06e9f2.

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/hip/abi.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/hip/abi.cpp
@@ -628,10 +628,6 @@ ROCP_SDK_ENFORCE_ABI(::HipDispatchTable, hipExtDisableLogging_fn, 508);
 ROCP_SDK_ENFORCE_ABI(::HipDispatchTable, hipExtEnableLogging_fn, 509);
 ROCP_SDK_ENFORCE_ABI(::HipDispatchTable, hipExtSetLoggingParams_fn, 510);
 #endif
-#if HIP_RUNTIME_API_TABLE_STEP_VERSION >= 22
-ROCP_SDK_ENFORCE_ABI(::HipDispatchTable, hipMemSetMemPool_fn, 511);
-ROCP_SDK_ENFORCE_ABI(::HipDispatchTable, hipMemGetMemPool_fn, 512);
-#endif
 
 #if HIP_RUNTIME_API_TABLE_STEP_VERSION == 0
 ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 442)
@@ -677,8 +673,6 @@ ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 507)
 ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 508)
 #elif HIP_RUNTIME_API_TABLE_STEP_VERSION == 21
 ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 511)
-#elif HIP_RUNTIME_API_TABLE_STEP_VERSION == 22
-ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 513)
 #else
 INTERNAL_CI_ROCP_SDK_ENFORCE_ABI_VERSIONING(::HipDispatchTable, 0)
 #endif
