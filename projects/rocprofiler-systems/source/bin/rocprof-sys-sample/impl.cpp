cbe8544c4bd3d80d7c9b45707e4906f69d1d6549
Aleksandar Janicijevic <Aleksandar.Janicijevic@amd.com>
2026-02-09 16:35:18 -0500

    AI NIC: ainics -> ai-nics

--- projects/rocprofiler-systems/source/bin/rocprof-sys-sample/impl.cpp
+++ projects/rocprofiler-systems/source/bin/rocprof-sys-sample/impl.cpp
@@ -673,13 +673,13 @@ parse_args(int argc, char** argv, std::vector<char*>& _env)
                 update_mode::REPLACE, ":", updated_envs, original_envs);
         });
     parser
-        .add_argument({ "--ainics" }, "AI NIC IDs for SMI queries. Comma-separated list.")
+        .add_argument({ "--ai-nics" }, "AI NIC IDs for SMI queries. Comma-separated list.")
         .dtype("string")
         .required({ "device" })
         .action([&](parser_t& p) {
             rocprofsys::common::update_env(
                 _env, "ROCPROFSYS_SAMPLING_AINICS",
-                join(array_config{ "," }, p.get<std::vector<std::string>>("ainics")),
+                join(array_config{ "," }, p.get<std::vector<std::string>>("ai-nics")),
                 update_mode::REPLACE, ":", updated_envs, original_envs);
         });
 
