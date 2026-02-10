66a1f38e195374a47c8080d7d846456130a7e9a8
ArvindCheru <Aravindan.Cheruvally@amd.com>
2026-02-09 23:56:40 -0500

    Update Lintian changes with ASAN build fix for hsarocr

--- projects/rocr-runtime/cmake_modules/utils.cmake
+++ projects/rocr-runtime/cmake_modules/utils.cmake
@@ -234,6 +234,11 @@ endfunction()
 
 ## Configure Copyright File for Debian Package
 function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTAINER_NM_T MAINTAINER_EMAIL_T)
+    if("${COMPONENT_NAME_T}" STREQUAL "asan")
+        set(LINTIAN_DOCS_DIR "${CMAKE_INSTALL_DOCDIR}-asan")
+    else()
+        set(LINTIAN_DOCS_DIR ${CMAKE_INSTALL_DOCDIR})
+    endif()
     # Check If Debian Platform
     find_file (DEBIAN debian_version debconf.conf PATHS /etc)
     if(DEBIAN)
@@ -253,7 +258,7 @@ function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTA
 
         # Install copyright file
         install ( FILES "${CMAKE_BINARY_DIR}/DEBIAN/copyright"
-                DESTINATION "${CMAKE_INSTALL_DOCDIR}"
+                DESTINATION "${LINTIAN_DOCS_DIR}"
                 COMPONENT ${COMPONENT_NAME_T} )
 
         # Configure the changelog file
@@ -277,14 +282,14 @@ function( configure_pkg PACKAGE_NAME_T COMPONENT_NAME_T PACKAGE_VERSION_T MAINTA
                 message(FATAL_ERROR "Failed to compress: ${error}")
             endif()
             install ( FILES "${CMAKE_BINARY_DIR}/DEBIAN/${DEB_CHANGELOG_INSTALL_FILENM}"
-                    DESTINATION ${CMAKE_INSTALL_DOCDIR}
+                    DESTINATION ${LINTIAN_DOCS_DIR}
                     COMPONENT ${COMPONENT_NAME_T})
         endif()
 
     else()
         # License file
         install ( FILES ${LICENSE_FILE}
-            DESTINATION ${CMAKE_INSTALL_DOCDIR} RENAME LICENSE.txt
+            DESTINATION ${LINTIAN_DOCS_DIR} RENAME LICENSE.txt
             COMPONENT ${COMPONENT_NAME_T})
 endif()
 endfunction()
