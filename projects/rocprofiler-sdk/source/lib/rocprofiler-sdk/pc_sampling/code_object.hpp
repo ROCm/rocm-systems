162ed3f45300b9404f6a10cd728f9337d571b3ff
vlaindic_amdeng <vladimir.indic@amd.com>
2026-02-11 14:17:23 +0100

    PCS: replicating attach/detach code object tracing mechanism

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/code_object.hpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/pc_sampling/code_object.hpp
@@ -24,6 +24,7 @@
 
 #include "lib/common/static_object.hpp"
 #include "lib/rocprofiler-sdk/pc_sampling/defines.hpp"
+#include "lib/rocprofiler-sdk-attach/table.h"
 
 #include <hsa/hsa_api_trace.h>
 #include <rocprofiler-sdk/callback_tracing.h>
@@ -119,6 +120,9 @@ private:
 
 void
 initialize(HsaApiTable* table);
+
+void
+initialize(RocAttachDispatchTable* table);
 
 void
 finalize();
