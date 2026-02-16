54cd31d16e2acf7c961c1f4dca7722a6dc7f76e4
Pedro Alves <pedro@palves.net>
2026-02-16 14:00:50 +0000

    memory cache: global memory => agent memory, fix FIXME
    "xfer_global_memory" is kind of a reminiscent of the Linux-only time
    where the cache was dealing with global addresses.  Even if yes, from
    the GPU perspective it is called "global", it is not necessarily
    global in the "unified global" sense.
    
    Since the memory cache is now specialized to agent_address_t, rename:
    
     xfer_global_memory => xfer_agent_memory
     read_global_memory => read_agent_memory
     write_global_memory => write_agent_memory
    
    Pass the agent to the memory_cache_t ctor, so we can use it in the
    memory_access_error_t throw in memory_cache_t::write_back, fixing a
    FIXME: instead of using address_space_t::global(), use the agent's
    address space.
    
    This also fixes a compilation error when compiling with GCC at -O2:
    
      In file included from /home/pedalves/rocm/dbgapi/src/src/memory.cpp:21:
      In member function ‘T amd::dbgapi::detail::base_address_t<T>::operator-(U) const [with U = amd::dbgapi::agent_address_t; T = amd::dbgapi::agent_address_t]’,
          inlined from ‘size_t amd::dbgapi::memory_cache_t::xfer_agent_memory(amd::dbgapi::agent_address_t, void*, const void*, size_t)’ at /home/pedalves/rocm/dbgapi/src/src/memory.cpp:678:32:
      /home/pedalves/rocm/dbgapi/src/src/memory.h:85:15: error: ‘<unnamed>.amd::dbgapi::agent_address_t::<unnamed>.amd::dbgapi::detail::base_address_t<amd::dbgapi::agent_address_t>::m_address’ may be used uninitialized [-Werror=maybe-uninitialized]
         85 |     return T{ m_address - decrement };
    	|               ^~~~~~~~~
      /home/pedalves/rocm/dbgapi/src/src/memory.cpp: In member function ‘size_t amd::dbgapi::memory_cache_t::xfer_agent_memory(amd::dbgapi::agent_address_t, void*, const void*, size_t)’:
      /home/pedalves/rocm/dbgapi/src/src/memory.cpp:678:28: note: ‘<anonymous>’ declared here
        678 |     size = agent_address_t{} - address;
    	|                            ^
    
    That's because of:
    
      base_address_t () = default;  => default-initializes members.
      uint64_t m_address;           => no initializer => left uninitialized.
    
    This is fixed in this commit because the "agent_address_t{}" simply
    goes away.
    
    Suggested-By: Lancelot SIX <lancelot.six@amd.com>
    Change-Id: Ib93ea39dcb9648a509a80c1dbd9f8bf79905587c
    commit-id:c509fa86

--- projects/rocdbgapi/src/agent.cpp
+++ projects/rocdbgapi/src/agent.cpp
@@ -75,6 +75,7 @@ agent_t::agent_t (amd_dbgapi_agent_id_t agent_id, process_t &process,
       }(os_agent_info)),
     m_architecture (architecture), m_process (process),
     m_memory_cache (
+      *this,
       [this] (agent_address_t address, void *read, const void *write,
               size_t size)
       {
