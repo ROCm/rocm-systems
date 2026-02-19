4496b316d19fbd97b3bc1ea6d28c33a1fa0c1d24
RahulC <168588218+rahulc-gh@users.noreply.github.com>
2026-02-19 12:57:18 -0800

    adding comm_libs and debug_tools projects

--- .github/scripts/therock_matrix.py
+++ .github/scripts/therock_matrix.py
@@ -2,21 +2,33 @@
 This dictionary is used to map specific file directory changes to the corresponding build flag and tests
 """
 subtree_to_project_map = {
+    "projects/amdsmi": "core",
     "projects/aqlprofile": "profiler",
     "projects/clr": "core",
+    "projects/cuid": "rdc",
     "projects/hip": "core",
     "projects/hip-tests": "core",
     "projects/hipother": "core",
     "projects/rdc": "rdc",
+    "projects/rccl": "comm-libs",
+    "projects/rccl-tests": "comm-libs",
+    "projects/rdc": "dc_tools",
+    "projects/rocdbgapi": "debug_tools",
+    "projects/rocdecode": "media-libs",
+    "projects/rocjpeg": "media-libs",
     "projects/rocm-core": "core",
     "projects/rocm-smi-lib": "core",
     "projects/rocminfo": "core",
+    "projects/rocm-smi-lib": "core",
+    "projects/rocprofiler": "profiler",
     "projects/rocprofiler-compute": "profiler",
     "projects/rocprofiler-register": "profiler",
     "projects/rocprofiler-sdk": "profiler",
     "projects/rocprofiler-systems": "profiler",
     "projects/rocprofiler": "profiler",
+    "projects/rocr-debug-agent": "debug_tools",
     "projects/rocr-runtime": "core",
+    "projects/rocshmem": "comm-libs",
     "projects/roctracer": "profiler",
 }
 
@@ -28,6 +40,23 @@ project_map = {
     "profiler": {
         "cmake_options": "-DTHEROCK_ENABLE_ALL=ON",
         "projects_to_test": "rocprofiler-tests",
+    },
+    # # This needs to be enabled as part of PR 3358 , currently not built by TheRock build system.
+    # "media-libs": {
+    #     "cmake_options": "-DTHEROCK_ENABLE_CORE=ON -DTHEROCK_ENABLE_PROFILER=ON -DTHEROCK_ENABLE_MEDIA_LIBS=OFF",
+    #     "projects_to_test": "rocdecode-tests, rocjpeg-tests",
+    # },
+    "dc_tools": {
+        "cmake_options": "-DTHEROCK_ENABLE_DC_TOOLS=ON",
+        "projects_to_test": "", # rdc-tests is not built by TheRock build system - TBD
+    },
+    "debug_tools": {
+        "cmake_options": "-DTHEROCK_ENABLE_DEBUG_TOOLS=ON -DTHEROCK_ENABLE_ROCGDB=ON",
+        "projects_to_test": "", # rocdbgapi-tests is not built by TheRock build system - TBD
+    },
+    "comm_libs": {
+        "cmake_options": "-DTHEROCK_ENABLE_COMM_LIBS=ON -DTHEROCK_ENABLE_RCCL=ON -DTHEROCK_ENABLE_PROFILER=ON",
+        "projects_to_test": "rccl-tests",
     },
     "all": {
         "cmake_options": "-DTHEROCK_ENABLE_ALL=ON",
