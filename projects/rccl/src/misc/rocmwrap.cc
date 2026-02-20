609ad924eba928020b3fab8a13a437f187ef216f
Pak Lui <pak.lui@amd.com>
2026-02-20 14:15:40 -0800

    Fix zcat path used for checking kernel configs

--- projects/rccl/src/misc/rocmwrap.cc
+++ projects/rccl/src/misc/rocmwrap.cc
@@ -203,7 +203,7 @@ static void initOnceFunc() {
     };
 
     // Check if zcat is available in the system
-    int has_zcat = (access("zcat", X_OK) == 0);
+    int has_zcat = (access("/usr/bin/zcat", X_OK) == 0 || access("/bin/zcat", X_OK) == 0);
 
     for (const auto& path : possiblePaths) {
       // Reset flags for each file
