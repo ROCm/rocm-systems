feee7bc5de968eaab04fabda2c79552b9ef7f155
Jan Stephan <jan.stephan@amd.com>
2026-02-23 17:10:02 +0100

    Use file from `develop` branch
    Signed-off-by: Jan Stephan <jan.stephan@amd.com>

--- projects/rocprofiler-sdk/samples/api_buffered_tracing/main.cpp
+++ projects/rocprofiler-sdk/samples/api_buffered_tracing/main.cpp
@@ -371,11 +371,7 @@ run_migrate(int rank, int tid, hipStream_t stream, int, char** argv)
     for(auto& itr : page_data)
         itr = init_v;
 
-    auto page_data_dev_ptr = static_cast<data_type*>(nullptr);
-    HIP_API_CALL(
-        hipHostGetDevicePointer(reinterpret_cast<void**>(&page_data_dev_ptr), page_data.data(), 0));
-
-    test_page_migrate<<<1, 1024, 0, stream>>>(page_data_dev_ptr, incr_v);
+    test_page_migrate<<<1, 1024, 0, stream>>>(d_ptr, incr_v);
     check_hip_error();
 
     HIP_API_CALL(hipStreamSynchronize(stream));
