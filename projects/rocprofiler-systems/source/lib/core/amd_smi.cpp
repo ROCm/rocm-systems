cfaeea2519081300c49ce0b83144ee472c47a00e
David Galiffi <David.Galiffi@amd.com>
2026-02-25 06:25:37 -0500

    Users/dgaliffi/sdma support (#3523)
    * Update default metrics in amd_smi.cpp to include sdma_usage
    
    * Add compile-time guards.
    
    the sdma_usage field was only added to `amdsmi_proc_info_t` in 26.3.

--- projects/rocprofiler-systems/source/lib/core/amd_smi.cpp
+++ projects/rocprofiler-systems/source/lib/core/amd_smi.cpp
@@ -61,6 +61,7 @@ config_settings(const std::shared_ptr<settings>& _config)
     std::string vcn_activity_support{};
     std::string xgmi_support{};
     std::string pcie_support{};
+    std::string sdma_support{};
 
     size_t device_count = gpu::get_processor_count();
     for(size_t i = 0; i < device_count; i++)
@@ -96,10 +97,14 @@ config_settings(const std::shared_ptr<settings>& _config)
         }
     }
 
+#    if AMD_SMI_SDMA_SUPPORTED == 1
+    sdma_support += ", sdma_usage";
+#    endif
+
     ROCPROFSYS_CONFIG_SETTING(
         std::string, "ROCPROFSYS_AMD_SMI_METRICS",
         "amd-smi metrics to collect: " + default_metrics + jpeg_activity_support +
-            vcn_activity_support + xgmi_support + pcie_support + ". " +
+            vcn_activity_support + xgmi_support + pcie_support + sdma_support + ". " +
             "An empty value implies 'all' and 'none' suppresses all.",
         "busy, temp, power, mem_usage", "backend", "amd_smi", "rocm", "process_sampling");
 }
--- projects/rocprofiler-systems/source/lib/core/amd_smi.hpp
+++ projects/rocprofiler-systems/source/lib/core/amd_smi.hpp
@@ -29,10 +29,12 @@
 #    if ROCPROFSYS_USE_AINIC > 0
 #        if AMDSMI_LIB_VERSION_MAJOR > 26 ||                                             \
             (AMDSMI_LIB_VERSION_MAJOR == 26 && AMDSMI_LIB_VERSION_MINOR > 2)
-#            define AINIC_SUPPORTED 1
+#            define AINIC_SUPPORTED        1
+#            define AMD_SMI_SDMA_SUPPORTED 1
 #        endif
 #    endif
 #endif
+
 namespace rocprofsys
 {
 namespace amd_smi
