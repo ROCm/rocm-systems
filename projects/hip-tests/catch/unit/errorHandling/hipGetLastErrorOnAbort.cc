5cd22032a73c2d596fee8112aab60d5d84af9441
Jatin Chaudhary <51944368+cjatin@users.noreply.github.com>
2026-03-02 09:11:46 +0000

    Update hipGetLastErrorOnAbort.cc

--- projects/hip-tests/catch/unit/errorHandling/hipGetLastErrorOnAbort.cc
+++ projects/hip-tests/catch/unit/errorHandling/hipGetLastErrorOnAbort.cc
@@ -210,8 +210,8 @@ TEST_CASE("Unit_hipGetLastError_KernelFailure_TwoStreams") {
   ret = hipGetLastError();
   REQUIRE(ret == hipErrorIllegalAddress);
 
-  RESTORE_CORE_DUMPS();
-
   HIP_CHECK(hipStreamDestroy(stream1));
   HIP_CHECK(hipStreamDestroy(stream2));
+
+  RESTORE_CORE_DUMPS();
 }
