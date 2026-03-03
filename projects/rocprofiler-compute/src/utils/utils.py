9477e4d39028d86b100de36880eb33bbeedaf430
ggottipa-amd <ggottipa@amd.com>
2026-03-03 01:51:22 -0600

    Ruff

--- projects/rocprofiler-compute/src/utils/utils.py
+++ projects/rocprofiler-compute/src/utils/utils.py
@@ -1417,9 +1417,7 @@ def build_kernel_name_to_id(
     kernel_df = kernel_name_shortener(kernel_df.copy(), kernel_verbose)
     if kernel_df is None:
         return None
-    return {
-        str(row["Kernel_Name"]).strip(): i for i, row in kernel_df.iterrows()
-    }
+    return {str(row["Kernel_Name"]).strip(): i for i, row in kernel_df.iterrows()}
 
 
 @demarcate
