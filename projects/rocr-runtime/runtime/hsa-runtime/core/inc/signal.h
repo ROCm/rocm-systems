00fffec4a20ecc67b8c8863f4bd49b9285c34f14
Patrick Schlangen <pschlan@amd.com>
2026-03-10 17:54:59 +0100

    Fix monitorx/mwaitx race condition

--- projects/rocr-runtime/runtime/hsa-runtime/core/inc/signal.h
+++ projects/rocr-runtime/runtime/hsa-runtime/core/inc/signal.h
@@ -105,9 +105,12 @@ inline void CheckAbortTimeout(const timer::fast_clock::time_point& start_time,
   }
 }
 
-inline void DoMwaitx(int64_t* addr, uint32_t timeout, bool timer_enable = false) {
+inline void DoMwaitx(int64_t* addr, int64_t val_on_last_check, uint32_t timeout, bool timer_enable = false) {
 #if defined(__i386__) || defined(__x86_64__)
   _mm_monitorx(addr, 0, 0);
+  if (atomic::Load(addr, std::memory_order_relaxed) != val_on_last_check) {
+    return;
+  }
   _mm_mwaitx(0, timeout, timer_enable ? MWAITX_ECX_TIMER_ENABLE : 0);
 #endif
 }
