142240cfd6c75ddf8f44b224bb9143fc8771f9af
Prerona Ghosh <Prerona.Ghosh@amd.com>
2026-03-11 15:45:03 -0400

    Make InitDerivedCuid() pure virtual and implement them inside all child agents

--- projects/rocr-runtime/runtime/hsa-runtime/core/inc/agent.h
+++ projects/rocr-runtime/runtime/hsa-runtime/core/inc/agent.h
@@ -315,10 +315,7 @@ class Agent : public Checked<0xF6BC25EB17E6F917> {
                                void* value) const = 0;
 
   // @brief Initialize secondary CUID for this agent.
-  virtual void InitDerivedCuid() {
-    // Default implementation does nothing. Only GPU agents support CUID, 
-    // so this is only overridden in the GpuAgent class.
-  }
+  virtual void InitDerivedCuid() = 0;
 
   // @brief Returns an array of regions owned by the agent.
   virtual const std::vector<std::shared_ptr<const core::MemoryRegion>>& regions() const = 0;
