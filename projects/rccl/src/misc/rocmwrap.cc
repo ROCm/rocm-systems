088991a446ae51c44d8478b1f88e2b5b1eea65f1
BertanDogancay <bertan.dogancay@gmail.com>
2026-03-12 11:11:57 -0400

    Gate cuMem support with Linux Kernel version

--- projects/rccl/src/misc/rocmwrap.cc
+++ projects/rccl/src/misc/rocmwrap.cc
@@ -36,12 +36,25 @@ static pthread_once_t initOnceControl = PTHREAD_ONCE_INIT;
 static ncclResult_t initResult;
 
 // This env var (NCCL_CUMEM_ENABLE) toggles cuMem API usage
-NCCL_PARAM(CuMemEnable, "CUMEM_ENABLE", 0);
+NCCL_PARAM(CuMemEnable, "CUMEM_ENABLE", -2);
 NCCL_PARAM(CuMemHostEnable, "CUMEM_HOST_ENABLE", -1);
 // Handle type used for cuMemCreate()
 CUmemAllocationHandleType ncclCuMemHandleType = CU_MEM_HANDLE_TYPE_POSIX_FILE_DESCRIPTOR;
 
 static int ncclCuMemSupported = 0;
+
+#define KERNEL_VERSION_CODE(major, minor) ((major << 16) | (minor << 8))
+
+static int ncclGetKernelVersionCode() {
+  struct utsname u;
+  int major = 0, minor = 0;
+
+  if (uname(&u) != 0) return -1;
+  sscanf(u.release, "%d.%d", &major, &minor);
+  INFO(NCCL_INIT, "Kernel version %d.%d", major, minor);
+
+  return KERNEL_VERSION_CODE(major, minor);
+}
 
 // Determine whether CUMEM & VMM RDMA is supported on this platform
 int ncclIsCuMemSupported() {
@@ -49,27 +62,36 @@ int ncclIsCuMemSupported() {
   int cudaDev;
   int cudaDriverVersion;
   int flag = 0;
+  int supported = 1;
   ncclResult_t ret = ncclSuccess;
+
+  if (ncclGetKernelVersionCode() < KERNEL_VERSION_CODE(6, 8)) {
+    WARN("cuMem support requires Linux kernel >= 6.8");
+    supported = 0;
+  }
   CUDACHECKGOTO(cudaDriverGetVersion(&cudaDriverVersion), ret, error);
-  if (cudaDriverVersion < 71260540) return 0;
+  if (cudaDriverVersion < 71260540) {
+    WARN("cuMem support requires HIP_VERSION >= 7.12.60540");
+    supported = 0;
+  }
   CUDACHECKGOTO(cudaGetDevice(&cudaDev), ret, error);
-  if (CUPFN(cuMemCreate) == NULL) return 0;
+  if (CUPFN(cuMemCreate) == NULL) supported = 0;
   CUCHECKGOTO(cuDeviceGet(&currentDev, cudaDev), ret, error);
   // Query device to see if CUMEM VMM support is available
   CUCHECKGOTO(cuDeviceGetAttribute(&flag, CU_DEVICE_ATTRIBUTE_VIRTUAL_MEMORY_MANAGEMENT_SUPPORTED, currentDev), ret, error);
-  if (!flag) return 0;
+  if (!flag) {
+    WARN("cuMem support requires VMM RDMA support");
+    supported = 0;
+  }
 
+  return supported;
 error:
   return (ret == ncclSuccess);
 }
 
 int ncclCuMemEnable() {
   int param = ncclParamCuMemEnable();
-  if (param > 0 && !ncclCuMemSupported) {
-    WARN("cuMem support requires HIP_VERSION >= 71260540");
-    return 0;
-  }
-  return param;
+  return param >= 0 ? param : (param == -2 && ncclCuMemSupported);
 }
 
 static int ncclCumemHostEnable = -1;
