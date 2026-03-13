ddf20dfeb4987ac265a870b16c5977ba451681eb
BertanDogancay <bertan.dogancay@gmail.com>
2026-03-13 13:47:07 -0400

    make generation of sym kernels default

--- projects/rccl/install.sh
+++ projects/rccl/install.sh
@@ -39,7 +39,7 @@ run_tests=false
 run_tests_all=false
 time_trace=false
 force_reduce_pipeline=false
-generate_sym_kernels=false
+generate_sym_kernels=true
 warp_speed_enabled=true # note that this flag will be overridden to false for non MI350/MI300 platforms
 quiet_warnings=false
 build_rocshmem_support=false
@@ -83,7 +83,7 @@ function display_help()
     echo "       --time-trace            Plot the build time of RCCL (requires \`ninja-build\` package installed on the system)"
     echo "       --verbose               Show compile commands"
     echo "       --force-reduce-pipeline Force reduce_copy sw pipeline to be used for every reduce-based collectives and datatypes"
-    echo "       --generate-sym-kernels  Generate symmetric memory kernels"
+    echo "       --disable-sym-kernels   Disable generation of symmetric memory kernels"
     echo "    -q|--quiet-warnings        Suppress majority of compiler warnings (not recommended)"
     echo "       --rocshmem              Build with rocSHMEM support"
 }
@@ -95,7 +95,7 @@ function display_help()
 # check if we have a modern version of getopt that can handle whitespace and long parameters
 getopt -T
 if [[ "$?" -eq 4 ]]; then
-    GETOPT_PARSE=$(getopt --name "${0}" --options cdfhij:lprtq --longoptions address-sanitizer,dependencies,debug,dump-asm,enable-code-coverage,enable_backtrace,disable-colltrace,disable-msccl-kernel,enable-mscclpp,fast,help,install,jobs:,kernel-resource-use,local_gpu_only,amdgpu_targets:,no_clean,npkit-enable,log-trace,openmp-test-enable,roctx-enable,package_build,prefix:,rm-legacy-include-dir,run_tests_all,run_tests_quick,static,tests_build,time-trace,force-reduce-pipeline,generate-sym-kernels,quiet-warnings,disable-warp-speed,verbose,rocshmem -- "$@")
+    GETOPT_PARSE=$(getopt --name "${0}" --options cdfhij:lprtq --longoptions address-sanitizer,dependencies,debug,dump-asm,enable-code-coverage,enable_backtrace,disable-colltrace,disable-msccl-kernel,enable-mscclpp,fast,help,install,jobs:,kernel-resource-use,local_gpu_only,amdgpu_targets:,no_clean,npkit-enable,log-trace,openmp-test-enable,roctx-enable,package_build,prefix:,rm-legacy-include-dir,run_tests_all,run_tests_quick,static,tests_build,time-trace,force-reduce-pipeline,disable-sym-kernels,quiet-warnings,disable-warp-speed,verbose,rocshmem -- "$@")
 else
     echo "Need a new version of getopt"
     exit 1
@@ -142,7 +142,7 @@ while true; do
          --time-trace)               time_trace=true;                                                                                  shift ;;
          --verbose)                  build_verbose=true;                                                                               shift ;;
          --force-reduce-pipeline)    force_reduce_pipeline=true;                                                                       shift ;;
-         --generate-sym-kernels)     generate_sym_kernels=true;                                                                        shift ;;
+         --disable-sym-kernels)      generate_sym_kernels=false;                                                                       shift ;;
          --disable-warp-speed)       warp_speed_enabled=false;                                                                         shift ;;
     -q | --quiet-warnings)           quiet_warnings=true;                                                                              shift ;;
          --rocshmem)                 build_rocshmem_support=true;                                                                      shift ;;
@@ -319,8 +319,8 @@ if [[ "${force_reduce_pipeline}" == true ]]; then
 fi
 
 # Generate symmetric memory kernels
-if [[ "${generate_sym_kernels}" == true ]]; then
-    cmake_common_options="${cmake_common_options} -DGENERATE_SYM_KERNELS=ON"
+if [[ "${generate_sym_kernels}" == false ]]; then
+    cmake_common_options="${cmake_common_options} -DGENERATE_SYM_KERNELS=OFF"
 fi
 
 # Enable NPKit
