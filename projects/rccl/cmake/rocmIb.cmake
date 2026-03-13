d2c87d76a131ce20309afffdaa0707e652cc9a23
Prasannakumar Murugesan <prmuruge@amd.com>
2026-03-13 12:27:34 -0400

    Add missing GIN files and rename symbols to avoid duplicates in link

--- projects/rccl/cmake/rocmIb.cmake
+++ projects/rccl/cmake/rocmIb.cmake
@@ -276,4 +276,8 @@ execute_process(
   COMMAND bash -c "sed -i 's/CU_MEM_RANGE_HANDLE_TYPE_DMA_BUF_FD/hipMemRangeHandleTypeDmaBufFd/g' ${ROCM_NETIB_FILE}"
   WORKING_DIRECTORY ${RCCL_SRC_DIR}
 )
+execute_process(
+  COMMAND bash -c "sed -i -e 's/ncclGinIb/rocmGinIb/g' -e 's/rocmGinIbCollComm/ncclGinIbCollComm/g' ${ROCM_NETIB_FILE}"
+  WORKING_DIRECTORY ${RCCL_SRC_DIR}
+)
 list(APPEND CMAKE_MODULE_PATH "${CMAKE_CURRENT_SOURCE_DIR}/cmake")
