3ffe81c6e065203ab0c7d12263fa4388b4b940c9
Todd tiantuo Li <Toddtiantuo.Li@amd.com>
2026-03-16 04:03:17 -0700

    Add DEBUG_CLR_DISABLE_IMAGE and fix image support logic

--- projects/clr/rocclr/utils/flags.hpp
+++ projects/clr/rocclr/utils/flags.hpp
@@ -266,7 +266,7 @@ release(bool, HIP_FORCE_SPIRV_CODEOBJECT, false,                              \
         "Force use of SPIRV instead of device specific code object.")         \
 release(uint, DEBUG_CLR_BATCH_CPU_SYNC_SIZE, 8,                               \
         "Forces the minimum batch size for CPU sync")                         \
-release(bool, ROC_DISABLE_IMAGE_SUPPORT, false,                               \
+release(bool, DEBUG_CLR_DISABLE_IMAGE, false,                               \
         "1 = Disable Image support for ROC path")  // clang-format on
 
 namespace amd {
