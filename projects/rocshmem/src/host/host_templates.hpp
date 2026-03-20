1d8403e2e241a6df422bef0408f2b1a2c5b4a17f
Aurelien Bouteiller <abouteil@amd.com>
2026-03-20 17:55:16 -0400

    Add implementation for hostInterface::wait*_vector

--- projects/rocshmem/src/host/host_templates.hpp
+++ projects/rocshmem/src/host/host_templates.hpp
@@ -625,6 +625,26 @@ __host__ void HostInterface::wait_until_all_vector(T* ivars, size_t nelems,
                                                    int cmp, T* vals,
                                                    WindowInfo* window_info) {
   DPRINTF("Function: host_wait_until_all_vector\n");
+
+  // zero nelems error condition
+  if (!nelems) {
+    return;
+  }
+
+  size_t pos{status_entry(nelems, status)};
+
+  // invalid (empty) status array error condition
+  if (pos == nelems) {
+    return;
+  }
+
+  for (size_t i{pos}; i < nelems; i++) {
+    if (status[i]) {
+      continue;
+    }
+    while (!test(ivars + i, cmp, vals[i], window_info)) {
+    }
+  }
 }
 
 template <typename T>
@@ -633,6 +653,30 @@ __host__ size_t HostInterface::wait_until_any_vector(T* ivars, size_t nelems,
                                                      int cmp, T* vals,
                                                      WindowInfo* window_info) {
   DPRINTF("Function: host_wait_until_any_vector\n");
+
+  // zero nelems error condition
+  if (!nelems) {
+    return SIZE_MAX;
+  }
+
+  size_t pos{status_entry(nelems, status)};
+
+  // invalid (empty) status array error condition
+  if (pos == nelems) {
+    return SIZE_MAX;
+  }
+
+  while (true) {
+    for (size_t i{pos}; i < nelems; i++) {
+      // skip entries marked with non-zero status
+      if (status[i]) {
+        continue;
+      }
+      if (test(ivars + i, cmp, vals[i], window_info)) {
+        return i;
+      }
+    }
+  }
   return 0;
 }
 
@@ -643,7 +687,35 @@ __host__ size_t HostInterface::wait_until_some_vector(T* ivars, size_t nelems,
                                                     int cmp, T* vals,
                                                     WindowInfo* window_info) {
   DPRINTF("Function: host_wait_until_some_vector\n");
-  return 0;
+
+  // zero nelems error condition
+  if (!nelems) {
+    return 0;
+  }
+
+  size_t pos{status_entry(nelems, status)};
+
+  // invalid (empty) status array error condition
+  if (pos == nelems) {
+    return 0;
+  }
+
+  bool done {false};
+  size_t ncompleted {0};
+  while (!done) {
+    for (size_t i{pos}; i < nelems; i++) {
+      // skip entries marked with non-zero status
+      if (status[i]) {
+        continue;
+      }
+      if (test(ivars + i, cmp, vals[i], window_info)) {
+        done = true;
+        indices[ncompleted] = i;
+        ncompleted++;
+      }
+    }
+  }
+  return ncompleted;
 }
 
 template <typename T>
