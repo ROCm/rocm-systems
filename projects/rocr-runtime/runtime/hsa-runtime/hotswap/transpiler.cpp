e4d61abd2e9fc7b23a71e75a52a8e1ff8225f83e
powderluv <anush.elangovan@amd.com>
2026-03-21 12:13:07 -0700

    Unconditional exec_hi=0 after v_cmpx + conditional kernarg save

--- projects/rocr-runtime/runtime/hsa-runtime/hotswap/transpiler.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/hotswap/transpiler.cpp
@@ -3411,17 +3411,7 @@ RewriteResult TranspileCodeObject(void** elf_data, size_t* elf_size,
           if (t.find("v_cmpx_") != std::string::npos) { has_vcmpx = true; break; }
         }
         if (has_vcmpx) {
-          bool next_is_execz = false;
-          for (size_t nxt = ii + 1; nxt < source_instrs.size(); nxt++) {
-            std::string nm = ExtractMnemonic(source_instrs[nxt].text);
-            if (nm.find("s_delay") == 0 || nm.find("s_wait") == 0 ||
-                nm.find("s_nop") == 0 || nm.find("s_clause") == 0) continue;
-            if (nm == "s_cbranch_execz") next_is_execz = true;
-            break;
-          }
-          if (!next_is_execz) {
-            translated_asm += "s_mov_b32 exec_hi, 0\n";
-          }
+          translated_asm += "s_mov_b32 exec_hi, 0\n";
         }
       }
 
