369c5e089a8258e985b0afcbd8c1c24e4a635f8f
German Andryeyev <German.Andryeyev@amd.com>
2026-04-02 12:40:37 -0400

    clr: Fix a hsa signal leak
    CLR swaps signals when HIP events are used, but it may swap them with an incorrect signal type, potentially causing a leak.

--- projects/clr/rocclr/device/rocm/rocvirtual.cpp
+++ projects/clr/rocclr/device/rocm/rocvirtual.cpp
@@ -499,7 +499,9 @@ hsa_signal_t VirtualGPU::HwQueueTracker::ActiveSignal(hsa_signal_value_t init_va
     // The signal was assigned to the global marker's event, hence runtime can't reuse it
     // and needs a new signal
     std::unique_ptr<ProfilingSignal> signal(new ProfilingSignal());
-    if ((signal != nullptr) && CreateSignal(signal.get())) {
+    // Makes ure the same signal type is created with the same interrupt flag, since the tracking
+    // list relies on that for the reuse
+    if ((signal != nullptr) && CreateSignal(signal.get(), signal_list_[current_id_]->flags_.interrupt_)) {
       signal_list_[current_id_]->release();
       signal_list_[current_id_] = signal.release();
     } else {
