1a5339fe7e055ffbefaa43020f60c1eb66028ed2
Bojan Aleksovski <bojan.aleksovski@canonical.com>
2026-04-03 18:16:31 +0200

    [rocr-runtime] Take in account old hwloc API as well

--- projects/rocr-runtime/rocrtst/suites/performance/memory_async_copy.cc
+++ projects/rocr-runtime/rocrtst/suites/performance/memory_async_copy.cc
@@ -897,8 +897,13 @@ static hsa_status_t GetAgentInfo(hsa_agent_t agent, void* data) {
 void MemoryAsyncCopy::FindTopology() {
   hsa_status_t err;
 
+#if HWLOC_API_VERSION >= 0x00020000
   hwloc_topology_set_flags(topology_, HWLOC_TOPOLOGY_FLAG_WHOLE_SYSTEM);
   hwloc_topology_set_io_types_filter(topology_, HWLOC_TYPE_FILTER_KEEP_ALL);
+#else
+  hwloc_topology_set_flags(topology_, HWLOC_TOPOLOGY_FLAG_WHOLE_SYSTEM |
+                                         HWLOC_TOPOLOGY_FLAG_IO_DEVICES);
+#endif
 
   hwloc_topology_load(topology_);
 
