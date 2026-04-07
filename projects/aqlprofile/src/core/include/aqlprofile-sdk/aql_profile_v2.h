9a5ab12cc5f0c88b5c8a8bc0a76a61f85c7d4e22
Sriraksha Nagaraj <Sriraksha.Nagaraj@amd.com>
2026-04-07 09:36:53 -0500

    Review comments

--- projects/aqlprofile/src/core/include/aqlprofile-sdk/aql_profile_v2.h
+++ projects/aqlprofile/src/core/include/aqlprofile-sdk/aql_profile_v2.h
@@ -591,12 +591,15 @@ typedef struct
 
 typedef enum
 {
-    AQLPROFILE_SPM_PARAMETER_TYPE_BUFFER_SIZE = 0,
-    AQLPROFILE_SPM_PARAMETER_TYPE_SAMPLE_INTERVAL,
+    AQLPROFILE_SPM_PARAMETER_TYPE_NONE = 0,
+    AQLPROFILE_SPM_PARAMETER_TYPE_BUFFER_SIZE,
+    AQLPROFILE_SPM_PARAMETER_TYPE_SAMPLE_INTERVAL_SCLK_CYCLES,
+    AQLPROFILE_SPM_PARAMETER_TYPE_SAMPLE_INTERVAL_REFCLK_CYCLES,
     AQLPROFILE_SPM_PARAMETER_TYPE_TIMEOUT,
     AQLPROFILE_SPM_PARAMETER_TYPE_SAMPLE_MODE,
     AQLPROFILE_SPM_PARAMETER_TYPE_LAST,
 } aqlprofile_spm_parameter_type_t;
+
 
 typedef enum
 {
@@ -605,11 +608,12 @@ typedef enum
     AQLPROFILE_SPM_PARAMETER_SAMPLE_MODE_REFCLK,
 } aqlprofile_spm_parameter_interval_mode_t;
 
+
 typedef struct aqlprofile_spm_available_configuration_t
 {
-   aqlprofile_spm_parameter_interval_mode_t sample_interval_unit;
-   uint64_t min_interval;
-   uint64_t max_interval;
+    aqlprofile_spm_parameter_type_t        type =  AQLPROFILE_SPM_PARAMETER_TYPE_NONE;
+    uint64_t                               min_interval = 0;
+    uint64_t                               max_interval = 0;
 } aqlprofile_spm_available_configuration_t;
 
 typedef struct
@@ -755,7 +759,7 @@ aqlprofile_spm_is_event_supported(aqlprofile_agent_handle_t agent, aqlprofile_pm
 
 typedef hsa_status_t (*aqlprofile_spm_available_configurations_cb_t)(
     const aqlprofile_spm_available_configuration_t*    config,
-    size_t*                                         num_config,
+    size_t                                         num_config,
     void*                                         user_data);
 
 hsa_status_t
