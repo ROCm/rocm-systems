60177354341214ddcd670d3a3ca71210e3ca0f5e
Fabio Mestre <fabio.mestre@amd.com>
2026-04-14 13:23:51 +0100

    Add missing header

---
+++ projects/hip-tests/catch/unit/memory/memcpyBatchAsync_common.hh
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Advanced Micro Devices, Inc., or its affiliates.
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#pragma once
+
+#include <hip_test_common.hh>
+#include <hip_test_kernels.hh>
+#include <hip_test_process.hh>
+#include <resource_guards.hh>
+#include <utils.hh>
+
+template <typename> struct get_test_values_unsupported_type : std::false_type {};
+ 
+ template <typename TestType> constexpr std::pair<TestType, TestType> get_test_values() {
+   if constexpr (std::is_same_v<TestType, char>) {
+     return {'a', 'b'};
+   } else if constexpr (std::is_integral_v<TestType>) {
+     return {static_cast<TestType>(10), static_cast<TestType>(4)};
+   } else if constexpr (std::is_floating_point_v<TestType>) {
+     return {static_cast<TestType>(2.25f), static_cast<TestType>(0.25f)};
+   } else {
+     static_assert(get_test_values_unsupported_type<TestType>::value,
+                   "get_test_values: add an explicit branch for this TestType");
+   }
+ }
+
+inline hipError_t getSwapExpectedReturn(const LinearAllocs allocTypeSrc,
+                                         const LinearAllocs allocTypeDst) {
+   int major, minor;
+   HIP_CHECK(hipDeviceComputeCapability(&major, &minor, 0));
+
+   hipError_t expectedError;
+   if (major == 9 && minor >= 4) {
+     expectedError = hipSuccess;
+
+     if (allocTypeSrc == LinearAllocs::malloc ||
+         allocTypeDst == LinearAllocs::malloc) {
+       expectedError = hipErrorInvalidValue;
+     }
+
+   } else {
+     expectedError = hipErrorNotSupported;
+   }
+
+   return expectedError;
+ }
