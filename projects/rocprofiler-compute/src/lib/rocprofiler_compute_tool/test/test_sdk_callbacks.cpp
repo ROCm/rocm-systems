0a60f435af6f0b38d7940e9afec57ff094966ba0
svolkov-amd <stanislav.volkov@amd.com>
2026-04-16 12:31:26 +0100

    additional sdk callback tests

--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_sdk_callbacks.cpp
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_sdk_callbacks.cpp
@@ -5,42 +5,77 @@
 #include "fmt/format.h"
 #include "rocprofiler_compute_tool.h"
 
+TEST_F(TestSdkCallbacks, ProvidedCountersAvaiableWithMultiplexingDisabled_ReturnsFirstPmcForCollection)
+{
+    m_tool_data->iteration_multiplexing_mode = rocprofiler_compute_tool::iteration_multiplexing_mode_t::DISABLED;
+    const auto config_index_0 = dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
+    const auto config_index_1 = dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
+
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_LT(config_index_0, created_config_info.size());
+    EXPECT_LT(config_index_1, created_config_info.size());
+    EXPECT_EQ(created_config_info[config_index_0].counter_names, m_counters_pmc0);
+    EXPECT_EQ(created_config_info[config_index_1].counter_names, m_counters_pmc0);
+}
+
+TEST_F(TestSdkCallbacks, ProvidedCountersAvaiableWithKernelMultiplexing_ReturnsEachPmcForCollection)
+{
+    m_tool_data->iteration_multiplexing_mode = rocprofiler_compute_tool::iteration_multiplexing_mode_t::KERNEL;
+    const auto config_index_0 = dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
+    const auto config_index_1 = dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
+
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_LT(config_index_0, created_config_info.size());
+    EXPECT_LT(config_index_1, created_config_info.size());
+    EXPECT_EQ(created_config_info[config_index_0].counter_names, m_counters_pmc0);
+    EXPECT_EQ(created_config_info[config_index_1].counter_names, m_counters_pmc1);
+}
+
 TEST_F(TestSdkCallbacks, ProvidedRequestedCountersAvaiable_AllConfiguredForCollection)
 {
-    const std::vector<std::string> counters_pmc0 = {"counter0", "counter1"};
-    const std::vector<std::string> counters_pmc1 = {"counter2"};
+    dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
 
-    dispatch_kernel(1, counters_pmc0, counters_pmc1);
-
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info().size(), 2);
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info()[0].counter_names, counters_pmc0);
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info()[1].counter_names, counters_pmc1);
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_EQ(created_config_info.size(), 2);
+    EXPECT_EQ(created_config_info[0].counter_names, m_counters_pmc0);
+    EXPECT_EQ(created_config_info[1].counter_names, m_counters_pmc1);
 }
 
-TEST_F(TestSdkCallbacks, ProvidedKernelIds_ReturnsResultForThemOnly)
+TEST_F(TestSdkCallbacks, ProvidedKernelIdsWithDisabledMultiplexing_ReturnsResultForThemOnly)
 {
-    const std::vector<std::string> counters_pmc0 = {"counter0", "counter1"};
-    const std::vector<std::string> counters_pmc1 = {"counter2"};
-
+    m_tool_data->iteration_multiplexing_mode = rocprofiler_compute_tool::iteration_multiplexing_mode_t::DISABLED;
     m_tool_data->target_kernel_ids.insert(2);
-    dispatch_kernel(1, counters_pmc0);
-    dispatch_kernel(2, counters_pmc1);
 
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info().size(), 1);
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info()[0].counter_names, counters_pmc1);
+    dispatch_kernel(1, m_counters_pmc0);
+    dispatch_kernel(2, m_counters_pmc1);
+
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_EQ(created_config_info.size(), 1);
+    EXPECT_EQ(created_config_info[0].counter_names, m_counters_pmc1);
 }
 
-TEST_F(TestSdkCallbacks, ProvidedKernelDispatchRanges_ReturnsResultForThemOnly)
+TEST_F(TestSdkCallbacks, ProvidedKernelDispatchRangesWithDisabledMultiplexing_ReturnsResultForThemOnly)
 {
-    const std::vector<std::string> counters_pmc0 = {"counter0", "counter1"};
-    const std::vector<std::string> counters_pmc1 = {"counter2"};
-
+    m_tool_data->iteration_multiplexing_mode = rocprofiler_compute_tool::iteration_multiplexing_mode_t::DISABLED;
     m_tool_data->kernel_filter_ranges.emplace_back(2, 2);
-    dispatch_kernel(11, counters_pmc0);
-    dispatch_kernel(11, counters_pmc1);
 
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info().size(), 1);
-    EXPECT_EQ(m_sdk_wrapper->get_create_counter_config_info()[0].counter_names, counters_pmc1);
+    const auto config_index_0 = dispatch_kernel(11, m_counters_pmc0);
+    const auto config_index_1 = dispatch_kernel(11, m_counters_pmc1);
+
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_EQ(created_config_info.size(), 1);
+    EXPECT_EQ(config_index_0, m_invalid_config_id);
+    EXPECT_EQ(config_index_1, 0);
+    EXPECT_EQ(created_config_info[config_index_1].counter_names, m_counters_pmc1);
+}
+
+TEST_F(TestSdkCallbacks, ProvidedMultiplexingDisabled_ReturnsFirstPmcConfig)
+{
+    const auto selected_config = dispatch_kernel(1, m_counters_pmc0, m_counters_pmc1);
+
+    const auto& created_config_info = m_sdk_wrapper->get_create_counter_config_info();
+    EXPECT_GE(created_config_info.size(), selected_config);
+    EXPECT_EQ(created_config_info[selected_config].counter_names, m_counters_pmc0);
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -53,9 +88,9 @@ void TestSdkCallbacks::SetUp()
     m_tool_data     = std::make_unique<rocprofiler_compute_tool::tool_data_t>();
 }
 
-void TestSdkCallbacks::dispatch_kernel(uint64_t                        kernel_id,
-                                       const std::vector<std::string>& counters_pmc0,
-                                       const std::vector<std::string>& counters_pmc1)
+uint64_t TestSdkCallbacks::dispatch_kernel(uint64_t                        kernel_id,
+                                           const std::vector<std::string>& counters_pmc0,
+                                           const std::vector<std::string>& counters_pmc1)
 {
     m_tool_data->requested_counters = convert_counters_per_pmc_to_str({counters_pmc0, counters_pmc1});
     m_sdk_wrapper->set_available_counters(concat_counters(counters_pmc0, counters_pmc1));
@@ -63,8 +98,9 @@ void TestSdkCallbacks::dispatch_kernel(uint64_t                        kernel_id
     rocprofiler_dispatch_counting_service_data_t dispatch_data = {};
     dispatch_data.dispatch_info.kernel_id                      = kernel_id;
     dispatch_data.dispatch_info.agent_id.handle                = 0xff;
-    rocprofiler_counter_config_id_t config;
+    rocprofiler_counter_config_id_t config{m_invalid_config_id};
     m_sdk_callbacks->dispatch_callback(dispatch_data, &config, &m_tool_data);
+    return config.handle;
 }
 
 std::string TestSdkCallbacks::convert_counters_per_pmc_to_str(
--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_sdk_callbacks.h
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/test/test_sdk_callbacks.h
@@ -8,9 +8,9 @@ class TestSdkCallbacks : public ::testing::Test
 protected:
     void SetUp() override;
 
-    void dispatch_kernel(uint64_t                        kernel_id,
-                         const std::vector<std::string>& counters_pmc0,
-                         const std::vector<std::string>& counters_pmc1 = {});
+    uint64_t dispatch_kernel(uint64_t                        kernel_id,
+                             const std::vector<std::string>& counters_pmc0,
+                             const std::vector<std::string>& counters_pmc1 = {});
 
     static std::string convert_counters_per_pmc_to_str(const std::vector<std::vector<std::string>>& counters_per_pmc);
     static std::string convert_counters_to_str(const std::vector<std::string>& counters);
@@ -21,4 +21,7 @@ protected:
     std::shared_ptr<MockSdkWrapper>                             m_sdk_wrapper;
     std::shared_ptr<rocprofiler_compute_tool::SdkCallbacksImpl> m_sdk_callbacks;
     std::unique_ptr<rocprofiler_compute_tool::tool_data_t>      m_tool_data;
+    const std::vector<std::string> m_counters_pmc0     = {"counter0", "counter1"};
+    const std::vector<std::string> m_counters_pmc1     = {"counter2"};
+    const uint64_t                 m_invalid_config_id = ~0u;
 };
