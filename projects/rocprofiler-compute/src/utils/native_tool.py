8518c36eee50b6b6b156ea47885fba964c02dcb1
svolkov-amd <stanislav.volkov@amd.com>
2026-04-17 20:28:55 +0100

    swapped order of search + build stub

--- projects/rocprofiler-compute/src/utils/native_tool.py
+++ projects/rocprofiler-compute/src/utils/native_tool.py
@@ -1,7 +1,6 @@
 # Copyright (c) Advanced Micro Devices, Inc.
 # SPDX-License-Identifier:  MIT
 import shlex
-import tempfile
 from pathlib import Path
 
 from utils.logger import console_debug, console_error
@@ -30,50 +29,17 @@ class NativeTool:
             compute_script_path, rocprofiler_sdk_tool_path
         )
         if not native_tool_path:
-            # Build native counter collection tool if not exists
-            native_tool_path = str(
-                Path(tempfile.mkdtemp(prefix="rocprofiler-compute-tool-", dir="/tmp"))
-                / "librocprofiler-compute-tool.so"
+            native_tool_path = self.__build_collector(
+                compute_script_path, rocprofiler_sdk_tool_path
             )
-            native_tool_cpp_path = Path(__file__).resolve().parents[1] / "lib"
-            link_libraries = ("rocprofiler-sdk",)
-            build_command = (
-                # Create shared object
-                "hipcc -shared -fPIC "
-                # Link with dependant libraries
-                + " ".join(f"-l{lib}" for lib in link_libraries)
-                + " "
-                # Compliler flags
-                "-std=c++17 -W -Wall -Wextra -Wshadow -O2 "
-                # rocprofiler sdk library path
-                f"-L {str(Path(rocprofiler_sdk_tool_path).parent.parent)} "
-                # native tool source files (tool.cpp and helper.cpp)
-                f"{native_tool_cpp_path}/"
-                "rocprofiler_compute_tool.cpp "
-                f"{native_tool_cpp_path}/"
-                "helper.cpp "
-                # temporary shared object for native tool
-                f"-o {native_tool_path}"
-            )
-            console_debug(f"Building native tool using command: {build_command}")
-            success, output = capture_subprocess_output(shlex.split(build_command))
-            console_debug(f"Build output: {output}")
-            if not success:
-                console_error(
-                    "Failed to use native counter collection tool.\n"
-                    "Could not find source .cpp files in folder: "
-                    f"{native_tool_cpp_path}\n"
-                    "Please ensure the native tool library is installed "
-                    "or source files are present."
-                )
         return Path(native_tool_path)
 
     def __find_existing_collector(
         self, compute_script_path: Path, rocprofiler_sdk_tool_path: Path
     ) -> Path | None:
-        collector_path = self.__find_installed_collector(rocprofiler_sdk_tool_path)
+        collector_path = self.__find_built_collector(compute_script_path)
         if not collector_path:
-            collector_path = self.__find_built_collector(compute_script_path)
+            collector_path = self.__find_installed_collector(rocprofiler_sdk_tool_path)
         return collector_path
 
     def __find_installed_collector(
@@ -106,3 +72,19 @@ class NativeTool:
     def __find_file_by_glob_pattern(self, base_path: Path, pattern: str) -> Path | None:
         match = next(base_path.glob(pattern), None)
         return Path(match) if match is not None else None
+
+    def __build_collector(
+        self, native_tool_path: Path, rocprofiler_sdk_tool_path: Path
+    ) -> Path:
+        # Build native counter collection tool if not exists
+        build_command = ""
+        console_debug(f"Building native tool using command: {build_command}")
+        success, output = capture_subprocess_output(shlex.split(build_command))
+        console_debug(f"Build output: {output}")
+        if not success:
+            console_error(
+                "Failed to use native counter collection tool.\n"
+                "Please ensure the native tool library is installed "
+                "or source files are present."
+            )
+        return native_tool_path
