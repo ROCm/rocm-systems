278a11350d3892efd911cefb778aa44b96a368c6
Alexey Sachkov <alexey.sachkov@amd.com>
2026-04-20 10:01:18 +0200

    Apply comments

--- projects/clr/rocclr/platform/command.hpp
+++ projects/clr/rocclr/platform/command.hpp
@@ -217,8 +217,8 @@ class Event : public RuntimeObject {
   }
 
   //! Set dependent hardware events
-  void setDepHwEvents(std::vector<void*> hw_events) {
-    dep_hw_events_ = std::move(hw_events);
+  void setDepHwEvents(const std::vector<void*> &hw_events) {
+    dep_hw_events_ = hw_events;
   }
 
   //! Get dependent hardware events
