94d4a6a09330d603e57507fea54cd8ac7714e3a0
Aurelien Bouteiller <abouteil@amd.com>
2026-04-21 13:02:18 -0400

    Change sdma tuning to 256, its best for w1z1

--- projects/rocshmem/src/envvar.cpp
+++ projects/rocshmem/src/envvar.cpp
@@ -212,7 +212,7 @@ namespace envvar {
 
   namespace sdma {
     const var<bool> enabled("ENABLED", "Enable SDMA transport at runtime", true);
-    const var<size_t> threshold("THRESHOLD", "SDMA transfer size threshold in bytes", 128);
+    const var<size_t> threshold("THRESHOLD", "SDMA transfer size threshold in bytes", 256);
     const var<int32_t> num_channels("NUM_CHANNELS", "Number of SDMA channels per destination", 1);
   }  // namespace sdma
 
