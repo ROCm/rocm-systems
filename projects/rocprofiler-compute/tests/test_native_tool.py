258a69868aab9a67d22066fa5422ceaeda19b746
svolkov-amd <stanislav.volkov@amd.com>
2026-04-23 10:45:45 +0100

    test_native_tool refactor

--- projects/rocprofiler-compute/tests/test_native_tool.py
+++ projects/rocprofiler-compute/tests/test_native_tool.py
@@ -10,6 +10,22 @@ from utils.native_tool import NativeTool
 
 
 class TestNativeTool:
+    def test_when_incorrect_compute_path_provided__asserts(
+        self, installed_sdk_tool_path: Path
+    ):
+        lib_path = None
+        with pytest.raises(AssertionError):
+            lib_path = NativeTool(Path("incorrect_path"), installed_sdk_tool_path)
+        assert lib_path is None
+
+    def test_when_incorrect_opt_path_provided__asserts(
+        self, installed_compute_path: Path
+    ):
+        lib_path = None
+        with pytest.raises(AssertionError):
+            lib_path = NativeTool(installed_compute_path, Path("incorrect_path"))
+        assert lib_path is None
+
     def test_when_run_from_opt__finds_prebuilt_native_collector(
         self,
         installed_lib_path: Path,
@@ -21,7 +37,7 @@ class TestNativeTool:
         ).get_collector_library_path()
         assert lib_path == installed_lib_path
 
-    def test_when_run_from_opt_and_doesnt_find_prebuilt_native_collector__throws(
+    def test_when_run_from_opt_and_doesnt_find_prebuilt_native_collector__raises(
         self, installed_compute_path: Path, installed_sdk_tool_path
     ):
         with pytest.raises(RuntimeError):
@@ -29,41 +45,10 @@ class TestNativeTool:
                 installed_compute_path, installed_sdk_tool_path
             ).get_collector_library_path()
 
-    def test_when_opt_lib_doesnt_exist_but_built_lib_exists__finds_it(
-        self,
-        installed_sdk_tool_path: Path,
-        sources_compute_path: Path,
-        sources_lib_path: Path,
-    ):
-        lib_path = NativeTool(
-            sources_compute_path, installed_sdk_tool_path
-        ).get_collector_library_path()
-        assert lib_path == sources_lib_path
-
-    def test_when_incorrect_compute_path_provided__asserts(
-        self, installed_sdk_tool_path: Path
-    ):
-        lib_path = None
-        with pytest.raises(AssertionError):
-            lib_path = NativeTool(
-                Path("incorrect_compute_path"), installed_sdk_tool_path
-            ).get_collector_library_path()
-        assert lib_path is None
-
-    def test_when_incorrect_opt_path_provided__asserts(
-        self, installed_compute_path: Path
-    ):
-        lib_path = None
-        with pytest.raises(AssertionError):
-            lib_path = NativeTool(
-                installed_compute_path, Path("incorrect_rocm_path")
-            ).get_collector_library_path()
-        assert lib_path is None
-
-    def test_when_no_lib_exists__builds_it(
+    def test_when_run_from_source_dir__builds_and_returns_collector(
         self, sources_path, installed_sdk_tool_path: Path, sources_compute_path: Path
     ):
-        def mock_build_collector(_src_path: Path) -> None:
+        def mock_build_collector(_: Path) -> None:
             self.__create_file(sources_path, Path(NativeTool.lib_relative_path))
 
         with (
@@ -76,6 +61,19 @@ class TestNativeTool:
                 sources_compute_path, installed_sdk_tool_path
             ).get_collector_library_path()
         assert lib_path == sources_path / NativeTool.lib_relative_path
+
+    def test_when_run_from_source_dir_and_generation_fails__returns_none(
+        self, installed_sdk_tool_path: Path, sources_compute_path: Path
+    ):
+        lib_path = None
+        with (
+            patch.object(NativeTool, "_generate_cmake_project", return_value=False),
+            patch.object(NativeTool, "_build_cmake_project", return_value=False),
+        ):
+            lib_path = NativeTool(
+                sources_compute_path, installed_sdk_tool_path
+            ).get_collector_library_path()
+        assert lib_path == None
 
     @pytest.fixture
     def rocm_path(self, tmp_path: Path) -> Path:
