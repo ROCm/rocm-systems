c3bb80ede812fdbc787196f07ba55a1e1ee61616
Jordan Patterson <jordan.patterson@amd.com>
2026-04-28 13:34:23 -0600

    async: Fix use after free race.

--- projects/hipfile/src/amd_detail/backend/fallback.cpp
+++ projects/hipfile/src/amd_detail/backend/fallback.cpp
@@ -235,8 +235,9 @@ async_io_bind_params(void *userargs)
 void
 async_io_cleanup(void *userargs)
 {
-    auto     op                = static_cast<AsyncOpFallback *>(userargs);
-    ssize_t *bytes_transferred = op->bytes_transferred;
+    auto     op                         = static_cast<AsyncOpFallback *>(userargs);
+    ssize_t *bytes_transferred          = op->bytes_transferred;
+    ssize_t  bytes_transferred_internal = op->bytes_transferred_internal;
     try {
         Context<AsyncMonitor>::get()->completeOp(op);
     }
@@ -244,7 +245,7 @@ async_io_cleanup(void *userargs)
         *bytes_transferred = -hipFileInternalError;
         return;
     }
-    *bytes_transferred = op->bytes_transferred_internal;
+    *bytes_transferred = bytes_transferred_internal;
 }
 
 void
