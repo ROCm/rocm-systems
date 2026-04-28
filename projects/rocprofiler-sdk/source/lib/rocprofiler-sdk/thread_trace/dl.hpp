dc5c7105d0009a7d4f6720ba6a72037b05cd2caf
Giovanni <gbaraldi@amd.com>
2026-04-28 16:17:30 -0500

    fix: update dl.hpp to use new aqlprofile include path
    Develop migrated aqlprofile from lib/rocprofiler-sdk/aql/aql_profile_v2.h
    to lib/aqlprofile/aqlprofile.hpp (PR #4933). Update the include in dl.hpp
    to match.

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/dl.hpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/dl.hpp
@@ -22,7 +22,7 @@
 
 #pragma once
 
-#include "lib/rocprofiler-sdk/aql/aql_profile_v2.h"
+#include "lib/aqlprofile/aqlprofile.hpp"
 #include "lib/rocprofiler-sdk/thread_trace/trace_decoder_api.h"
 
 namespace rocprofiler
