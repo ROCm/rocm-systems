98089352650ada90fff2737e99190f1471522df3
svolkov-amd <stanislav.volkov@amd.com>
2026-04-29 10:39:37 +0100

    storing test mock

--- projects/rocprofiler-compute/src/lib/pc_sampling_collector/tests/test_pc_sampling_collector.h
+++ projects/rocprofiler-compute/src/lib/pc_sampling_collector/tests/test_pc_sampling_collector.h
@@ -10,6 +10,8 @@ class test_pc_sampling_collector_t : public ::testing::Test
 protected:
 	void SetUp() override;
 
-	std::shared_ptr<mock_code_object_translator_t> m_translator;
-	rocm_compute::pc_sampling_collector_impl_t::ptr m_pc_sampling_collector;
+	std::shared_ptr<mock_code_object_translator_t>       m_translator;
+	rocm_compute::pc_sampling_collector_impl_t::ptr      m_pc_sampling_collector;
+    rocprofiler_callback_tracing_code_object_load_data_t m_mem_info = {};
+    rocprofiler_callback_tracing_code_object_load_data_t m_file_info = {};
 };
