d6762429afb23c66f445ffbeb8b41505d8249a8c
svolkov-amd <stanislav.volkov@amd.com>
2026-04-29 10:57:13 +0100

    get_symbol_map function

--- projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_translator.cpp
+++ projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_translator.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) Advanced Micro Devices, Inc.
 // SPDX-License-Identifier:  MIT
 #include "code_object_translator.h"
+#include "rocprofiler-sdk/cxx/codeobj/code_printing.hpp"
 
 
 using namespace rocm_compute;
@@ -9,6 +10,8 @@ code_object_translator_impl_t::code_object_translator_impl_t()
     : m_translator(std::make_unique<rocprofiler::sdk::codeobj::disassembly::CodeobjAddressTranslate>())
 {
 }
+
+code_object_translator_impl_t::~code_object_translator_impl_t() = default;
 
 void code_object_translator_impl_t::add_code_object(const char* filepath, size_t id, uint64_t load_addr, uint64_t mem_size)
 {
@@ -29,4 +32,9 @@ void code_object_translator_impl_t::add_code_object(uint64_t memory_base,
 std::vector<size_t> code_object_translator_impl_t::get_code_object_ids() const
 {
     return m_code_object_ids;
+}
+
+void code_object_translator_impl_t::get_symbol_map(size_t object_id) const
+{
+    const auto& symbols = m_translator->getSymbolMap(object_id);
 }
--- projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_translator.h
+++ projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_translator.h
@@ -1,8 +1,13 @@
 // Copyright (c) Advanced Micro Devices, Inc.
 // SPDX-License-Identifier:  MIT
 #pragma once
+
 #include <memory>
-#include "rocprofiler-sdk/cxx/codeobj/code_printing.hpp"
+#include <vector>
+
+namespace rocprofiler::sdk::codeobj::disassembly {
+class CodeobjAddressTranslate;
+}
 
 namespace rocm_compute
 {
@@ -11,30 +16,31 @@ class code_object_translator_t
 public:
     virtual ~code_object_translator_t() = default;
     virtual void add_code_object(const char* filepath, size_t id, uint64_t load_addr, uint64_t mem_size) = 0;
-    virtual void add_code_object(uint64_t data,
-                                 size_t   memory_size,
-                                 size_t   id,
-                                 uint64_t load_addr,
-                                 uint64_t mem_size) = 0;
-    virtual std::vector<size_t> get_code_object_ids() const = 0;
-
+    virtual void                add_code_object(uint64_t data,
+                                                size_t   memory_size,
+                                                size_t   id,
+                                                uint64_t load_addr,
+                                                uint64_t mem_size) = 0;
+    virtual std::vector<size_t> get_code_object_ids() const        = 0;
 };
 
 class code_object_translator_impl_t : public code_object_translator_t
 {
 public:
     code_object_translator_impl_t();
+    ~code_object_translator_impl_t() override;
     void add_code_object(const char* filepath, size_t id, uint64_t load_addr, uint64_t mem_size) override;
-    void add_code_object(uint64_t memory_base,
-                         size_t   memory_size,
-                         size_t   id,
-                         uint64_t load_base,
-                         uint64_t load_size) override;
+    void                add_code_object(uint64_t memory_base,
+                                        size_t   memory_size,
+                                        size_t   id,
+                                        uint64_t load_base,
+                                        uint64_t load_size) override;
     std::vector<size_t> get_code_object_ids() const override;
+    void                get_symbol_map(size_t object_id) const;
 
 private:
     std::unique_ptr<rocprofiler::sdk::codeobj::disassembly::CodeobjAddressTranslate> m_translator;
     std::vector<size_t> m_code_object_ids;
 };
 
-}  // namespace pc_sampling_collector
+}  // namespace rocm_compute
