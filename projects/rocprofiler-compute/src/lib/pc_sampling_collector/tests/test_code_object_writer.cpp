1b081b9eee9695561e1e2db83840b8b613d7f6ed
svolkov-amd <stanislav.volkov@amd.com>
2026-04-29 14:36:43 +0100

    red: check for result being non-empty.

--- projects/rocprofiler-compute/src/lib/pc_sampling_collector/tests/test_code_object_writer.cpp
+++ projects/rocprofiler-compute/src/lib/pc_sampling_collector/tests/test_code_object_writer.cpp
@@ -1,10 +1,12 @@
 // Copyright (c) Advanced Micro Devices, Inc.
 // SPDX-License-Identifier:  MIT
 #include "test_code_object_writer.h"
+#include "nlohmann/json.hpp"
 
-TEST_F(test_code_object_writer_t, Dummy)
+TEST_F(test_code_object_writer_t, ProvidedNoData_ReturnsMinimalJson)
 {
-    
+    const auto& result = m_writer.get_result();
+    EXPECT_TRUE(!result.empty());
 }
 
 void test_code_object_writer_t::SetUp()
