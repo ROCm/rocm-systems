631c2cce4e02009bd70f7a24a331e7215e545a9d
Milan Radosavljevic <milan.radosavljevic@amd.com>
2026-04-29 15:36:53 +0200

    Revert 'try to fix shmem test'

--- projects/rocprofiler-systems/tests/pytest/test_shmem.py
+++ projects/rocprofiler-systems/tests/pytest/test_shmem.py
@@ -110,7 +110,6 @@ class TestShmem(RocprofsysTest):
         if mode == "sys_run":
             self.assert_perfetto(
                 result,
-                categories=["shmem"],
                 labels=["shmem_pingpong", "start_pes"],
                 counts=[1, 1],
                 depths=[0, 1],
