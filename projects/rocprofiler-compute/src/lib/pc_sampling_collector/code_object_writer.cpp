1e5d1fed6a2601c604e86256cbe2d0536b67e0f7
svolkov-amd <stanislav.volkov@amd.com>
2026-04-29 16:00:40 +0100

    refactor of error handling

--- projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_writer.cpp
+++ projects/rocprofiler-compute/src/lib/pc_sampling_collector/code_object_writer.cpp
@@ -2,53 +2,36 @@
 // SPDX-License-Identifier:  MIT
 #include "code_object_writer.h"
 
+#include "gsl_assert.h"
 #include "nlohmann/json.hpp"
 
 using namespace rocm_compute;
 
 void code_object_writer_json_t::start_code_obj(size_t obj_id)
 {
-    if (m_code_object_closure_count != 0)
-    {
-        throw std::runtime_error("Code object description is not properly closed");
-    }
+    Expects(m_code_object_closure_count == 0);
     ++m_code_object_closure_count;
     m_code_objects.push_back(code_object_entry_t{obj_id, {}});
 }
 
 void code_object_writer_json_t::end_code_obj()
 {
-    if (m_code_object_closure_count == 0)
-    {
-        throw std::runtime_error("Code object description is not properly opened");
-    }
-    if (m_symbol_closure_count != 0)
-    {
-        throw std::runtime_error("Symbol description is not properly closed");
-    }
+    Expects(m_code_object_closure_count != 0);
+    Expects(m_symbol_closure_count == 0);
     --m_code_object_closure_count;
 }
 
 void code_object_writer_json_t::start_symbol(const symbol_t& symbol)
 {
-    if (m_code_object_closure_count == 0)
-    {
-        throw std::runtime_error("Symbol must be opened inside a code object");
-    }
-    if (m_symbol_closure_count != 0)
-    {
-        throw std::runtime_error("Symbol description is not properly closed");
-    }
+    Expects(m_code_object_closure_count != 0);
+    Expects(m_symbol_closure_count == 0);
     ++m_symbol_closure_count;
     m_code_objects.back().symbols.push_back(symbol);
 }
 
 void code_object_writer_json_t::end_symbol()
 {
-    if (m_symbol_closure_count == 0)
-    {
-        throw std::runtime_error("Symbol description is not properly opened");
-    }
+    Expects(m_symbol_closure_count != 0);
     --m_symbol_closure_count;
 }
 
@@ -56,14 +39,8 @@ void code_object_writer_json_t::write_instruction(const instruction_t& inst) {}
 
 std::string code_object_writer_json_t::get_result()
 {
-    if (m_code_object_closure_count != 0)
-    {
-        throw std::runtime_error("Code object description is not properly closed");
-    }
-    if (m_symbol_closure_count != 0)
-    {
-        throw std::runtime_error("Symbol description is not properly closed");
-    }
+    Expects(m_code_object_closure_count == 0);
+    Expects(m_symbol_closure_count == 0);
 
     auto code_objects = nlohmann::json::array();
     for (const auto& entry : m_code_objects)
