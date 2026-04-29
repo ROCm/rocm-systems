bf496a27d1e3382a89b2b6e54aab6da8585515fa
Jatin Chaudhary <51944368+cjatin@users.noreply.github.com>
2026-04-29 15:51:41 +0100

    Fix the spelling mistake

--- projects/clr/hipamd/include/hip/amd_detail/hip_cooperative_groups_helper.h
+++ projects/clr/hipamd/include/hip/amd_detail/hip_cooperative_groups_helper.h
@@ -248,14 +248,14 @@ __CG_STATIC_QUALIFIER__ void barrier_wait() {
 
 namespace tiled_group {
 
-// enforce ordering for memory intructions
+// enforce ordering for memory instructions
 __CG_STATIC_QUALIFIER__ void sync() { __builtin_amdgcn_fence(__ATOMIC_ACQ_REL, "wavefront"); }
 
 }  // namespace tiled_group
 
 namespace coalesced_group {
 
-// enforce ordering for memory intructions
+// enforce ordering for memory instructions
 __CG_STATIC_QUALIFIER__ void sync() { __builtin_amdgcn_fence(__ATOMIC_ACQ_REL, "wavefront"); }
 
 // Masked bit count
