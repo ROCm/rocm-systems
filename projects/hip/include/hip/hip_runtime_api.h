61422ddfa6e8ee1c91b9920971cd8d6096cd10ea
Julia Jiang <56359287+jujiang-del@users.noreply.github.com>
2026-05-01 12:09:55 -0400

    Update hip_runtime_api.h

--- projects/hip/include/hip/hip_runtime_api.h
+++ projects/hip/include/hip/hip_runtime_api.h
@@ -584,14 +584,14 @@ typedef enum hipDeviceAttribute_t {
                                                   ///< units for the device
   hipDeviceAttributeFineGrainSupport,  ///< '1' if Device supports fine grain, '0' otherwise
   hipDeviceAttributeWallClockRate,     ///< Constant frequency of wall clock in kilohertz.
-  hipDeviceAttributeNumberOfXccs,           ///< The number of Accelerated Compute Cores(XCCs)
-                                            ///< in the device's current compute partition.\n
-                                            ///< An XCC is AMD's fundamental GPU compute tile.\n
-                                            ///< Devices such as MI300X contain
-                                            ///< multiple XCCs per package.\n
-                                            ///< The reported value depends on the partition mode
-                                            ///< configured at boot time.\n
-                                            ///< AMD-specific attribute; no CUDA equivalent.
+  hipDeviceAttributeNumberOfXccs,  ///< Number of XCCs in the device’s current
+                                   ///< compute partition (XCP).
+                                   ///< An XCC is AMD’s fundamental GPU compute tile.
+                                   ///< Devices like MI300X include multiple XCCs
+                                   ///< per package. Value varies by boot‑time
+                                   ///< partition mode (compute or memory).
+                                   ///< Partition modes can be set using amd‑smi.
+                                   ///< AMD-specific; no CUDA equivalent.
   hipDeviceAttributeMaxAvailableVgprsPerThread,  ///< Max number of available (directly or
                                                  ///< indirectly addressable) VGPRs per thread in
                                                  ///< DWORDs.
