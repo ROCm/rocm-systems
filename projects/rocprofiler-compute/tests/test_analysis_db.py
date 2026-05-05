8757c7651c0201bac06c99ea5e6b9ee12e30488c
Xuan Chen <xuchen@amd.com>
2026-05-05 19:58:40 +0000

    address comments

--- projects/rocprofiler-compute/tests/test_analysis_db.py
+++ projects/rocprofiler-compute/tests/test_analysis_db.py
@@ -7,6 +7,7 @@ from unittest.mock import MagicMock, patch
 
 import numpy as np
 import pandas as pd
+import pytest
 
 from rocprof_compute_analyze.analysis_db import db_analysis
 from utils import schema
