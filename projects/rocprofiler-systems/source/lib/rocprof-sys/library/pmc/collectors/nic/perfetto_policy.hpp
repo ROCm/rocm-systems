5c3a93047a0bc67ddf21dd3d74da554e95d3e5ab
Aleksandar Janicijevic <ajanicij@amd.com>
2026-05-05 14:15:07 -0400

    Removed extra lines of code that don't belong

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/pmc/collectors/nic/perfetto_policy.hpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/pmc/collectors/nic/perfetto_policy.hpp
@@ -364,10 +364,6 @@ struct perfetto_policy
                         "nic_tx_cnp_pkts",
                         counter_track::at(device_index, it->second.track_index), ts,
                         static_cast<double>(sample.metric_values.tx_rdma_cnp_pkts));
-                    TRACE_COUNTER(
-                        "nic_tx_rdma_ack_timeout",
-                        counter_track::at(device_index, it->second.track_index), ts,
-                        static_cast<double>(sample.metric_values.tx_rdma_ack_timeout));
                 }
             }
 
