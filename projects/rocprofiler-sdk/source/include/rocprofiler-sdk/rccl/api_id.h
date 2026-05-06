3e0af20be5df8f96926e80a75d5dad9177937824
Nikita Gusev <nikita.gusev2@amd.com>
2026-05-06 16:55:40 +0000

    Bump rocprofiler-sdk RCCL trace version and fix ncclCommRevoke placement
    Move ncclCommRevoke_fn to the end of rcclApiFuncTable for backward
    compatibility. Bump RCCL_API_TRACE_VERSION_PATCH 3 -> 4 and gate
    ncclCommRevoke under a new >= 4 version block in rccl.def.cpp.

--- projects/rocprofiler-sdk/source/include/rocprofiler-sdk/rccl/api_id.h
+++ projects/rocprofiler-sdk/source/include/rocprofiler-sdk/rccl/api_id.h
@@ -75,11 +75,13 @@ typedef enum rocprofiler_rccl_api_id_t  // NOLINT(performance-enum-size)
     ROCPROFILER_RCCL_API_ID_ncclCommShrink,
     ROCPROFILER_RCCL_API_ID_ncclCommWindowRegister,
     ROCPROFILER_RCCL_API_ID_ncclCommWindowDeregister,
-    ROCPROFILER_RCCL_API_ID_ncclCommRevoke,
 #endif
 #if RCCL_API_TRACE_VERSION_PATCH >= 3
     ROCPROFILER_RCCL_API_ID_ncclAlltoAll,
     ROCPROFILER_RCCL_API_ID_ncclAlltoAllv,
+#endif
+#if RCCL_API_TRACE_VERSION_PATCH >= 4
+    ROCPROFILER_RCCL_API_ID_ncclCommRevoke,
 #endif
     ROCPROFILER_RCCL_API_ID_LAST,
 } rocprofiler_rccl_api_id_t;
