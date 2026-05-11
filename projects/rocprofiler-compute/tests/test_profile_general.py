f2c2595ef64482ce7ab6721ec910765620e4a0a5
Yang Wang <ywang103@amd.com>
2026-05-11 13:30:55 -0400

    remove csv conversion for all test but any one more sigle test in csv

--- projects/rocprofiler-compute/tests/test_profile_general.py
+++ projects/rocprofiler-compute/tests/test_profile_general.py
@@ -57,8 +57,6 @@ DEFAULT_ABS_DIFF = 15
 DEFAULT_REL_DIFF = 50
 MAX_REOCCURING_COUNT = 28
 
-LIVE_ATTACH_OUTPUT_OPTIONS = ["--format-rocprof-output", "csv"]
-
 CSVS = sorted([
     "sysinfo.csv",
 ])
@@ -2041,14 +2039,66 @@ def test_comprehensive_error_paths():
 
 
 @pytest.mark.live_attach_detach
-def test_live_attach_detach_block(binary_handler_profile_rocprof_compute):
+def test_live_attach_detach_block_csv(binary_handler_profile_rocprof_compute):
     options = [
         "--block",
         "3.1.1",
         "4.1.1",
         "5.1.1",
-        *LIVE_ATTACH_OUTPUT_OPTIONS,
+        "--format-rocprof-output",
+        "csv",
     ]
+    workload_dir = common.get_output_dir()
+
+    # TODO: temp fix for sdk defautly disable attach/detach,
+    # remove after it sets default to enable
+    env = os.environ.copy()
+    env["ROCP_TOOL_ATTACH"] = "1"
+
+    process_workload = None
+
+    try:
+        # Start workload
+        process_workload = subprocess.Popen(config["app_hip_dynamic_shared"], env=env)
+        time.sleep(5)  # Give workload time to start
+
+        attach_detach = {
+            "attach_pid": process_workload.pid,
+            "attach-duration-msec": attach_detach_interval_msec_no_delay,
+        }
+
+        # Run profiler (might fail / timeout / throw)
+        binary_handler_profile_rocprof_compute(
+            config,
+            workload_dir,
+            options,
+            check_success=True,
+            roof=False,
+            app_name="app_hip_dynamic_shared",
+            attach_detach_para=attach_detach,
+        )
+
+    finally:
+        if process_workload and process_workload.poll() is None:
+            print(f"[finally] killing workload pid={process_workload.pid}")
+            process_workload.kill()
+            process_workload.wait()
+        # Clean up any stale rocprof-attach processes to prevent interference
+        # with subsequent tests.
+        subprocess.run(
+            ["pkill", "-9", "-f", "rocprof-attach"],
+            capture_output=True,
+        )
+
+    # Validate results
+    file_dict = common.check_csv_files(workload_dir, 1, num_kernels)
+    validate(inspect.stack()[0][3], workload_dir, file_dict)
+    common.clean_output_dir(config["cleanup"], workload_dir)
+
+
+@pytest.mark.live_attach_detach
+def test_live_attach_detach_block(binary_handler_profile_rocprof_compute):
+    options = ["--block", "3.1.1", "4.1.1", "5.1.1"]
     workload_dir = common.get_output_dir()
 
     # TODO: temp fix for sdk defautly disable attach/detach,
@@ -2103,7 +2153,7 @@ def test_live_attach_detach_block(binary_handler_profile_rocprof_compute):
 )
 @pytest.mark.live_attach_detach
 def test_live_attach_detach_block_thread_sleep(binary_handler_profile_rocprof_compute):
-    options = ["--block", "3.1.1", "4.1.1", "5.1.1", *LIVE_ATTACH_OUTPUT_OPTIONS]
+    options = ["--block", "3.1.1", "4.1.1", "5.1.1"]
     workload_dir = common.get_output_dir()
 
     # TODO: temp fix for sdk defautly disable attach/detach,
@@ -2168,7 +2218,7 @@ def test_live_attach_detach_block_thread_sleep(binary_handler_profile_rocprof_co
 def test_live_attach_detach_singlepass_launch_stats(
     binary_handler_profile_rocprof_compute,
 ):
-    options = ["--set", "launch_stats", *LIVE_ATTACH_OUTPUT_OPTIONS]
+    options = ["--set", "launch_stats"]
     workload_dir = common.get_output_dir()
 
     # TODO: temp fix for sdk defautly disable attach/detach,
@@ -2222,13 +2272,14 @@ def test_live_attach_detach_singlepass_launch_stats(
     # Check that launch-stat sets were applied
     config_file = f"{workload_dir}/profiling_config.yaml"
     for tag in [
-        "7.2.0",
-        "7.2.1",
-        "7.3.0",
-        "7.2.2",
-        "7.2.3",
-        "7.2.4",
-        "7.2.5",
+        "7.1.0",
+        "7.1.1",
+        "7.1.2",
+        "7.1.5",
+        "7.1.6",
+        "7.1.7",
+        "7.1.8",
+        "7.1.9",
     ]:
         assert common.check_file_pattern(f"- {tag}", config_file)
 
@@ -2241,7 +2292,7 @@ def test_live_attach_detach_pc_sampling(
 ):
     skip_unsupported_pc_sampling_soc(is_stochastic=True)
 
-    options = ["-b", "21", *LIVE_ATTACH_OUTPUT_OPTIONS]
+    options = ["-b", "21"]
     workload_dir = common.get_output_dir()
 
     # TODO: temp fix for sdk defautly disable attach/detach,
