9535cd4bbb30c26291499da1919310027beb15e1
ajanicijamd <ajanicij@amd.com>
2026-05-13 13:32:34 -0400

    Update projects/rocprofiler-systems/source/lib/rocprof-sys/library/pmc/collectors/common/settings.hpp
    Co-authored-by: David Galiffi <David.Galiffi@amd.com>

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/pmc/collectors/common/settings.hpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/pmc/collectors/common/settings.hpp
@@ -132,7 +132,7 @@ struct settings_policy
     /**
      * @brief Get NIC enabled metrics.
      *
-     * For NIC, all 10 RDMA metrics are enabled when NIC sampling is active.
+     * For NIC, all RDMA metrics are enabled when NIC sampling is active.
      */
     static nic::enabled_metrics get_nic_enabled_metrics() noexcept
     {
