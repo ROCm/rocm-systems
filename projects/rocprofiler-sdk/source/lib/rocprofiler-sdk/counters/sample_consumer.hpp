b0a754abfc274241a29dfc69fe2aff8d8462deee
Giovanni <gbaraldi@amd.com>
2026-05-14 13:35:53 -0500

    Further optimize

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/counters/sample_consumer.hpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/counters/sample_consumer.hpp
@@ -73,18 +73,19 @@ public:
 
     void add(DataType&& params)
     {
-        std::unique_lock<std::mutex> lk(mut);
-
-        if(read_ptr + buffer.size() <= write_ptr || !valid)
+        bool selfconsume = true;
         {
-            // If not possible to use consumer thread, process with this thread
-            consume_fn(std::move(params));
-            return;
+            auto lk = std::unique_lock{mut};
+            if(valid && read_ptr + buffer.size() > write_ptr)
+            {
+                buffer.at(write_ptr.fetch_add(1) % buffer.size()) = std::move(params);
+                selfconsume = false;
+            }
         }
-
-        buffer.at(write_ptr % buffer.size()) = std::move(params);
-        write_ptr.fetch_add(1);
-        cv.notify_all();
+        if (selfconsume)
+            consume_fn(std::move(params));
+        else
+            cv.notify_all();
     }
 
 protected:
@@ -92,12 +93,13 @@ protected:
     {
         while(true)
         {
-            while(read_ptr == write_ptr)
             {
-                std::unique_lock<std::mutex> lk(mut);
+                auto lk = std::unique_lock{mut};
                 cv.wait(lk, [&] { return read_ptr != write_ptr || !valid; });
-                if(!valid && read_ptr == write_ptr)
+                if(read_ptr == write_ptr)
                 {
+                    if (valid) continue;
+
                     exited.store(true);
                     cv.notify_all();
                     return;
