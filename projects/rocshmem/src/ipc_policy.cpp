ae0dc8083d6a304312643dd4107708fa03c3cbc7
Aurelien Bouteiller <abouteil@amd.com>
2026-05-14 10:34:40 -0400

    Fix warning unused gda_provider

--- projects/rocshmem/src/ipc_policy.cpp
+++ projects/rocshmem/src/ipc_policy.cpp
@@ -188,7 +188,6 @@ __host__ void IpcOnImpl::ipcHostInit(int my_pe, const HEAP_BASES_T &heap_bases,
       mpilib_ftable_.Group_free(&thread_grp);
     }
   }
-
 }
 
 __host__ void IpcOnImpl::ipcHostInit(int my_pe, const HEAP_BASES_T &heap_bases,
