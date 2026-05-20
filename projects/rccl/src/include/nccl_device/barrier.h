f97b422c8c7f69aa7370c7350c2b2cfa83117b1b
root <root@ctr-cx65-mi300x-3.adc.amd.com>
2026-05-20 15:49:44 +0000

    Unify HIP/CUDA barrier device code with hip_compat.h

--- projects/rccl/src/include/nccl_device/barrier.h
+++ projects/rccl/src/include/nccl_device/barrier.h
@@ -11,7 +11,7 @@
 #include "impl/lsa_barrier__types.h"
 #include "impl/gin_barrier__types.h"
 
-#if NCCL_DEVICE_COMPILE
+#if __CUDACC__
 template<typename Coop>
 struct ncclBarrierSession_internal;
 
@@ -41,11 +41,7 @@ struct ncclBarrierSession: ncclBarrierSession_internal<Coop> {
   NCCL_DEVICE_INLINE ncclLsaBarrierSession<Coop>& lsaBarrier();
   NCCL_DEVICE_INLINE ncclGinBarrierSession<Coop>& ginBarrier();
 
-#if __HIP_PLATFORM_AMD__
-  NCCL_DEVICE_INLINE void sync(Coop, std::memory_order, ncclGinFenceLevel);
-#else
   NCCL_DEVICE_INLINE void sync(Coop, cuda::memory_order, ncclGinFenceLevel);
-#endif
 };
 #endif
 
