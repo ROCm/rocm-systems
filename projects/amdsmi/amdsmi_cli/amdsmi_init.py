0aff826e87240fc9955f102ba42e481765d9c53f
josnarlo <joseph.narlo@amd.com>
2026-05-20 13:24:48 -0500

    Change error code base from 200 to 192
    Signed-off-by: josnarlo <joseph.narlo@amd.com>

--- projects/amdsmi/amdsmi_cli/amdsmi_init.py
+++ projects/amdsmi/amdsmi_cli/amdsmi_init.py
@@ -38,10 +38,11 @@ sys.path.insert(0, python_lib_path)
 try:
     from amdsmi import amdsmi_interface, amdsmi_exception
 except ImportError as e:
-    print(f"Unhandled import error: {e}")
+    error_code = 192
     print("Failed to import the amdsmi Python library. Ensure it is installed in Python.")
     print(f"Alternatively, verify that the library is in the path:\n{python_lib_path}")
-    sys.exit(200)
+    print(f"Unhandled import error: {e}. Error code: {error_code}", file=sys.stderr)
+    sys.exit(error_code)
 
 # Using basic python logging for user errors and development
 logging.basicConfig(format="%(levelname)s: %(message)s", level=logging.ERROR)  # User level logging
@@ -155,7 +156,7 @@ def amdsmi_cli_init():
             logging.error(
                 "Drivers not loaded (amdgpu, amd_hsmp, ionic, rdma drivers not found in modules)"
             )
-            sys.exit(34)
+            sys.exit(amdsmi_interface.amdsmi_wrapper.AMDSMI_STATUS_DRIVER_NOT_LOADED)
         else:
             raise e
     elif init_result["exception"] is not None:
