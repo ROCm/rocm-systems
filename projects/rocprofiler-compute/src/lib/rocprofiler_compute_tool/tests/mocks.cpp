9b58611a4a28326657e495941e10ab97a709de44
svolkov-amd <stanislav.volkov@amd.com>
2026-06-02 05:28:21 +0100

    [rocprofiler-compute] Support for code objects tracing in native collector (#5482)
    ## Motivation
    This PR adds code object collection and parsing into ISA to the native
    collector. This is required to show PC Sampling data atop of a full
    kernel ISA listing.
    
    ## Technical Details
    - New `pc_sampling_collector` module has been added which parses and
    dumps kernel objects information.
    - Additional tests are added both for new and existing classes.
    - Some refactoring according to match coding style in rocprof-systems
    and to improve code quality somewhat.
    
    Notes:
    - This PR doesn't include python code changes necessary to enable a new
    mode. Therefore, therefore to access this mode environment variables
    should be set manually.
    - Example of output:
    <img width="667" height="607" alt="image"
    src="https://github.com/user-attachments/assets/7615edbf-a8e7-4e55-b37d-c04faee077a6"
    />
    
    
    ## JIRA ID
    - AIPROFCOMP-533
    - AIPROFCOMP-535
    
    ## Test Plan
    - Regression testing for iteration multiplexing.
    - Dump of code objects for mixbench.
    - Cmake tests as part of CI.
    
    ## Test Result
    TBD
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Co-authored-by: abchoudh-amd <abchoudh@amd.com>

--- projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/tests/mocks.cpp
+++ projects/rocprofiler-compute/src/lib/rocprofiler_compute_tool/tests/mocks.cpp
@@ -41,6 +41,26 @@ std::string_view MockInputParameters::get_kernel_filter_range()
     return std::string_view{m_kernel_filter_range};
 }
 
+std::string_view MockInputParameters::get_pc_sampling_method()
+{
+    return std::string_view{m_pc_sampling_method};
+}
+
+std::string_view MockInputParameters::get_pc_sampling_beta_enabled()
+{
+    return std::string_view{m_pc_sampling_beta_enabled};
+}
+
+void MockInputParameters::set_pc_sampling_method(const std::string& method)
+{
+    m_pc_sampling_method = method;
+}
+
+void MockInputParameters::set_pc_sampling_beta_enabled(const std::string& value)
+{
+    m_pc_sampling_beta_enabled = value;
+}
+
 void MockInputParameters::set_output_path(const std::string& output_path)
 {
     m_output_path     = output_path;
