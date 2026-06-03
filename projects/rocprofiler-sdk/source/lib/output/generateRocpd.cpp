f8d4b47b8b2c03b69d39d75d12cf7fa3ba388e00
Sam Crater <scrater@amd.com>
2026-06-03 16:25:26 +0000

    change empty value behavior from skipping to inserting <unknown>

--- projects/rocprofiler-sdk/source/lib/output/generateRocpd.cpp
+++ projects/rocprofiler-sdk/source/lib/output/generateRocpd.cpp
@@ -280,9 +280,11 @@ iterate_args_callback(rocprofiler_buffer_tracing_kind_t /*kind*/,
     ROCP_FATAL_IF(data == nullptr) << "nullptr to data for iterate_args_callback";
 
     auto* _data = static_cast<function_args_t*>(data);
-    if(arg_type && arg_name && arg_value_str && *arg_value_str != '\0')
+    if(arg_type && arg_name && arg_value_str) {
+        const char* arg_val = *arg_value_str != '\0' ? arg_value_str : "<unknown>";
         _data->emplace_back(
-            argument_info{arg_number, common::cxx_demangle(arg_type), arg_name, arg_value_str});
+            argument_info{arg_number, common::cxx_demangle(arg_type), arg_name, arg_val});
+    }
     return 0;
 }
 
