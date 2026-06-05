0905fe8a63c6639b25e1830b26bd2607f6fa40cd
Young Hui <young.hui@amd.com>
2026-06-05 14:12:22 -0400

    move otf2 to local import too, while we're at it

--- projects/rocprofiler-sdk/source/lib/python/rocpd/otf2.py
+++ projects/rocprofiler-sdk/source/lib/python/rocpd/otf2.py
@@ -106,6 +106,16 @@ def write_otf2(importData, config):
             "otf2 module not found. Please install it using 'pip install otf2' to convert to OTF2 format"
         ) from e
 
+    try:
+        import otf2
+        from otf2.enums import LocationType, LocationGroupType, RegionRole, Paradigm
+    except ImportError:
+        print(
+            "otf2 module not found. Conversion to OTF2 format is not possible.\n"
+            "Please install it using 'pip install otf2'"
+        )
+        return
+
     timer_resolution = 1_000_000_000
     trace_dir = getattr(config, "output_path", "./otf_traces")
     trace_file = f"{getattr(config, 'output_file', 'traces')}_results"
