63b2f8312f81183e746ca6f96d602f70e03b48d6
Alex Xu <alex.xu@amd.com>
2026-06-05 10:44:16 -0400

    switch to generic flavor for rdc

--- projects/rdc/docs/conf.py
+++ projects/rdc/docs/conf.py
@@ -18,7 +18,21 @@ with open('../CMakeLists.txt', encoding='utf-8') as f:
     version_number = match[1]
 
 html_theme = "rocm_docs_theme"
-html_theme_options = {"flavor": "rocm"}
+html_theme_options = {
+    "announcement": f"This is ROCm 7.13.0 technology preview release documentation. For the latest production stream release, refer to <a id='rocm-banner' href='https://rocm.docs.amd.com/en/latest/'>ROCm documentation</a>.",
+    "flavor": "generic",
+    "header_title": f"ROCm™ 7.13.0 Preview",
+    "header_link": f"https://rocm.docs.amd.com/en/7.13.0-preview/index.html",
+    "version_list_link": "",
+    "nav_secondary_items": {
+        "GitHub": "https://github.com/ROCm/ROCm",
+        "Community": "https://github.com/ROCm/ROCm/discussions",
+        "Blogs": "https://rocm.blogs.amd.com/",
+        "System and Infra Docs": "https://instinct.docs.amd.com/",
+        "Support": "https://github.com/ROCm/ROCm/issues/new/choose",
+    },
+    "link_main_doc": False,
+}
 html_title = f"RDC {version_number} documentation"
 external_toc_path = "./sphinx/_toc.yml"
 
