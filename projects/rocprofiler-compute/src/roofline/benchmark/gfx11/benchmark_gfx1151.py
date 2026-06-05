e91b35ca116a8641f8c074a011cab44e18bc2c69
Carrie Fallows <Carrie.Fallows@amd.com>
2026-06-05 02:03:43 -0400

    Changes requested in review- remove dupe lists
    Signed-off-by: Carrie Fallows <Carrie.Fallows@amd.com>

--- projects/rocprofiler-compute/src/roofline/benchmark/gfx11/benchmark_gfx1151.py
+++ projects/rocprofiler-compute/src/roofline/benchmark/gfx11/benchmark_gfx1151.py
@@ -18,19 +18,3 @@ from . import benchmark_gfx11_base
 class Bench_gfx1151(benchmark_gfx11_base.Bench_gfx11):
     def __init__(self, device_id: int, cache_sizes: dict) -> None:
         super().__init__(device_id, cache_sizes)
-
-        self.unsupported_data_types = [
-            "HBM",
-            "I8",
-            "I32",
-            "I64",
-            "WMMA-F4",
-            "WMMA-F6",
-            "WMMA-F6F4",
-            "WMMA-F8",
-            "WMMA-F16",
-            "WMMA-BF16",
-            "WMMA-F32",
-            "WMMA-F64",
-            "WMMA-I8",
-        ]
--- projects/rocprofiler-compute/src/roofline/benchmark/gfx11/benchmark_gfx1152.py
+++ projects/rocprofiler-compute/src/roofline/benchmark/gfx11/benchmark_gfx1152.py
@@ -19,19 +19,4 @@ class Bench_gfx1152(benchmark_gfx11_base.Bench_gfx11):
     def __init__(self, device_id: int, cache_sizes: dict) -> None:
         super().__init__(device_id, cache_sizes)
 
-        self.unsupported_data_types = [
-            "HBM",
-            "MALL",
-            "I8",
-            "I32",
-            "I64",
-            "WMMA-F4",
-            "WMMA-F6",
-            "WMMA-F6F4",
-            "WMMA-F8",
-            "WMMA-F16",
-            "WMMA-BF16",
-            "WMMA-F32",
-            "WMMA-F64",
-            "WMMA-I8",
-        ]
+        self.unsupported_data_types.append("MALL")
