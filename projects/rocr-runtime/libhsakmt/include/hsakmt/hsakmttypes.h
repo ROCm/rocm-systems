96177ca8813c5ed18894d9029b8557c17d154c9d
David Yat Sin <David.YatSin@amd.com>
2026-06-07 02:13:45 +0000

    rocr: Implement fabric support handle query

--- projects/rocr-runtime/libhsakmt/include/hsakmt/hsakmttypes.h
+++ projects/rocr-runtime/libhsakmt/include/hsakmt/hsakmttypes.h
@@ -278,7 +278,7 @@ typedef struct _HsaNodeProperties
                                        // e.g a "discrete HSA GPU"
     HSAuint32       NumFComputeCores;  // # of HSA throughtput (= GPU) FCompute cores ("SIMD") present in a node.
                                        // This value is 0 if no FCompute cores are present (e.g. pure "CPU node").
-    HSAuint32 NumNeuralCores;          // # of HSA neural processing units (= AIE) present in a
+    HSAuint32       NumNeuralCores;    // # of HSA neural processing units (= AIE) present in a
                                        // node. This value is 0 if there are no NeuralCores.
     HSAuint32       NumMemoryBanks;    // # of discoverable memory bank affinity properties on this "H-NUMA" node.
     HSAuint32       NumCaches;         // # of discoverable cache affinity properties on this "H-NUMA"  node.
@@ -356,6 +356,8 @@ typedef struct _HsaNodeProperties
     HSAuint32       LuidLowPart;       // Windows Locally Unique Identifier Low 4 bytes
     HSAuint32       LuidHighPart;      // Windows Locally Unique Identifier High 4 bytes
     HSAuint64       WallClockKHz;      // Wall Clock Frequency in KHz
+
+    HSAuint32       FabricHandleSupported; // 0 - not supported, 1 - supported
 } HsaNodeProperties;
 
 
