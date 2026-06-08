b985212cc06a0dadddeb35d2198e1aa2f87ffbf1
Mikhail <lloginov@amd.com>
2026-06-08 10:32:55 +0200

    add units.hpp

---
+++ projects/rocprofiler-systems/source/lib/common/units.hpp
@@ -0,0 +1,7 @@
+// Copyright (c) Advanced Micro Devices, Inc.
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+namespace rocprofsys::inline common::units
+{}  // namespace rocprofsys::inline common::units
