0f363cc7fb8c1e32496a51f352cadcc0ece0a647
David Yat Sin <David.YatSin@amd.com>
2026-06-10 23:44:05 +0000

    rocr: Limit queue error message prints in release builds

--- projects/rocr-runtime/runtime/hsa-runtime/core/util/utils.h
+++ projects/rocr-runtime/runtime/hsa-runtime/core/util/utils.h
@@ -177,6 +177,23 @@ static __forceinline unsigned long long int strtoull(const char* str,
 } while (false)
 #endif
 
+/* Limitted printing when in release mode, always prints when in debug mode*/
+#ifdef NDEBUG
+#define log_warning_n(limit, fmt, ...)                                                             \
+  do {                                                                                             \
+    fprintf(stderr, "Warning: " fmt, ##__VA_ARGS__);                                             \
+  } while (false)
+#else
+#define log_warning_n(limit, fmt, ...)                                                             \
+  do {                                                                                             \
+    static std::atomic<unsigned int> count(0);                                                     \
+    if (limit == 0 || count < limit) {                                                             \
+      fprintf(stderr, "Warning: " fmt, ##__VA_ARGS__);                                             \
+      count++;                                                                                     \
+    }                                                                                              \
+  } while (false)
+#endif
+
 #ifdef NDEBUG
 #define ifdebug if (false)
 #else
