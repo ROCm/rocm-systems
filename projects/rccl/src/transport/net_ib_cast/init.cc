4efb0446ea57a925d2ac1f0f07675224e9d66e8b
Wenkai Du <wenkai.du@amd.com>
2026-06-11 10:33:45 -0500

    Remove defunct comment line

--- projects/rccl/src/transport/net_ib_cast/init.cc
+++ projects/rccl/src/transport/net_ib_cast/init.cc
@@ -517,7 +517,6 @@ ncclResult_t IbCastInitDevices(ncclDebugLogger_t logFunction, ncclProfilerCallba
       }
       // for AINIC IbUseInline is enabled by default always
       IbCastUseInline = true;
-      // for AINIC GDR flush is disabled by default
 
       INFO(NCCL_INIT|NCCL_NET, "NET/IB : AINIC RoCEv2 optimizations enabled: CTS Inline Data: %s; CTS Offload: %s; "
            "IB Use Inline: enabled; GDR Flush: disabled", IbCastUseInline ? "Enabled": "Disabled",
