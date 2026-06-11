d7f29379cff97315c76c0b27791fe7d9a57beac1
John Tyler <jetyler@ualberta.ca>
2026-06-11 10:21:49 -0600

    hipfile/configuration: use fprintf to print to stderr

--- projects/hipfile/src/amd_detail/configuration.cpp
+++ projects/hipfile/src/amd_detail/configuration.cpp
@@ -34,7 +34,7 @@ Configuration::fallback() const noexcept
         bool force_compat = Environment::force_compat_mode().value_or(false);
         bool allow_compat = Environment::allow_compat_mode().value_or(true);
         if (force_compat && !allow_compat) {
-            printf("hipFile: HIPFILE_FORCE_COMPAT_MODE=1 and HIPFILE_ALLOW_COMPAT_MODE=0 "
+            fprintf(stderr, "hipFile: HIPFILE_FORCE_COMPAT_MODE=1 and HIPFILE_ALLOW_COMPAT_MODE=0 "
                    "would disable all I/O backends; enabling the fallback path to avoid "
                    "failing all I/O.\n");
             return true;
