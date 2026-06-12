d387f96b0e4a5f70aeb5578d2ee79e6ae01e916e
Aurelien Bouteiller <abouteil@amd.com>
2026-06-12 09:08:30 -0400

    DEBUG: Remove bnxt-qp-init info that is mostly redundant with dump_qp

--- projects/rocshmem/src/gda/bnxt/backend_gda_bnxt.cpp
+++ projects/rocshmem/src/gda/bnxt/backend_gda_bnxt.cpp
@@ -79,16 +79,8 @@ void GDABackend::bnxt_initialize_gpu_qp(QueuePair* gpu_qp, int conn_num) {
   gpu_qp->bnxt_sq.mtu         = ibv_mtu_to_int(nic.portinfo.active_mtu);
 
   /* Export DB */
-  {
-    auto *dbattr = bnxt_qps[conn_num].db_region_attr;
-    LOG_INFO("gda_qp dbr[%d]: handle=%u dpi=%u umdbr=0x%llx dbr_host=%p",
-             conn_num, dbattr->handle, dbattr->dpi,
-             (unsigned long long)dbattr->umdbr, (void*)dbattr->dbr);
-  }
   CHECK_HIP(hipHostRegister(bnxt_qps[conn_num].db_region_attr->dbr, getpagesize(), hipHostRegisterDefault));
   CHECK_HIP(hipHostGetDevicePointer((void**) &gpu_qp->bnxt_dbr, bnxt_qps[conn_num].db_region_attr->dbr, 0));
-  LOG_INFO("gda_qp dbr[%d]: host=%p -> device=%p",
-           conn_num, (void*)bnxt_qps[conn_num].db_region_attr->dbr, (void*)gpu_qp->bnxt_dbr);
 
   /* Export Memory Keys */
   gpu_qp->lkey = nic.heap_mr->lkey;
@@ -101,16 +93,6 @@ void GDABackend::bnxt_initialize_gpu_qp(QueuePair* gpu_qp, int conn_num) {
   gpu_qp->base_heap = (uintptr_t) heap.get_local_heap_base();
   gpu_qp->base_heap_size = heap.get_size();
 
-  LOG_INFO("gda_qp init[%d]: qp_num=%u sq.buf=%p sq.depth=%u sq.id=%u "
-           "cq.buf=%p cq.depth=%u cq.id=%u dbr=%p (host=%p) "
-           "msntbl=%p msn_tbl_sz=%u psn_sz_log2=%u mtu=%lu inline=%u",
-           conn_num, gpu_qp->qp_num,
-           gpu_qp->bnxt_sq.buf, gpu_qp->bnxt_sq.depth, gpu_qp->bnxt_sq.id,
-           gpu_qp->bnxt_cq.buf, gpu_qp->bnxt_cq.depth, gpu_qp->bnxt_cq.id,
-           (void*)gpu_qp->bnxt_dbr, (void*)bnxt_qps[conn_num].db_region_attr->dbr,
-           gpu_qp->bnxt_sq.msntbl, gpu_qp->bnxt_sq.msn_tbl_sz,
-           gpu_qp->bnxt_sq.psn_sz_log2, (unsigned long)gpu_qp->bnxt_sq.mtu,
-           gpu_qp->inline_threshold);
 }
 
 void GDABackend::bnxt_create_cqs(int cqe) {
