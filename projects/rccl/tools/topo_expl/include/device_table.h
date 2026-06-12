2f0e0274b15a60054fc9f3a8fe9844523da7dbf8
isaki001 <ioannissakiotis@gmail.com>
2026-06-12 21:56:52 +0000

    add unroll 8,16,32 to topo_expl

--- projects/rccl/tools/topo_expl/include/device_table.h
+++ projects/rccl/tools/topo_expl/include/device_table.h
@@ -14,5 +14,8 @@ __forceinline__ __device__ void NCCL_CALL_FUNCTIONS(unsigned short funcIndex) no
 __forceinline__ __device__ void NCCL_CALL_FUNCTIONS_1(unsigned short funcIndex) noexcept { }
 __forceinline__ __device__ void NCCL_CALL_FUNCTIONS_2(unsigned short funcIndex) noexcept { }
 __forceinline__ __device__ void NCCL_CALL_FUNCTIONS_4(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_8(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_16(unsigned short funcIndex) noexcept { }
+__forceinline__ __device__ void NCCL_CALL_FUNCTIONS_32(unsigned short funcIndex) noexcept { }
 
 #endif
