4ffee76bf7bd7c8ea6450eba063f7a0f7a91bcd5
nileshnegi <Nilesh.Negi@amd.com>
2026-06-14 01:27:08 -0500

    [RCCL-Tests] Add gfx1250 to hip_fp8 path
    Add gfx1250 to the hip_fp8 architecture allowlist (same as RCCL)

--- projects/rccl-tests/src/rccl_float8.h
+++ projects/rccl-tests/src/rccl_float8.h
@@ -40,14 +40,17 @@ typedef struct
 } rccl_bfloat8;
 
 // __cplusplus < 201103L || (!defined(__HIP_PLATFORM_AMD__) && !defined(__HIPCC__))
-#elif HIP_VERSION >= 60300000
+
+#elif HIP_VERSION >= 60300000 && \
+      (!__HIP_DEVICE_COMPILE__ || \
+       defined(__gfx942__)  || defined(__gfx950__)  || \
+       defined(__gfx1100__) || defined(__gfx1101__) || \
+       defined(__gfx1200__) || defined(__gfx1201__) || \
+       defined(__gfx1250__))
 
 #include <hip/hip_fp8.h>
 
-#if   __HIP_DEVICE_COMPILE__ && (defined(__gfx950__) || defined(__gfx1200__) || defined(__gfx1201__) ||  (defined(__gfx1100__) || defined(__gfx1101__)))//HIP_FP8_TYPE_OCP is enabled.
-typedef __hip_fp8_e4m3 rccl_float8;
-typedef __hip_fp8_e5m2 rccl_bfloat8;
-#elif __HIP_DEVICE_COMPILE__ && (defined(__gfx942__))
+#if __HIP_DEVICE_COMPILE__ && (defined(__gfx942__))
 typedef __hip_fp8_e4m3_fnuz rccl_float8;
 typedef __hip_fp8_e5m2_fnuz rccl_bfloat8;
 #else
@@ -90,6 +93,7 @@ inline std::ostream& operator<<(std::ostream& os, const __hip_fp8_e5m2_fnuz& bf8
 #endif
 
 extern bool rccl_float8_useFnuz;
+
 // For older versions of ROCm that do not include hip_fp8.h,
 // we provide a local version of the header file as a fallback.
 #else
