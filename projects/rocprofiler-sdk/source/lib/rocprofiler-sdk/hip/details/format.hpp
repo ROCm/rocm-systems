02e7ec6499aa830d150d85ff347bc62f95f56923
Minseob Shin <minseob.shin11@gmail.com>
2026-06-15 15:41:14 +0000

    add hipMemHandleTypeFabric in HIP formatter

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/hip/details/format.hpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/hip/details/format.hpp
@@ -308,6 +308,9 @@ struct formatter<hipMemAllocationHandleType> : rocprofiler::hip::details::base_f
             ROCP_SDK_HIP_FORMAT_CASE_STMT(hipMemHandleType, PosixFileDescriptor);
             ROCP_SDK_HIP_FORMAT_CASE_STMT(hipMemHandleType, Win32);
             ROCP_SDK_HIP_FORMAT_CASE_STMT(hipMemHandleType, Win32Kmt);
+#if HIP_VERSION >= 71400000
+            ROCP_SDK_HIP_FORMAT_CASE_STMT(hipMemHandleType, Fabric);
+#endif
             ROCP_SDK_HIP_FORMAT_DFLT_CASE(hipMemHandleType);
         }
         return fmt::format_to(ctx.out(), "Unknown");
