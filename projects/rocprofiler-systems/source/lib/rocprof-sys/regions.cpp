3a992d1106bea8706f4a773858da132c47bcec78
Kian Cossettini <Kian.Cossettini@amd.com>
2026-06-24 09:36:41 -0400

    Use composition and use policy-templated design

--- projects/rocprofiler-systems/source/lib/rocprof-sys/regions.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/regions.cpp
@@ -108,7 +108,7 @@ rocprofsys_pop_trace_hidden(const char* name)
 extern "C" void
 rocprofsys_flush_pending_region_cache_hidden()
 {
-    rocprofsys::component::category_region_base::flush_pending_cached_entries();
+    rocprofsys::utility::category_region<>::instance().flush_pending_cached_entries();
 }
 
 //======================================================================================//
