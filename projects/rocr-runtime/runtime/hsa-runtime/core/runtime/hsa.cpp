02f12e8deeda6ccf19918e668af2f9307b78e4e2
Bincy Mani <bincy.mani@amd.com>
2026-06-25 11:06:50 -0400

    fix(rocr): Update hsa_sgnal_silent_store_relaxed implementation
      ## Motivation
        hsa_signal_silent_store_relaxed says that it should not wake waiting
        agents.Atomically set the value of a signal without necessarily
        notifying the the agents waiting on it.
    
      ##Technical Details
        Updated the implementation of (hsa_signal_silent_store_relaxed ->
        hsa_signal_store_relaxed so it doesnt reroute to hsa_signal_store_relaxed
        which calls SetEvent()
    
        Updated a test case so that we can measure the difference between
        the context switches of threads and efficiency between
        hsa_signal_silent_store_screlease and Update hsa_signal_store_screlease.
    
       ##JIRA ID
         ROCM-21690

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/hsa.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/hsa.cpp
@@ -1224,6 +1224,24 @@ hsa_signal_value_t hsa_signal_wait_relaxed(hsa_signal_t hsa_signal,
   CATCHRET(hsa_signal_value_t);
 }
 
+
+void hsa_signal_silent_store_relaxed(hsa_signal_t hsa_signal, hsa_signal_value_t value) {
+  TRY;
+  core::Signal* signal = core::Signal::Convert(hsa_signal);
+  assert(IsValid(signal));
+  signal->SilentStoreRelaxed(value);
+  CATCHRET(void);
+}
+
+void hsa_signal_silent_store_screlease(hsa_signal_t hsa_signal, hsa_signal_value_t value) {
+  TRY;
+  core::Signal* signal = core::Signal::Convert(hsa_signal);
+  assert(IsValid(signal));
+  signal->SilentStoreRelease(value);
+  CATCHRET(void);
+}
+
+
 hsa_signal_value_t hsa_signal_wait_scacquire(hsa_signal_t hsa_signal,
                                              hsa_signal_condition_t condition,
                                              hsa_signal_value_t compare_value,
