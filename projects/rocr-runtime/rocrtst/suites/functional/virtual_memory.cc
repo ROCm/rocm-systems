c316c92c1fcd50e20f5b4963825f24a585a8a746
Prerona Ghosh <Prerona.Ghosh@amd.com>
2026-06-30 11:49:34 -0400

    Update VirtMemoryTestBasic::TestVirtAddressAlias test

--- projects/rocr-runtime/rocrtst/suites/functional/virtual_memory.cc
+++ projects/rocr-runtime/rocrtst/suites/functional/virtual_memory.cc
@@ -1736,6 +1736,19 @@ void VirtMemoryTestBasic::TestVirtAddressAlias(void) {
   std::vector<hsa_agent_t> gpus;
   ASSERT_SUCCESS(hsa_iterate_agents(rocrtst::IterateGPUAgents, &gpus));
 
+  // Run on CPU pools - CPU-only alias verification via direct reads/writes
+  std::vector<std::shared_ptr<rocrtst::agent_pools_t>> agent_pools;
+  ASSERT_SUCCESS(rocrtst::GetAgentPools(&agent_pools));
+  for (auto a : agent_pools) {
+    for (auto p : a->pools) {
+      TestVirtAddressAlias(a->agent, p);
+    }
+  }
+
+  if (verbosity() > 0) {
+    std::cout << "    Host Memory VA alias test PASSED" << std::endl;
+  }
+
   // Run on GPU pools
   for (unsigned int i = 0; i < gpus.size(); ++i) {
     hsa_amd_memory_pool_t gpu_pool;
@@ -1749,16 +1762,8 @@ void VirtMemoryTestBasic::TestVirtAddressAlias(void) {
     TestVirtAddressAlias(cpus[0], gpus[i], gpu_pool);
   }
 
-  // Run on CPU pools - CPU-only alias verification via direct reads/writes
-  std::vector<std::shared_ptr<rocrtst::agent_pools_t>> agent_pools;
-  ASSERT_SUCCESS(rocrtst::GetAgentPools(&agent_pools));
-  for (auto a : agent_pools) {
-    for (auto p : a->pools) {
-      TestVirtAddressAlias(a->agent, p);
-    }
-  }
-
   if (verbosity() > 0) {
+    std::cout << "    Device memory VA alias test PASSED" << std::endl;
     std::cout << "    Subtest finished" << std::endl;
     std::cout << kSubTestSeparator << std::endl;
   }
