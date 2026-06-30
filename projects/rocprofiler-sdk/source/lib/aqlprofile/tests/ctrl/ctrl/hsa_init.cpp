0a27986c9cb2f8a30ff7af67d0d0fea00c16b5ba
Mythreya, Kuricheti <mythreya.kuricheti@amd.com>
2026-06-30 22:07:46 +0000

    format code

--- projects/rocprofiler-sdk/source/lib/aqlprofile/tests/ctrl/ctrl/hsa_init.cpp
+++ projects/rocprofiler-sdk/source/lib/aqlprofile/tests/ctrl/ctrl/hsa_init.cpp
@@ -20,7 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 
-
 #include "ctrl/hsa_init.h"
 
 #include "hsa/hsa.h"
@@ -29,64 +28,68 @@
 
 #include "lib/aqlprofile/util/hsa_rsrc_factory.h"
 
-namespace ctrl_test {
+namespace ctrl_test
+{
+void
+InitHsaTables()
+{
+    static CoreApiTable core{};
+    static AmdExtTable  amd_ext{};
 
-void InitHsaTables() {
-  static CoreApiTable core{};
-  static AmdExtTable amd_ext{};
+    core.hsa_init_fn                                = ::hsa_init;
+    core.hsa_shut_down_fn                           = ::hsa_shut_down;
+    core.hsa_status_string_fn                       = ::hsa_status_string;
+    core.hsa_system_get_info_fn                     = ::hsa_system_get_info;
+    core.hsa_system_get_major_extension_table_fn    = ::hsa_system_get_major_extension_table;
+    core.hsa_iterate_agents_fn                      = ::hsa_iterate_agents;
+    core.hsa_agent_get_info_fn                      = ::hsa_agent_get_info;
+    core.hsa_queue_create_fn                        = ::hsa_queue_create;
+    core.hsa_queue_destroy_fn                       = ::hsa_queue_destroy;
+    core.hsa_queue_load_read_index_relaxed_fn       = ::hsa_queue_load_read_index_relaxed;
+    core.hsa_queue_load_write_index_relaxed_fn      = ::hsa_queue_load_write_index_relaxed;
+    core.hsa_queue_store_write_index_relaxed_fn     = ::hsa_queue_store_write_index_relaxed;
+    core.hsa_signal_create_fn                       = ::hsa_signal_create;
+    core.hsa_signal_destroy_fn                      = ::hsa_signal_destroy;
+    core.hsa_signal_store_relaxed_fn                = ::hsa_signal_store_relaxed;
+    core.hsa_signal_wait_scacquire_fn               = ::hsa_signal_wait_scacquire;
+    core.hsa_memory_free_fn                         = ::hsa_memory_free;
+    core.hsa_code_object_reader_create_from_file_fn = ::hsa_code_object_reader_create_from_file;
+    core.hsa_code_object_reader_destroy_fn          = ::hsa_code_object_reader_destroy;
+    core.hsa_executable_create_alt_fn               = ::hsa_executable_create_alt;
+    core.hsa_executable_load_agent_code_object_fn   = ::hsa_executable_load_agent_code_object;
+    core.hsa_executable_freeze_fn                   = ::hsa_executable_freeze;
+    core.hsa_executable_get_symbol_fn               = ::hsa_executable_get_symbol;
 
-  core.hsa_init_fn = ::hsa_init;
-  core.hsa_shut_down_fn = ::hsa_shut_down;
-  core.hsa_status_string_fn = ::hsa_status_string;
-  core.hsa_system_get_info_fn = ::hsa_system_get_info;
-  core.hsa_system_get_major_extension_table_fn = ::hsa_system_get_major_extension_table;
-  core.hsa_iterate_agents_fn = ::hsa_iterate_agents;
-  core.hsa_agent_get_info_fn = ::hsa_agent_get_info;
-  core.hsa_queue_create_fn = ::hsa_queue_create;
-  core.hsa_queue_destroy_fn = ::hsa_queue_destroy;
-  core.hsa_queue_load_read_index_relaxed_fn = ::hsa_queue_load_read_index_relaxed;
-  core.hsa_queue_load_write_index_relaxed_fn = ::hsa_queue_load_write_index_relaxed;
-  core.hsa_queue_store_write_index_relaxed_fn = ::hsa_queue_store_write_index_relaxed;
-  core.hsa_signal_create_fn = ::hsa_signal_create;
-  core.hsa_signal_destroy_fn = ::hsa_signal_destroy;
-  core.hsa_signal_store_relaxed_fn = ::hsa_signal_store_relaxed;
-  core.hsa_signal_wait_scacquire_fn = ::hsa_signal_wait_scacquire;
-  core.hsa_memory_free_fn = ::hsa_memory_free;
-  core.hsa_code_object_reader_create_from_file_fn = ::hsa_code_object_reader_create_from_file;
-  core.hsa_code_object_reader_destroy_fn = ::hsa_code_object_reader_destroy;
-  core.hsa_executable_create_alt_fn = ::hsa_executable_create_alt;
-  core.hsa_executable_load_agent_code_object_fn = ::hsa_executable_load_agent_code_object;
-  core.hsa_executable_freeze_fn = ::hsa_executable_freeze;
-  core.hsa_executable_get_symbol_fn = ::hsa_executable_get_symbol;
+    amd_ext.hsa_amd_memory_pool_get_info_fn       = ::hsa_amd_memory_pool_get_info;
+    amd_ext.hsa_amd_agent_iterate_memory_pools_fn = ::hsa_amd_agent_iterate_memory_pools;
+    amd_ext.hsa_amd_memory_pool_allocate_fn       = ::hsa_amd_memory_pool_allocate;
+    amd_ext.hsa_amd_agents_allow_access_fn        = ::hsa_amd_agents_allow_access;
+    amd_ext.hsa_amd_memory_async_copy_fn          = ::hsa_amd_memory_async_copy;
 
-  amd_ext.hsa_amd_memory_pool_get_info_fn = ::hsa_amd_memory_pool_get_info;
-  amd_ext.hsa_amd_agent_iterate_memory_pools_fn = ::hsa_amd_agent_iterate_memory_pools;
-  amd_ext.hsa_amd_memory_pool_allocate_fn = ::hsa_amd_memory_pool_allocate;
-  amd_ext.hsa_amd_agents_allow_access_fn = ::hsa_amd_agents_allow_access;
-  amd_ext.hsa_amd_memory_async_copy_fn = ::hsa_amd_memory_async_copy;
-
-  HsaApiTable table{};
-  table.core_ = &core;
-  table.amd_ext_ = &amd_ext;
-  rocprofiler::aqlprofile::hsa_rsrc_factory_init(&table);
+    HsaApiTable table{};
+    table.core_    = &core;
+    table.amd_ext_ = &amd_ext;
+    rocprofiler::aqlprofile::hsa_rsrc_factory_init(&table);
 }
 
-const hsa_ven_amd_aqlprofile_pfn_t* InTreeAqlProfileApi() {
-  // Point each entry at the in-tree (statically linked) implementation from
-  // rocprofiler-sdk-aqlprofile. Referencing these symbols also forces the
-  // linker to pull the in-tree implementation into the test binary.
-  static hsa_ven_amd_aqlprofile_pfn_t api = {};
-  api.hsa_ven_amd_aqlprofile_error_string = ::hsa_ven_amd_aqlprofile_error_string;
-  api.hsa_ven_amd_aqlprofile_validate_event = ::hsa_ven_amd_aqlprofile_validate_event;
-  api.hsa_ven_amd_aqlprofile_start = ::hsa_ven_amd_aqlprofile_start;
-  api.hsa_ven_amd_aqlprofile_stop = ::hsa_ven_amd_aqlprofile_stop;
+const hsa_ven_amd_aqlprofile_pfn_t*
+InTreeAqlProfileApi()
+{
+    // Point each entry at the in-tree (statically linked) implementation from
+    // rocprofiler-sdk-aqlprofile. Referencing these symbols also forces the
+    // linker to pull the in-tree implementation into the test binary.
+    static hsa_ven_amd_aqlprofile_pfn_t api   = {};
+    api.hsa_ven_amd_aqlprofile_error_string   = ::hsa_ven_amd_aqlprofile_error_string;
+    api.hsa_ven_amd_aqlprofile_validate_event = ::hsa_ven_amd_aqlprofile_validate_event;
+    api.hsa_ven_amd_aqlprofile_start          = ::hsa_ven_amd_aqlprofile_start;
+    api.hsa_ven_amd_aqlprofile_stop           = ::hsa_ven_amd_aqlprofile_stop;
 #ifdef AQLPROF_NEW_API
-  api.hsa_ven_amd_aqlprofile_read = ::hsa_ven_amd_aqlprofile_read;
+    api.hsa_ven_amd_aqlprofile_read = ::hsa_ven_amd_aqlprofile_read;
 #endif
-  api.hsa_ven_amd_aqlprofile_legacy_get_pm4 = ::hsa_ven_amd_aqlprofile_legacy_get_pm4;
-  api.hsa_ven_amd_aqlprofile_get_info = ::hsa_ven_amd_aqlprofile_get_info;
-  api.hsa_ven_amd_aqlprofile_iterate_data = ::hsa_ven_amd_aqlprofile_iterate_data;
-  return &api;
+    api.hsa_ven_amd_aqlprofile_legacy_get_pm4 = ::hsa_ven_amd_aqlprofile_legacy_get_pm4;
+    api.hsa_ven_amd_aqlprofile_get_info       = ::hsa_ven_amd_aqlprofile_get_info;
+    api.hsa_ven_amd_aqlprofile_iterate_data   = ::hsa_ven_amd_aqlprofile_iterate_data;
+    return &api;
 }
 
 }  // namespace ctrl_test
