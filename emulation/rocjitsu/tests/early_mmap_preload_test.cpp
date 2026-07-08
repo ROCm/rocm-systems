2469edf4227072e329f907b20ba461d64771ba9f
James Newling <james.newling@gmail.com>
2026-07-08 09:47:05 -0700

    update

--- emulation/rocjitsu/tests/early_mmap_preload_test.cpp
+++ emulation/rocjitsu/tests/early_mmap_preload_test.cpp
@@ -4,16 +4,36 @@
 /// @file early_mmap_preload_test.cpp
 /// @brief Test helper whose constructor calls mmap before rocjitsu initializes.
 
+#include <cerrno>
+#include <cstdlib>
 #include <sys/mman.h>
 #include <unistd.h>
 
+namespace {
+
+template <size_t N> [[noreturn]] void fail(const char (&message)[N]) {
+  static_cast<void>(write(STDERR_FILENO, message, N - 1));
+  _exit(EXIT_FAILURE);
+}
+
+} // namespace
+
 __attribute__((constructor)) static void early_mmap_constructor() {
+  errno = 0;
+  if (mmap(nullptr, 0, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0) != MAP_FAILED ||
+      errno != EINVAL)
+    fail("early mmap preload: zero-length mmap did not fail with EINVAL\n");
+
   void *ptr = mmap(nullptr, 4096, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
   if (ptr == MAP_FAILED)
-    _exit(101);
+    fail("early mmap preload: mmap failed\n");
 
   static_cast<char *>(ptr)[0] = 7;
 
+  errno = 0;
+  if (munmap(ptr, 0) == 0 || errno != EINVAL)
+    fail("early mmap preload: zero-length munmap did not fail with EINVAL\n");
+
   if (munmap(ptr, 4096) != 0)
-    _exit(102);
+    fail("early mmap preload: munmap failed\n");
 }
