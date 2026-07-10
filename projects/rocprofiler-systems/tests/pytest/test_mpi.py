3aaf30bb21af2757119b4fbca9f3dd288ed40c47
Kian Cossettini <Kian.Cossettini@amd.com>
2026-07-10 13:21:20 -0400

    Use path name when attempting to read Symtab from Object

--- projects/rocprofiler-systems/tests/pytest/test_mpi.py
+++ projects/rocprofiler-systems/tests/pytest/test_mpi.py
@@ -89,7 +89,8 @@ class TestMPI(RocprofsysTest):
         ]
         BINARY_REWRITE_PASS_REGEX = [r"perfetto-trace-0\.proto", r"wall_clock-0\.txt"]
         BINARY_REWRITE_FAIL_REGEX = [
-            r"Outputting.*(perfetto-trace|trip_count|sampling_percent|sampling_cpu_clock|sampling_wall_clock|wall_clock)-[0-9][0-9]+.(json|txt|proto)"
+            r"Outputting.*(perfetto-trace|trip_count|sampling_percent|sampling_cpu_clock|sampling_wall_clock|wall_clock)-[0-9][0-9]+.(json|txt|proto)",
+            "Failed to open Symtab for",
         ]
         ENV = {"ROCPROFSYS_VERBOSE": "1"}
 
