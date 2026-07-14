c7309cae7aaff128b52e24c805ac552865631efd
kesag <keshav.aggarwal@amd.com>
2026-07-14 20:07:46 +0000

    updated functional test to use

--- projects/rocshmem/scripts/functional_tests/driver.sh
+++ projects/rocshmem/scripts/functional_tests/driver.sh
@@ -760,7 +760,7 @@ TestColl() {
     ExecTest  "broadcast_wave"   2       1            $WAVE_SIZE        32768
     ExecTest  "alltoall_wave"    2       1            $WAVE_SIZE        512
     ExecTest  "fcollect_wave"    2       1            $WAVE_SIZE        32768
-    ExecTest  "reduce_wave"      2       1            64        32768
+    ExecTest  "reduce_wave"      2       1            $WAVE_SIZE        32768
   else echo "Skip:   *_wave (AIROCSHMEM-409: wave tests not supported on RO)"; fi
 }
 
