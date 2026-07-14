92d0a2bcb20d0a995dd445ee378ed3e2295dae31
Rahul Vaidya <ravaidya@amd.com>
2026-07-13 19:11:02 -0700

    Increment opCount for bcast path and fix dead code in ncclMaxDevWorkBatchBytes
    Signed-off-by: Rahul Vaidya <ravaidya@amd.com>

--- projects/rccl/src/enqueue.cc
+++ projects/rccl/src/enqueue.cc
@@ -3355,6 +3355,7 @@ static ncclResult_t collTaskAppend(struct ncclComm* comm, struct ncclInfo* info,
     // enqueue to peer's bcast queue instead of collSorter
     ncclIntruQueueEnqueue(&planner->peers[info->root].bcastQueue, t);
     planner->nTasksBcast += 1;
+    comm->opCount++;
   }
   else {
   struct ncclTaskColl* t = ncclMemoryPoolAlloc<struct ncclTaskColl>(&comm->memPool_ncclTaskColl, &comm->memPermanent);
