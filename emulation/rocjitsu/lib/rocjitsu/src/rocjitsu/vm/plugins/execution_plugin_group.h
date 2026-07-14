2f2ea59b1f8159615487b66882909da8c7856007
Erick Ochoa <erick.ochoalopez@amd.com>
2026-07-14 17:11:39 -0400

    Revert "Preload ASan first, empty_groups survives process teardown"
    This reverts commit 117d474c6649a942d722fc87d507f2dec2ab3c60.

--- emulation/rocjitsu/lib/rocjitsu/src/rocjitsu/vm/plugins/execution_plugin_group.h
+++ emulation/rocjitsu/lib/rocjitsu/src/rocjitsu/vm/plugins/execution_plugin_group.h
@@ -143,9 +143,8 @@ public:
   }
 
   static std::shared_ptr<ExecutionPluginGroup> empty_group() {
-    static const auto *instance =
-        new std::shared_ptr<ExecutionPluginGroup>(std::make_shared<ExecutionPluginGroup>());
-    return *instance;
+    static auto instance = std::make_shared<ExecutionPluginGroup>();
+    return instance;
   }
 
 protected:
