53fcc6b3f5c5447df20d41894dc6733e8d1cae30
Mythreya, Kuricheti <mythreya.kuricheti@amd.com>
2026-07-15 15:54:50 +0000

    initial callback api

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/callback_tracing.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/callback_tracing.cpp
@@ -32,6 +32,7 @@
 #include "lib/rocprofiler-sdk/hsa/memory_allocation.hpp"
 #include "lib/rocprofiler-sdk/hsa/scratch_memory.hpp"
 #include "lib/rocprofiler-sdk/kernel_dispatch/kernel_dispatch.hpp"
+#include "lib/rocprofiler-sdk/kernel_replay/kernel_replay.hpp"
 #include "lib/rocprofiler-sdk/marker/marker.hpp"
 #include "lib/rocprofiler-sdk/ompt/ompt.hpp"
 #include "lib/rocprofiler-sdk/rccl/rccl.hpp"
@@ -104,6 +105,7 @@ ROCPROFILER_CALLBACK_TRACING_KIND_STRING(MARKER_CORE_RANGE_API)
 ROCPROFILER_CALLBACK_TRACING_KIND_STRING(HIP_GRAPH)
 ROCPROFILER_CALLBACK_TRACING_KIND_STRING(ROCSHMEM_API)
 ROCPROFILER_CALLBACK_TRACING_KIND_STRING(HIPFILE_API)
+ROCPROFILER_CALLBACK_TRACING_KIND_STRING(KERNEL_REPLAY)
 
 template <size_t Idx, size_t... Tail>
 std::pair<const char*, size_t>
@@ -265,6 +267,11 @@ rocprofiler_query_callback_tracing_kind_operation_name(rocprofiler_callback_trac
         case ROCPROFILER_CALLBACK_TRACING_KERNEL_DISPATCH:
         {
             val = rocprofiler::kernel_dispatch::name_by_id(operation);
+            break;
+        }
+        case ROCPROFILER_CALLBACK_TRACING_KERNEL_REPLAY:
+        {
+            val = rocprofiler::kernel_replay::name_by_id(operation);
             break;
         }
         case ROCPROFILER_CALLBACK_TRACING_MEMORY_COPY:
@@ -431,6 +438,11 @@ rocprofiler_iterate_callback_tracing_kind_operations(
         case ROCPROFILER_CALLBACK_TRACING_KERNEL_DISPATCH:
         {
             ops = rocprofiler::kernel_dispatch::get_ids();
+            break;
+        }
+        case ROCPROFILER_CALLBACK_TRACING_KERNEL_REPLAY:
+        {
+            ops = rocprofiler::kernel_replay::get_ids();
             break;
         }
         case ROCPROFILER_CALLBACK_TRACING_MEMORY_COPY:
@@ -659,6 +671,7 @@ rocprofiler_iterate_callback_tracing_kind_operation_args(
         case ROCPROFILER_CALLBACK_TRACING_SCRATCH_MEMORY:
         case ROCPROFILER_CALLBACK_TRACING_CODE_OBJECT:
         case ROCPROFILER_CALLBACK_TRACING_KERNEL_DISPATCH:
+        case ROCPROFILER_CALLBACK_TRACING_KERNEL_REPLAY:
         case ROCPROFILER_CALLBACK_TRACING_MEMORY_COPY:
         case ROCPROFILER_CALLBACK_TRACING_MEMORY_ALLOCATION:
         case ROCPROFILER_CALLBACK_TRACING_RCCL_API:
