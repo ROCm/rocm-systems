d16d27213c79b589343926175d7dde650b0713b5
Aryan Salmanpour <aryan.salmanpour@amd.com>
2026-07-15 15:37:44 -0500

    Address Copilot review

--- projects/rocdecode/utils/rocvideodecode/roc_video_dec.cpp
+++ projects/rocdecode/utils/rocvideodecode/roc_video_dec.cpp
@@ -274,7 +274,7 @@ int RocVideoDecoder::HandleVideoSequence(RocdecVideoFormat *p_video_format) {
     decode_caps.bit_depth_minus_8 = p_video_format->bit_depth_luma_minus8;
     decode_caps.device_id = device_id_;
 
-    rocDecGetDecoderCaps(&decode_caps);
+    ROCDEC_API_CALL(rocDecGetDecoderCaps(&decode_caps));
     if(!decode_caps.is_supported) {
         ROCDEC_THROW("rocDecode:: Codec not supported on this GPU ", ROCDEC_NOT_SUPPORTED);
         return 0;
