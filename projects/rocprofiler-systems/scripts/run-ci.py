a080d99fba640d8e51bd566c86eb6c201ede012a
David Galiffi <David.Galiffi@amd.com>
2026-07-16 00:44:50 -0400

    Revert "fix(workflows): drop unused CTEST_CMD assignment in generate_custom"
    This reverts commit 55d270b1ac700983aeacd4af2119e7677ed381ad.

--- projects/rocprofiler-systems/scripts/run-ci.py
+++ projects/rocprofiler-systems/scripts/run-ci.py
@@ -117,6 +117,7 @@ def generate_custom(args, cmake_args, ctest_args):
     GIT_CMD = which("git", require=True)
     GCOV_CMD = which("gcov", require=False)
     CMAKE_CMD = which("cmake", require=True)
+    CTEST_CMD = which("ctest", require=True)
 
     NAME = re.sub(r"(.*)-([0-9]+)/merge", "PR_\\2_\\1", NAME)
 
