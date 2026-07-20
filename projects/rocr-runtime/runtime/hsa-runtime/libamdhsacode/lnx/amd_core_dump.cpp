ROCm/rocm-systems

commit 99a0be1e168eb463c23e5fc4d9283943a95eb639

rocr/coredump: Extend list of required memory ranges for lightweight cores
On some systems (gfx1201 for example) debugger testsuite fails when
exercising lightweight core dump generation.  Looking at it, is seems
that the core dump is missing key information regarding queues, making
the coredump unusable.

The first issue the debugger fails on is when trying to find the queue's
read/write pointer values (stored in the amd_queue_t).  It seems that on
gfx1201, the amd_queue_t resides in device memory, and because it is not
explicitly listed as "must keep" is absent from the core dump when
enabling lightweight core dumps.  Other architectures seem to hold this
data in system memory, so it is eventually available since is is saved
by the kernel.

In this patch I make sure that any data the debugger will need is kept
when opening the core dump.  When listing the address ranges that we
must include in the core dump, it is safe to include data that might
actually reside in system memory.  The core dumping machinery will list
all the device mappings, and filter out any range not intersecting with
ranges to keep.  If such range are on system memory, they will never
intersect with any device mappings, and this is fine.

Tested on gfx1201 using ROCgdb's gdb.rocm/runtime-core.exp testcase.
This patches fixes existing failures.

projects/rocr-runtime/runtime/hsa-runtime/libamdhsacode/lnx/amd_core_dump.cpp
@@ -307,6 +307,40 @@ static hsa_status_t build_lightweight_coredump_ranges(MemoryRegionFilter& filter
                   reinterpret_cast<uint64_t>(scratch_base) + scratch_size,
                   scratch_size);
     }
+
+    for (core::Queue* q : gpu_agent->GetAqlQueues()) {
+      AMD::AqlQueue* aql_queue = static_cast<AMD::AqlQueue*>(q);
+
+      // We need to capture the queue amd_queue_t for the debugger.
+      debug_print("Added aql_queue_t range: %#p - %#p (size: %zu)\n",
+                  &aql_queue->amd_queue_, &aql_queue->amd_queue_ + 1,
+                  sizeof(aql_queue->amd_queue_));
+      filter.add_range(reinterpret_cast<uint64_t>(&aql_queue->amd_queue_),
+                       sizeof(aql_queue->amd_queue_));
+
+      // Same goes for the ring buffer.
+      debug_print("Added ring buffer range: %#p - %#p (size: %zu)\n",
+                  aql_queue->amd_queue_.hsa_queue.base_address,
+                  static_cast<void*>(aql_queue->amd_queue_.hsa_queue.base_address)
+                    + aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t),
+                  aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t));
+      filter.add_range(reinterpret_cast<uint64_t>(aql_queue->amd_queue_.hsa_queue.base_address),
+                       aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t));
+
+      HsaQueueInfo queue_info;
+      HSAKMT_STATUS status = HSAKMT_CALL(hsaKmtGetQueueInfo(aql_queue->aql_queue_id(), &queue_info));
+      if (status != HSAKMT_STATUS_SUCCESS) {
+        fprintf(stderr, "hsaKmtGetQueueInfo failed during core dump creation\n");
+        return HSA_STATUS_ERROR;
+      }
+
+      debug_print("Added CWSR area range: %#p - %#p (size: %zu)\n",
+                  queue_info.SaveAreaHeader,
+                  static_cast<void*>(queue_info.SaveAreaHeader) + queue_info.SaveAreaSizeInBytes,
+                  queue_info.SaveAreaSizeInBytes);
+      filter.add_range(reinterpret_cast<uint64_t>(queue_info.SaveAreaHeader),
+                       queue_info.SaveAreaSizeInBytes);
+    }
   }
 
   // Add code object allocations from allocation_map_

commit 0aee84c30c029917dbc9aa720a71d8ed2c4bddda

rocr: Fix debug builds with clang
When creating debuild build with clang, we have build failures
introduced by 99a0be1e16 "rocr/coredump: Extend list of required memory
ranges for lightweight cores":

    .../libamdhsacode/lnx/amd_core_dump.cpp:315:46: warning: flag '#' results in undefined
          behavior with 'p' conversion specifier [-Wformat]
      315 |       debug_print("Added aql_queue_t range: %#p - %#p (size: %zu)\n",
          |                                             ~^~
    .../core/util/utils.h:161:21: note: expanded from macro 'debug_print'
      161 |     fprintf(stderr, fmt, ##__VA_ARGS__);                                                           \
          |                     ^~~
    .../libamdhsacode/lnx/amd_core_dump.cpp:315:52: warning: flag '#' results in undefined
          behavior with 'p' conversion specifier [-Wformat]
      315 |       debug_print("Added aql_queue_t range: %#p - %#p (size: %zu)\n",
          |                                                   ~^~
    .../core/util/utils.h:161:21: note: expanded from macro 'debug_print'
      161 |     fprintf(stderr, fmt, ##__VA_ARGS__);                                                           \
          |                     ^~~
    .../libamdhsacode/lnx/amd_core_dump.cpp:325:21: error: arithmetic on a pointer to void
      324 |                   static_cast<void*>(aql_queue->amd_queue_.hsa_queue.base_address)
          |                   ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      325 |                     + aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t),
          |                     ^
    .../core/util/utils.h:161:28: note: expanded from macro 'debug_print'
      161 |     fprintf(stderr, fmt, ##__VA_ARGS__);                                                           \
          |                            ^~~~~~~~~~~
    .../libamdhsacode/lnx/amd_core_dump.cpp:339:65: error: arithmetic on a pointer to void

projects/rocr-runtime/runtime/hsa-runtime/libamdhsacode/lnx/amd_core_dump.cpp
@@ -312,16 +312,17 @@ static hsa_status_t build_lightweight_coredump_ranges(MemoryRegionFilter& filter
       AMD::AqlQueue* aql_queue = static_cast<AMD::AqlQueue*>(q);
 
       // We need to capture the queue amd_queue_t for the debugger.
-      debug_print("Added aql_queue_t range: %#p - %#p (size: %zu)\n",
-                  &aql_queue->amd_queue_, &aql_queue->amd_queue_ + 1,
+      debug_print("Added aql_queue_t range: %#" PRIx64 " - %#" PRIx64 " (size: %zu)\n",
+                  reinterpret_cast<uint64_t>(&aql_queue->amd_queue_),
+                  reinterpret_cast<uint64_t>(&aql_queue->amd_queue_ + 1),
                   sizeof(aql_queue->amd_queue_));
       filter.add_range(reinterpret_cast<uint64_t>(&aql_queue->amd_queue_),
                        sizeof(aql_queue->amd_queue_));
 
       // Same goes for the ring buffer.
-      debug_print("Added ring buffer range: %#p - %#p (size: %zu)\n",
-                  aql_queue->amd_queue_.hsa_queue.base_address,
-                  static_cast<void*>(aql_queue->amd_queue_.hsa_queue.base_address)
+      debug_print("Added ring buffer range: %#" PRIx64 " - %#" PRIx64 " (size: %zu)\n",
+                  reinterpret_cast<uint64_t>(aql_queue->amd_queue_.hsa_queue.base_address),
+                  reinterpret_cast<uint64_t>(aql_queue->amd_queue_.hsa_queue.base_address)
                     + aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t),
                   aql_queue->amd_queue_.hsa_queue.size * sizeof(hsa_kernel_dispatch_packet_t));
       filter.add_range(reinterpret_cast<uint64_t>(aql_queue->amd_queue_.hsa_queue.base_address),
@@ -334,9 +335,9 @@ static hsa_status_t build_lightweight_coredump_ranges(MemoryRegionFilter& filter
         return HSA_STATUS_ERROR;
       }
 
-      debug_print("Added CWSR area range: %#p - %#p (size: %zu)\n",
-                  queue_info.SaveAreaHeader,
-                  static_cast<void*>(queue_info.SaveAreaHeader) + queue_info.SaveAreaSizeInBytes,
+      debug_print("Added CWSR area range: %#" PRIx64 " - %#" PRIx64 " (size: %zu)\n",
+                  reinterpret_cast<uint64_t>(queue_info.SaveAreaHeader),
+                  reinterpret_cast<uint64_t>(queue_info.SaveAreaHeader) + queue_info.SaveAreaSizeInBytes,
                   queue_info.SaveAreaSizeInBytes);
       filter.add_range(reinterpret_cast<uint64_t>(queue_info.SaveAreaHeader),
                        queue_info.SaveAreaSizeInBytes);