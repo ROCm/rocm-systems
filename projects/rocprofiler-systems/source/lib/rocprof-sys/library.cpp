579ccceabc7473affdeac4e2fe390b818765a624
Kian Cossettini <Kian.Cossettini@amd.com>
2026-07-23 08:40:00 -0400

    Gate repeated per-thread finalization messages on more verbose conditions

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
@@ -1118,7 +1118,7 @@ rocprofsys_finalize_hidden(void)
     // if they are still running (e.g. thread-pool still alive), the
     // thread-specific data will be wrong if try to stop them from
     // the main thread.
-    auto _thr_verbose = (config::get_use_causal()) ? 1 : 0;
+    auto _thr_verbose = (config::get_use_causal() || config::get_verbose() > 0) ? 1 : 0;
     if(thread_data<thread_bundle_t>::get())
     {
         for(auto& itr : *thread_data<thread_bundle_t>::get())
@@ -1129,7 +1129,7 @@ rocprofsys_finalize_hidden(void)
                 std::string _msg = itr->as_string();
                 auto        _pos = _msg.find(">>>  ");
                 if(_pos != std::string::npos) _msg = _msg.substr(_pos + 5);
-                if(_thr_verbose >= 0)
+                if(_thr_verbose > 0)
                 {
                     LOG_INFO("{}", _msg);
                 }
