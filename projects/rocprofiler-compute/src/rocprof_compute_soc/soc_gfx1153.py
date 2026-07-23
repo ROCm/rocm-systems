69d75a158367ebdf25de3dcbaaf9273bf4bc651c
xuchen-amd <xuchen@amd.com>
2026-07-22 23:18:58 -0400

    fix(rocprof-compute): gfx1153 SoC inherits OmniSoC_Base directly

--- projects/rocprofiler-compute/src/rocprof_compute_soc/soc_gfx1153.py
+++ projects/rocprofiler-compute/src/rocprof_compute_soc/soc_gfx1153.py
@@ -2,14 +2,47 @@
 # SPDX-License-Identifier:  MIT
 
 import argparse
+from typing import Optional
 
-from rocprof_compute_soc.soc_gfx1152 import gfx1152_soc
+from rocprof_compute_soc.soc_base import OmniSoC_Base
+from utils.logger import demarcate
 from utils.mi_gpu_spec import mi_gpu_specs
 from utils.specs import MachineSpecs
 
 
-class gfx1153_soc(gfx1152_soc):
+class gfx1153_soc(OmniSoC_Base):
     def __init__(self, args: argparse.Namespace, mspec: MachineSpecs) -> None:
         super().__init__(args, mspec)
         self.set_arch("gfx1153")
-        self.set_perfmon_config(mi_gpu_specs.get_perfmon_config("gfx1153"))
+        self.set_compatible_profilers(["rocprofv3", "rocprofiler-sdk"])
+        try:
+            self.set_perfmon_config(mi_gpu_specs.get_perfmon_config("gfx1153"))
+        except KeyError:
+            self.set_perfmon_config({
+                "SQ": 16,
+                "SQC": 8,
+                "TCP": 4,
+                "GL1C": 4,
+                "GL2C": 4,
+                "SPI": 6,
+                "GRBM": 2,
+            })
+
+        self._mspec.l2_banks = 8
+        self._mspec.lds_banks_per_cu = 32
+        self._mspec.pipes_per_gpu = 2
+
+    # -----------------------
+    # Required child methods
+    # -----------------------
+    @demarcate
+    def profiling_setup(self) -> Optional[list[str]]:
+        """Perform any SoC-specific setup prior to profiling."""
+        super().profiling_setup()
+        filter_blocks = self.perfmon_filter()
+        return filter_blocks
+
+    @demarcate
+    def post_profiling(self) -> None:
+        """Perform any SoC-specific post profiling activities."""
+        super().post_profiling()
