6fead2ed335cd2d0031ee395f5a8d9fe294626ea
cadolphe <cadolphe@amd.com>
2026-07-27 11:42:55 -0400

    SWDEV-1 - Remove quick running version of test

--- projects/hip-tests/catch/unit/synchronization/copy_coherency.cc
+++ projects/hip-tests/catch/unit/synchronization/copy_coherency.cc
@@ -293,12 +293,8 @@ void testWrapper(size_t numElements) {
  */
 
 HIP_TEST_CASE(Unit_Copy_Coherency) {
-  if (isQuickLevel()) {
-    testWrapper(g_elementSizes[0]);
-  } else {
-    for (int index = 0; index < sizeof(g_elementSizes) / sizeof(int); index++) {
-      size_t numElements = g_elementSizes[index];
-      testWrapper(numElements);
-    }
+  for (int index = 0; index < sizeof(g_elementSizes) / sizeof(int); index++) {
+    size_t numElements = g_elementSizes[index];
+    testWrapper(numElements);
   }
 }
