59a83e45a113c4a9660c28da87166afbddeb8440
Prerona Ghosh <Prerona.Ghosh@amd.com>
2026-07-27 14:30:31 -0400

    simplify check

--- projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
+++ projects/rocr-runtime/runtime/hsa-runtime/core/runtime/runtime.cpp
@@ -934,15 +934,14 @@ hsa_status_t Runtime::GetSystemInfo(hsa_system_info_t attribute, void* value) {
       break;
     }
     case HSA_AMD_SYSTEM_INFO_HOST_ALLOC_DMA_BUF_SUPPORTED: {
-      // Host memory DMA-BUF allocation via vmem APIs requires:
-      //  - Virtual Memory APIs supported by the driver
-      //  - At least one GPU agent (needed for drm ops)
-      bool supported = false;
-      if (core::Runtime::runtime_singleton_->VirtualMemApiSupported())
-        supported = !core::Runtime::runtime_singleton_->gpu_agents().empty();
-      *((bool*)value) = supported;
-      break;
-    }
+    // Host memory DMA-BUF allocation via vmem APIs requires:
+    //  - Virtual Memory APIs supported by the driver
+    //  - At least one GPU agent (needed for DRM operations)
+    auto* runtime = core::Runtime::runtime_singleton_;
+    *((bool*)value) = runtime->VirtualMemApiSupported() &&
+                      !runtime->gpu_agents().empty();
+    break;
+  }
     default:
       return HSA_STATUS_ERROR_INVALID_ARGUMENT;
   }
