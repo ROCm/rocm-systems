2e382b7129cdcc4dd4cc5691cc305570f79293a6
Galantsev, Dmitrii <dmitrii.galantsev@amd.com>
2026-07-29 12:41:47 -0500

    format
    Signed-off-by: Galantsev, Dmitrii <dmitrii.galantsev@amd.com>

--- projects/amdsmi/tests/amd_smi_test/functional/gpu/clock/clock_limit_read_write_test.cc
+++ projects/amdsmi/tests/amd_smi_test/functional/gpu/clock/clock_limit_read_write_test.cc
@@ -28,8 +28,8 @@
 #include <iostream>
 #include <string>
 
-#include "test_common.h"
 #include "amd_smi/amdsmi.h"
+#include "test_common.h"
 
 TestClockLimitReadWrite::TestClockLimitReadWrite() : TestBase() {
   set_title("AMDSMI Clock Limit Read/Write Test");
