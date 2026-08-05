834b21a93d4f7cc64a114b43ce3a764f07e746b2
ywang103-amd <ywang103@amd.com>
2026-08-05 09:29:13 -0400

    feat(rocprofiler-compute): enable roofline and benchmarking of gfx1153 hardware (#9512)

--- projects/rocprofiler-compute/src/utils/roofline_calc.py
+++ projects/rocprofiler-compute/src/utils/roofline_calc.py
@@ -119,6 +119,15 @@ SUPPORTED_DATATYPES: dict[str, dict[str, OpsSupport]] = {
         "I32": OpsSupport.VALU,
         "I64": OpsSupport.VALU,
     },  # Unsupported: F4, F6, F8
+    "gfx1153": {
+        "FP16": OpsSupport.VALU,
+        "BF16": OpsSupport.VALU,
+        "FP32": OpsSupport.VALU,
+        "FP64": OpsSupport.VALU,
+        "I8": OpsSupport.VALU,
+        "I32": OpsSupport.VALU,
+        "I64": OpsSupport.VALU,
+    },  # Unsupported: F4, F6, F8
     "gfx1250": {
         "FP4": OpsSupport.MATRIX,
         "FP6": OpsSupport.MATRIX,
