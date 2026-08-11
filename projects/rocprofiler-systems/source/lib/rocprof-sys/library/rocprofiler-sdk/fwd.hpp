8a3803088481fa1e6b5ef291ef749c752dfee342
Aleksandar Djordjevic <aleksandar.djordjevic@amd.com>
2026-08-11 14:41:28 +0200

    Use const in ROCPROFILER_CALL macro

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk/fwd.hpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk/fwd.hpp
@@ -272,13 +272,13 @@ as_client_data(void* _ptr)
 #if !defined(ROCPROFILER_CALL)
 #    define ROCPROFILER_CALL(result)                                                     \
         {                                                                                \
-            rocprofiler_status_t ROCPROFSYS_VARIABLE(_rocp_status_, __LINE__) =          \
+            const rocprofiler_status_t ROCPROFSYS_VARIABLE(_rocp_status_, __LINE__) =    \
                 (result);                                                                \
             if(ROCPROFSYS_VARIABLE(_rocp_status_, __LINE__) !=                           \
                ROCPROFILER_STATUS_SUCCESS)                                               \
             {                                                                            \
-                auto        msg        = std::stringstream{};                            \
-                std::string status_msg = rocprofiler_get_status_string(                  \
+                auto              msg        = std::stringstream{};                      \
+                const std::string status_msg = rocprofiler_get_status_string(            \
                     ROCPROFSYS_VARIABLE(_rocp_status_, __LINE__));                       \
                 msg << "[" #result "][" << __FILE__ << ":" << __LINE__ << "] "           \
                     << "rocprofiler-sdk call [" << #result                               \
