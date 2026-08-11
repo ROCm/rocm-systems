0d715aaa624e16d77ca2e0047de6bde711f58776
Marjan Antic <Marjan.Antic@amd.com>
2026-08-11 12:34:04 +0200

    fix(control): Address review findings

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library.cpp
@@ -1061,6 +1061,8 @@ rocprofsys_finalize_hidden(void)
 
     LOG_INFO("Finalizing rocprof-sys...");
 
+    stop_time_windows();
+
     sampling::block_samples();
 
     thread_info::set_stop(comp::wall_clock::record());
@@ -1413,7 +1415,6 @@ rocprofsys_finalize_hidden(void)
         _output_registry.print_summary();
     }
 
-    stop_time_windows();
     categories::shutdown();
 
     _finalization.stop();
