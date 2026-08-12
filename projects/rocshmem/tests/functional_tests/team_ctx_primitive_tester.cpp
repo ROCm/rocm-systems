8a86fba01fe98fec3a6e2117a7ca3ff1d14e89f9
amirsojoodi <amir.sojoodi@amd.com>
2026-08-12 12:22:11 +0000

    Dev: remove clang inline attr from call site

--- projects/rocshmem/tests/functional_tests/team_ctx_primitive_tester.cpp
+++ projects/rocshmem/tests/functional_tests/team_ctx_primitive_tester.cpp
@@ -91,9 +91,9 @@ __global__ void TeamCtxPrimitiveTest(int loop, int skip, long long int *start_ti
     } else if constexpr (Type == TeamCtxGetNBITestType) {
       rocshmem_ctx_getmem_nbi(ctx, dest + offset, source + offset, size, 1);
     } else if constexpr (Type == TeamCtxPutTestType) {
-      [[clang::always_inline]] rocshmem_ctx_putmem(ctx, dest + offset, source + offset, size, 1);
+      rocshmem_ctx_putmem(ctx, dest + offset, source + offset, size, 1);
     } else if constexpr (Type == TeamCtxPutNBITestType) {
-      [[clang::always_inline]] rocshmem_ctx_putmem_nbi(ctx, dest + offset, source + offset, size, 1);
+      rocshmem_ctx_putmem_nbi(ctx, dest + offset, source + offset, size, 1);
     }
   }
 
