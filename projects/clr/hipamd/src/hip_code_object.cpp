5c5b2ced02e538b61619b262af4a24743312bea2
Rakesh Roy <rakesh.roy@amd.com>
2026-08-19 10:08:54 +0530

    feat(hip): Implement API hipModuleEnumerateFunctions

--- projects/clr/hipamd/src/hip_code_object.cpp
+++ projects/clr/hipamd/src/hip_code_object.cpp
@@ -156,6 +156,25 @@ hipError_t DynCO::getFuncCount(unsigned int* count) {
   return hipSuccess;
 }
 
+hipError_t DynCO::enumerateFunctions(hipFunction_t* functions, unsigned int numFunctions) {
+  std::scoped_lock lock(dclock_);
+
+  assert(functions != nullptr);
+  unsigned int count = 0;
+  for (const auto& kv : functions_) {
+    if (count >= numFunctions) {
+      break;
+    }
+    hipFunction_t hfunc = nullptr;
+    auto ret = kv.second->GetDynFunc(&hfunc, module_);
+    if (ret != hipSuccess) {
+      return ret;
+    }
+    functions[count++] = hfunc;
+  }
+  return hipSuccess;
+}
+
 bool DynCO::isValidDynFunc(const void* hfunc) {
   std::scoped_lock lock(dclock_);
   return std::any_of(functions_.begin(), functions_.end(),
