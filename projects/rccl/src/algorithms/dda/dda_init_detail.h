b185eaed521769f8f1e39639c9878e5489b30560
isaki001 <ioannissakiotis@gmail.com>
2026-08-21 17:06:41 +0000

    enable env. variables to override tuning-table protocol thresholds

--- projects/rccl/src/algorithms/dda/dda_init_detail.h
+++ projects/rccl/src/algorithms/dda/dda_init_detail.h
@@ -41,7 +41,7 @@ constexpr size_t kDdaLL128SlotStrideLines =
 // An explicit buffer-size override takes precedence over derived sizing.
 //
 // The derived size is: max(simpleCap, llFloor, ll128Floor) where:
-// - simpleCap: DDA_THRESHOLD (default 128 MiB)
+// - simpleCap: resolved DDA_THRESHOLD (env if set, else the arch table)
 // - llFloor:   2 banks * nRanks * kDdaLLMaxBytes (when LL enabled)
 // - ll128Floor: 2 banks * nRanks * kDdaLL128SlotStrideLines * 128B (when LL128 enabled)
 //
