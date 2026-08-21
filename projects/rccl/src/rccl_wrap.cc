686eb1d48202bcaa2a079da701a93be0939857df
isaki001 <ioannissakiotis@gmail.com>
2026-08-21 18:46:07 +0000

    prevent channel overrides for gfx1250 from channel tuning table

--- projects/rccl/src/rccl_wrap.cc
+++ projects/rccl/src/rccl_wrap.cc
@@ -264,6 +264,10 @@ extern int64_t rcclParamForceCe();
 RCCL_PARAM(ChannelTuningEnable, "CHANNEL_TUNING_ENABLE", 1);
 
 ncclResult_t rcclOverrideChannels(struct ncclComm* comm, ncclFunc_t coll, size_t nBytes, int& nc) {
+  if (IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx1250")) {
+    INFO(NCCL_TUNING, "RCCL Channel Tuning not applied for gfx1250 (pending sweep data, AICOMRCCL-1756)");
+    return ncclSuccess;
+  }
   const bool isGfx_110x_120x = IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx110") ||
                                IsArchMatch(comm->topo->nodes[GPU].nodes[0].gpu.gcn, "gfx120");
   // Make an exception for gfx110x and gfx120x
