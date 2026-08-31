1cb4865981ec872b435f740731ec7d3dede8f0e9
kesag <keshav.aggarwal@amd.com>
2026-08-31 19:45:10 +0000

    update functional tests; add type support for host, and reduce variants

--- projects/rocshmem/tests/functional_tests/tester.cpp
+++ projects/rocshmem/tests/functional_tests/tester.cpp
@@ -1158,7 +1158,9 @@ std::vector<Tester*> Tester::create(TesterArguments args) {
       break;
     case ReduceOnStreamTestType:
       test_name = "Reduce On Stream";
-      testers.push_back(new ReduceOnStreamTester(args));
+      testers.push_back(new ReduceOnStreamTester<int>(args));
+      testers.push_back(new ReduceOnStreamTester<__half>(args));
+      testers.push_back(new ReduceOnStreamTester<__hip_bfloat16>(args));
       break;
     case HostCtxCreateTestType:
       test_name = "Host CTX Create";
