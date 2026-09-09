ed99ff8b868e61b30e732c75d504b8b5517b160d
Vignesh Edithal <Vignesh.Edithal@amd.com>
2026-09-09 16:33:51 +0000

    Revert "[rocprof-compute] Collecting operator arguments for ml api tracing (#7157)"
    This reverts commit b1bf891933c074ac226180d33e135890f729ae50.
    
    Re-landing operator-argument collection and display as a single combined
    pull request.

--- projects/rocprofiler-compute/src/lib/torch_trace_collector/process_state.h
+++ projects/rocprofiler-compute/src/lib/torch_trace_collector/process_state.h
@@ -5,14 +5,11 @@
 
 #pragma once
 
-#include "schema_arg_names.h"
 #include "snapshot_store.h"
 #include "stats.h"
 #include "synchronized.hpp"
 
 #include <ATen/record_function.h>
-
-#include <atomic>
 
 namespace torch_trace_collector::detail
 {
@@ -29,9 +26,6 @@ struct ProcessState
     Stats                        stats;
     synchronized_t<InstallState> install;
     SnapshotStore                snapshots{stats};
-    SchemaArgNamesCache          schema_arg_names_cache;
-    std::atomic<bool>            capture_args{true};
-    std::atomic<bool>            capture_values{false};
 };
 
 ProcessState& process_state();
