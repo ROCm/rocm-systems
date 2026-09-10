1b510489d54b983331df04eeb31f07ca0c2a9826
Mikhail <lloginov@amd.com>
2026-09-10 20:59:43 +0200

    rem consume_parameters

--- projects/rocprofiler-systems/source/python/libpyrocprofsys.cpp
+++ projects/rocprofiler-systems/source/python/libpyrocprofsys.cpp
@@ -300,7 +300,7 @@ get_frame_code(PyFrameObject* frame)
 }
 //
 void
-profiler_function(py::object pframe, const char* swhat, py::object arg)
+profiler_function(py::object pframe, const char* swhat, [[maybe_unused]] py::object arg)
 {
     if(get_paused() > 0 || g_library_paused.load(std::memory_order_relaxed)) return;
 
@@ -543,9 +543,6 @@ profiler_function(py::object pframe, const char* swhat, py::object arg)
         case PyTrace_C_RETURN: _profiler_return(); break;
         default: break;
     }
-
-    // don't do anything with arg
-    tim::consume_parameters(arg);
 }
 //
 py::module
