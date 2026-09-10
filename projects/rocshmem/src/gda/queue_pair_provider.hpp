1f1bf3103c58e92ce0154cb09f8ce08d0d199371
Omri Mor <Omri.Mor@amd.com>
2026-09-10 02:36:23 +0000

    test(rocshmem): [gda] add QueuePairMock type for mocking QueuePair objects
    rocSHMEM GDA tests using QueuePairMock objects to be added later.
    
    Signed-off-by: Omri Mor <Omri.Mor@amd.com>

--- projects/rocshmem/src/gda/queue_pair_provider.hpp
+++ projects/rocshmem/src/gda/queue_pair_provider.hpp
@@ -27,28 +27,24 @@
 
 #include "rocshmem/rocshmem_config.h"  // NOLINT(build/include_subdir)
 
-#if   defined(GDA_MUX)
+/* define GDA_QUEUEPAIR_MOCK prior to including queue_pair_provider.hpp
+ * to use mock QueuePair objects */
+
+#if   defined(GDA_QUEUEPAIR_MOCK)
+#include "queue_pair/queue_pair_mock.hpp"
+namespace rocshmem { using QueuePair = QueuePairMock; }
+#elif defined(GDA_MUX)
 #include "queue_pair_mux.hpp"
+namespace rocshmem { using QueuePair = QueuePairMux; }
 #elif defined(GDA_IONIC)
 #include "gda/ionic/queue_pair_ionic.hpp"
+namespace rocshmem { using QueuePair = QueuePairIONIC; }
 #elif defined(GDA_BNXT)
 #include "gda/bnxt/queue_pair_bnxt.hpp"
+namespace rocshmem { using QueuePair = QueuePairBNXT; }
 #elif defined(GDA_MLX5)
 #include "gda/mlx5/queue_pair_mlx5.hpp"
+namespace rocshmem { using QueuePair = QueuePairMLX5; }
 #endif
-
-namespace rocshmem {
-
-#if   defined(GDA_MUX)
-using QueuePair = QueuePairMux;
-#elif defined(GDA_IONIC)
-using QueuePair = QueuePairIONIC;
-#elif defined(GDA_BNXT)
-using QueuePair = QueuePairBNXT;
-#elif defined(GDA_MLX5)
-using QueuePair = QueuePairMLX5;
-#endif
-
-}  // namespace rocshmem
 
 #endif  // LIBRARY_SRC_GDA_QUEUE_PAIR_PROVIDER_HPP_
