04bfc08afe3c3d56dd99457c243c83a1eea2836a
Milan Radosavljevic <milan.radosavljevic@amd.com>
2026-09-11 14:20:43 +0200

    add missing on_configure

--- projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk/buffered/kfd_event_page_migrate.hpp
+++ projects/rocprofiler-systems/source/lib/rocprof-sys/library/rocprofiler-sdk/buffered/kfd_event_page_migrate.hpp
@@ -8,6 +8,10 @@
 
 namespace rocprofsys::domains::buffered
 {
+template <policies::rocprofiler_sdk::domain_service_externals Externals>
+inline void
+on_kfd_event_page_migrate_configure()
+{}
 
 template <policies::rocprofiler_sdk::domain_service_backend   SdkBackend,
           policies::rocprofiler_sdk::domain_service_externals Externals>
@@ -31,7 +35,8 @@ inline constexpr auto k_kfd_event_page_migrate = buffered_domain_definition<SdkB
         },
     .on_records = buffered_callback_dispatcher<
         SdkBackend, typename SdkBackend::kfd_event_page_migrate_record,
-        on_kfd_event_page_migrate<SdkBackend, Externals>>::callback
+        on_kfd_event_page_migrate<SdkBackend, Externals>>::callback,
+    .on_configure = on_kfd_event_page_migrate_configure<Externals>
 };
 
 }  // namespace rocprofsys::domains::buffered
