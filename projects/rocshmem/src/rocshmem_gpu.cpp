b3a18324b8f95292aff201c3b57a07d223c5200b
kesag <keshav.aggarwal@amd.com>
2026-09-16 20:05:47 +0000

    fix artifact from rebase

--- projects/rocshmem/src/rocshmem_gpu.cpp
+++ projects/rocshmem/src/rocshmem_gpu.cpp
@@ -3027,9 +3027,6 @@ RMA_GEN(unsigned long long)
 RMA_GEN(__half)
 RMA_GEN(__hip_bfloat16)
 
-RMA_GEN(__half)
-RMA_GEN(__hip_bfloat16)
-
 AMO_STANDARD_GEN(int)
 AMO_STANDARD_GEN(long)
 AMO_STANDARD_GEN(long long)
@@ -3122,9 +3119,6 @@ RMA_DEF_GEN(uint32_t, uint32)
 RMA_DEF_GEN(uint64_t, uint64)
 RMA_DEF_GEN(size_t, size)
 RMA_DEF_GEN(ptrdiff_t, ptrdiff)
-RMA_DEF_GEN(__half, half)
-RMA_DEF_GEN(__hip_bfloat16, bfloat16)
-
 RMA_DEF_GEN(__half, half)
 RMA_DEF_GEN(__hip_bfloat16, bfloat16)
 
