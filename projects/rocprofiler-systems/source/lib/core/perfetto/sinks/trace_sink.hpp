f5e54aff15f437690f96275248dc949134beb2e4
Marjan Antić <marantic@amd.com>
2026-09-16 13:42:21 +0200

    Update projects/rocprofiler-systems/source/lib/core/perfetto/sinks/trace_sink.hpp
    Co-authored-by: Milan Radosavljevic <milan.radosavljevic@amd.com>

--- projects/rocprofiler-systems/source/lib/core/perfetto/sinks/trace_sink.hpp
+++ projects/rocprofiler-systems/source/lib/core/perfetto/sinks/trace_sink.hpp
@@ -23,7 +23,7 @@ public:
     trace_sink_interface(trace_sink_interface&&)                 = delete;
     trace_sink_interface& operator=(trace_sink_interface&&)      = delete;
 
-    virtual void on_source_drained(int source_id, std::vector<char> bytes) = 0;
+    virtual void on_source_drained(int source_id, std::span<char> bytes) = 0;
     virtual void finalize()                                                = 0;
 };
 }  // namespace rocprofsys::core
