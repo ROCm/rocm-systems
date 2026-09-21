3d496753ac1d725f5ef95aa063c5ad03643a1bab
Tony Gutierrez <anthony.gutierrez@amd.com>
2026-09-21 11:43:01 -0700

    fix(amdf) Correct license/copyright
    Signed-off-by: Tony Gutierrez <anthony.gutierrez@amd.com>

--- runtimes/api-headers/include/amdf/amdf.h
+++ runtimes/api-headers/include/amdf/amdf.h
@@ -1,8 +1,5 @@
-// Copyright 2026 The IREE Authors
-//
-// Licensed under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Copyright (c) 2026 Advanced Micro Devices, Inc.
+// SPDX-License-Identifier: MIT
 
 #ifndef AMDF_AMDF_H_
 #define AMDF_AMDF_H_
