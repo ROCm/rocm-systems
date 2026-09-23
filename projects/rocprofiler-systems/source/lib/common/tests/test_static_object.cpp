0fd6d35a5481bcef42be6d3bb8525a534a93bc7e
Meng Cao <mcao@amd.com>
2026-09-23 12:48:48 -0500

    test(rocprofiler-systems): follow constexpr naming convention

--- projects/rocprofiler-systems/source/lib/common/tests/test_static_object.cpp
+++ projects/rocprofiler-systems/source/lib/common/tests/test_static_object.cpp
@@ -10,10 +10,10 @@
 
 namespace
 {
-constexpr auto over_aligned_object_alignment = std::size_t{ 64 };
-constexpr auto expected_value                = std::uint64_t{ 42 };
+constexpr auto k_over_aligned_object_alignment = std::size_t{ 64 };
+constexpr auto k_expected_value                = std::uint64_t{ 42 };
 
-struct alignas(over_aligned_object_alignment) over_aligned_object
+struct alignas(k_over_aligned_object_alignment) over_aligned_object
 {
     std::uint64_t value = 0;
 };
@@ -27,10 +27,10 @@ TEST(static_object, aligns_backing_storage_to_object)
     using object_type =
         rocprofsys::common::static_object<over_aligned_object, over_aligned_context>;
 
-    auto* object = object_type::construct(expected_value);
+    auto* object = object_type::construct(k_expected_value);
 
     ASSERT_NE(object, nullptr);
     EXPECT_EQ(reinterpret_cast<std::uintptr_t>(object) % alignof(over_aligned_object),
               std::uintptr_t{ 0 });
-    EXPECT_EQ(object->value, expected_value);
+    EXPECT_EQ(object->value, k_expected_value);
 }
