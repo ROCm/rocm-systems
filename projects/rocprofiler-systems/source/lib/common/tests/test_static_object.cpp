4a7ca548a2e25f48d7f2d96c0fd94d43d4d1f9f4
Meng Cao <mcao@amd.com>
2026-09-23 10:10:59 -0500

    test(rocprofiler-systems): name static object test constants

--- projects/rocprofiler-systems/source/lib/common/tests/test_static_object.cpp
+++ projects/rocprofiler-systems/source/lib/common/tests/test_static_object.cpp
@@ -5,11 +5,15 @@
 
 #include <gtest/gtest.h>
 
+#include <cstddef>
 #include <cstdint>
 
 namespace
 {
-struct alignas(64) over_aligned_object
+constexpr auto over_aligned_object_alignment = std::size_t{ 64 };
+constexpr auto expected_value                = std::uint64_t{ 42 };
+
+struct alignas(over_aligned_object_alignment) over_aligned_object
 {
     std::uint64_t value = 0;
 };
@@ -23,10 +27,10 @@ TEST(static_object, aligns_backing_storage_to_object)
     using object_type =
         rocprofsys::common::static_object<over_aligned_object, over_aligned_context>;
 
-    auto* object = object_type::construct(std::uint64_t{ 42 });
+    auto* object = object_type::construct(expected_value);
 
     ASSERT_NE(object, nullptr);
     EXPECT_EQ(reinterpret_cast<std::uintptr_t>(object) % alignof(over_aligned_object),
               std::uintptr_t{ 0 });
-    EXPECT_EQ(object->value, std::uint64_t{ 42 });
+    EXPECT_EQ(object->value, expected_value);
 }
