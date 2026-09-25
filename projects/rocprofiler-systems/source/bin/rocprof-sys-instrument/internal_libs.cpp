a9866e37dab213265ea971d08d2ea621129bab6c
Sajina Kandy <sputhala@amd.com>
2026-09-25 12:34:53 -0500

    Fix formatting and clang-tidy errors

--- projects/rocprofiler-systems/source/bin/rocprof-sys-instrument/internal_libs.cpp
+++ projects/rocprofiler-systems/source/bin/rocprof-sys-instrument/internal_libs.cpp
@@ -313,7 +313,7 @@ get_internal_basic_libs_impl()
                                            LIBTHREAD_DB_SO,    LIBUTIL_SO };
 
     // shared libraries used by or provided by dyninst
-    const auto _dyn_libs =
+    const auto dyn_libs =
         strview_init_t{ "libdyninstAPI_RT.so", "libcommon.so", "libbfd.so", "libelf.so",
                         "libdwarf.so",         "libdw.so",     "libz.so",   "libzstd.so",
                         "libbz2.so",           "liblzma.so" };
@@ -353,7 +353,7 @@ get_internal_basic_libs_impl()
         "libtcmalloc_minimal_debug.so",
     };
 
-    for(const auto& gitr : { _gnu_libs, _dyn_libs, _rocprof_sys_libs, _3rdparty_libs })
+    for(const auto& gitr : { _gnu_libs, dyn_libs, _rocprof_sys_libs, _3rdparty_libs })
     {
         for(auto itr : gitr)
         {
