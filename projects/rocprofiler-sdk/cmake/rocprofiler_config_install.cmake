b6cfd80ce6cba66815880dcc6fa288e85e848493
Young Hui <young.hui@amd.com>
2026-09-25 18:23:38 -0400

    copy requirements.txt at configure time

--- projects/rocprofiler-sdk/cmake/rocprofiler_config_install.cmake
+++ projects/rocprofiler-sdk/cmake/rocprofiler_config_install.cmake
@@ -36,10 +36,15 @@ install(
     COMPONENT tests
     USE_SOURCE_PERMISSIONS)
 
+configure_file(
+    ${PROJECT_SOURCE_DIR}/requirements-runtime.txt
+    ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/${PACKAGE_NAME}/requirements.txt
+    COPYONLY)
+
 install(
-    FILES ${PROJECT_SOURCE_DIR}/requirements-runtime.txt
+    FILES
+        ${PROJECT_BINARY_DIR}/${CMAKE_INSTALL_DATAROOTDIR}/${PACKAGE_NAME}/requirements.txt
     DESTINATION ${CMAKE_INSTALL_DATAROOTDIR}/${PACKAGE_NAME}
-    RENAME requirements.txt
     COMPONENT core)
 
 # tests/requirements.txt includes requirements-runtime.txt via a relative "-r" line
